8d9303c7d249cbd5d5e5b39cb3191e440643ff73
Victor Turansky <victor.turansky@gmail.com>
2021-08-06 06:46:26 +0300

    Factory for `IntrinsicType`

--- react-kotlin/src/main/kotlin/react/IntrinsicType.kt
+++ react-kotlin/src/main/kotlin/react/IntrinsicType.kt
@@ -1,5 +1,13 @@
 // Automatically generated - do not modify!
 
+@file:Suppress("NOTHING_TO_INLINE")
+
 package react
 
+
 external interface IntrinsicType<P : react.RProps> : ElementType<P>
+
+inline fun <P : react.RProps> IntrinsicType(
+    tag: String,
+): IntrinsicType<P> =
+    tag.unsafeCast<IntrinsicType<P>>()
