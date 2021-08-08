24e61566dfe205e10cd94149a5051772535e611a
Victor Turansky <victor.turansky@gmail.com>
2021-08-08 03:29:32 +0300

    Update declarations

---
+++ react-kotlin/src/main/kotlin/react/dom/NativeEvents.kt
@@ -0,0 +1,27 @@
+// Automatically generated - do not modify!
+
+package react.dom
+
+typealias NativeAnimationEvent = org.w3c.dom.events.Event
+
+typealias NativeClipboardEvent = org.w3c.dom.events.Event
+
+typealias NativeCompositionEvent = org.w3c.dom.events.CompositionEvent
+
+typealias NativeDragEvent = org.w3c.dom.events.Event
+
+typealias NativeFocusEvent = org.w3c.dom.events.FocusEvent
+
+typealias NativeKeyboardEvent = org.w3c.dom.events.KeyboardEvent
+
+typealias NativeMouseEvent = org.w3c.dom.events.MouseEvent
+
+typealias NativeTouchEvent = org.w3c.dom.events.Event
+
+typealias NativePointerEvent = org.w3c.dom.events.Event
+
+typealias NativeTransitionEvent = org.w3c.dom.events.Event
+
+typealias NativeUIEvent = org.w3c.dom.events.UIEvent
+
+typealias NativeWheelEvent = org.w3c.dom.events.WheelEvent
