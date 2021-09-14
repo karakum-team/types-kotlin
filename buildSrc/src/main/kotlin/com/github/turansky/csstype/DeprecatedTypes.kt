a85c849a57d0a0ca12ec374fd4526854ff89b79b
Victor Turansky <victor.turansky@gmail.com>
2021-09-14 21:31:46 +0300

    Deprecated types [A-L]

---
+++ buildSrc/src/main/kotlin/com/github/turansky/csstype/DeprecatedTypes.kt
@@ -0,0 +1,26 @@
+package com.github.turansky.csstype
+
+internal val DEPRECATED_TYPES = setOf(
+    "Azimuth",
+
+    "BoxAlign",
+    "BoxDirection",
+    "BoxFlex",
+    "BoxFlexGroup",
+    "BoxLines",
+    "BoxOrdinalGroup",
+    "BoxOrient",
+    "BoxPack",
+
+    "CompositeStyle",
+
+    "Dasharray",
+
+    "FontVariantAlternates",
+
+    "GridColumnGap",
+    "GridGap",
+    "GridRowGap",
+
+    "ImeMode",
+)
