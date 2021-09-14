889ce8e92b7c299b3d95222a8707cb60e37c6228
Victor Turansky <victor.turansky@gmail.com>
2021-09-14 04:38:06 +0300

    Add `Length` generation logic

---
+++ buildSrc/src/main/kotlin/com/github/turansky/csstype/Length.kt
@@ -0,0 +1,53 @@
+package com.github.turansky.csstype
+
+private const val LENGTH = "Length"
+
+private val TYPES = listOf(
+    // Units
+    LengthType("ch"),
+    LengthType("em"),
+    LengthType("ex"),
+    LengthType("rem"),
+
+    // Viewport-percentage lengths
+    LengthType("vh"),
+    LengthType("vw"),
+    LengthType("vmin"),
+    LengthType("vmax"),
+
+    // Absolute length units
+    LengthType("px"),
+    LengthType("cm"),
+    LengthType("mm"),
+    LengthType("`in`", "in"),
+    LengthType("pc"),
+    LengthType("pt"),
+
+    // Percent
+    LengthType("pct", "%"),
+)
+
+private data class LengthType(
+    val name: String,
+    val suffix: String,
+) {
+    constructor(name: String) : this(name, name)
+}
+
+internal fun Length(): ConversionResult {
+    val declarations = sequenceOf(
+        "sealed external interface $LENGTH",
+
+        """
+            inline fun $LENGTH(value: String): Length =
+                value.unsafeCast<$LENGTH>()
+        """.trimIndent()
+    ) + TYPES.map { (name, suffix) ->
+        """
+            inline val Number.$name: Length
+                get() = "${'$'}{this}$suffix".unsafeCast<$LENGTH>()
+        """.trimIndent()
+    }
+
+    return ConversionResult(LENGTH, declarations.joinToString("\n\n"))
+}
