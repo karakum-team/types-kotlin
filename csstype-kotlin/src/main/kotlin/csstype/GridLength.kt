5ad79f6a5d2f1637ac98bb0589ee7120092ff417
Victor Turansky <victor.turansky@gmail.com>
2021-09-24 00:29:10 +0300

    Update declarations

---
+++ csstype-kotlin/src/main/kotlin/csstype/GridLength.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+sealed external interface GridLength
+
+inline val Number.fr: GridLength
+    get() = "${this}fr".unsafeCast<GridLength>()
