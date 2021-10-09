c0d828394b90aeaf22f9faea25d743ccf1dfab88
Victor Turansky <victor.turansky@gmail.com>
2021-10-09 07:02:02 +0300

    Update declarations

--- react-kotlin/src/main/kotlin/react/dom/svg/ReactSVG.kt
+++ react-kotlin/src/main/kotlin/react/dom/svg/ReactSVG.kt
@@ -2,173 +2,177 @@
 
 package react.dom.svg
 
+import org.w3c.css.masking.SVGClipPathElement
+import org.w3c.css.masking.SVGMaskElement
+import org.w3c.dom.svg.*
 import react.IntrinsicType
 
-typealias DefaultSvgType = IntrinsicType<SVGAttributes<org.w3c.dom.svg.SVGElement>>
-
 object ReactSVG {
-    inline val animate: DefaultSvgType
-        get() = "animate".unsafeCast<DefaultSvgType>()
+    inline val svg: IntrinsicType<SVGAttributes<SVGSVGElement>>
+        get() = "svg".unsafeCast<IntrinsicType<SVGAttributes<SVGSVGElement>>>()
 
-    inline val circle: DefaultSvgType
-        get() = "circle".unsafeCast<DefaultSvgType>()
+    inline val animate: IntrinsicType<SVGAttributes<SVGElement>>
+        get() = "animate".unsafeCast<IntrinsicType<SVGAttributes<SVGElement>>>()
 
-    inline val clipPath: DefaultSvgType
-        get() = "clipPath".unsafeCast<DefaultSvgType>()
+    inline val animateTransform: IntrinsicType<SVGAttributes<SVGElement>>
+        get() = "animateTransform".unsafeCast<IntrinsicType<SVGAttributes<SVGElement>>>()
 
-    inline val defs: DefaultSvgType
-        get() = "defs".unsafeCast<DefaultSvgType>()
+    inline val clipPath: IntrinsicType<SVGAttributes<SVGClipPathElement>>
+        get() = "clipPath".unsafeCast<IntrinsicType<SVGAttributes<SVGClipPathElement>>>()
 
-    inline val desc: DefaultSvgType
-        get() = "desc".unsafeCast<DefaultSvgType>()
+    inline val defs: IntrinsicType<SVGAttributes<SVGDefsElement>>
+        get() = "defs".unsafeCast<IntrinsicType<SVGAttributes<SVGDefsElement>>>()
 
-    inline val ellipse: DefaultSvgType
-        get() = "ellipse".unsafeCast<DefaultSvgType>()
+    inline val desc: IntrinsicType<SVGAttributes<SVGDescElement>>
+        get() = "desc".unsafeCast<IntrinsicType<SVGAttributes<SVGDescElement>>>()
 
-    inline val feBlend: DefaultSvgType
-        get() = "feBlend".unsafeCast<DefaultSvgType>()
+    inline val ellipse: IntrinsicType<SVGAttributes<SVGEllipseElement>>
+        get() = "ellipse".unsafeCast<IntrinsicType<SVGAttributes<SVGEllipseElement>>>()
 
-    inline val feColorMatrix: DefaultSvgType
-        get() = "feColorMatrix".unsafeCast<DefaultSvgType>()
+    inline val feBlend: IntrinsicType<SVGAttributes<SVGElement /* SVGFEBlendElement */>>
+        get() = "feBlend".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEBlendElement */>>>()
 
-    inline val feComponentTransfer: DefaultSvgType
-        get() = "feComponentTransfer".unsafeCast<DefaultSvgType>()
+    inline val feColorMatrix: IntrinsicType<SVGAttributes<SVGElement /* SVGFEColorMatrixElement */>>
+        get() = "feColorMatrix".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEColorMatrixElement */>>>()
 
-    inline val feComposite: DefaultSvgType
-        get() = "feComposite".unsafeCast<DefaultSvgType>()
+    inline val feComponentTransfer: IntrinsicType<SVGAttributes<SVGElement /* SVGFEComponentTransferElement */>>
+        get() = "feComponentTransfer".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEComponentTransferElement */>>>()
 
-    inline val feConvolveMatrix: DefaultSvgType
-        get() = "feConvolveMatrix".unsafeCast<DefaultSvgType>()
+    inline val feComposite: IntrinsicType<SVGAttributes<SVGElement /* SVGFECompositeElement */>>
+        get() = "feComposite".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFECompositeElement */>>>()
 
-    inline val feDiffuseLighting: DefaultSvgType
-        get() = "feDiffuseLighting".unsafeCast<DefaultSvgType>()
+    inline val feConvolveMatrix: IntrinsicType<SVGAttributes<SVGElement /* SVGFEConvolveMatrixElement */>>
+        get() = "feConvolveMatrix".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEConvolveMatrixElement */>>>()
 
-    inline val feDisplacementMap: DefaultSvgType
-        get() = "feDisplacementMap".unsafeCast<DefaultSvgType>()
+    inline val feDiffuseLighting: IntrinsicType<SVGAttributes<SVGElement /* SVGFEDiffuseLightingElement */>>
+        get() = "feDiffuseLighting".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEDiffuseLightingElement */>>>()
 
-    inline val feDistantLight: DefaultSvgType
-        get() = "feDistantLight".unsafeCast<DefaultSvgType>()
+    inline val feDisplacementMap: IntrinsicType<SVGAttributes<SVGElement /* SVGFEDisplacementMapElement */>>
+        get() = "feDisplacementMap".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEDisplacementMapElement */>>>()
 
-    inline val feDropShadow: DefaultSvgType
-        get() = "feDropShadow".unsafeCast<DefaultSvgType>()
+    inline val feDistantLight: IntrinsicType<SVGAttributes<SVGElement /* SVGFEDistantLightElement */>>
+        get() = "feDistantLight".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEDistantLightElement */>>>()
 
-    inline val feFlood: DefaultSvgType
-        get() = "feFlood".unsafeCast<DefaultSvgType>()
+    inline val feDropShadow: IntrinsicType<SVGAttributes<SVGElement /* SVGFEDropShadowElement */>>
+        get() = "feDropShadow".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEDropShadowElement */>>>()
 
-    inline val feFuncA: DefaultSvgType
-        get() = "feFuncA".unsafeCast<DefaultSvgType>()
+    inline val feFlood: IntrinsicType<SVGAttributes<SVGElement /* SVGFEFloodElement */>>
+        get() = "feFlood".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEFloodElement */>>>()
 
-    inline val feFuncB: DefaultSvgType
-        get() = "feFuncB".unsafeCast<DefaultSvgType>()
+    inline val feFuncA: IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncAElement */>>
+        get() = "feFuncA".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncAElement */>>>()
 
-    inline val feFuncG: DefaultSvgType
-        get() = "feFuncG".unsafeCast<DefaultSvgType>()
+    inline val feFuncB: IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncBElement */>>
+        get() = "feFuncB".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncBElement */>>>()
 
-    inline val feFuncR: DefaultSvgType
-        get() = "feFuncR".unsafeCast<DefaultSvgType>()
+    inline val feFuncG: IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncGElement */>>
+        get() = "feFuncG".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncGElement */>>>()
 
-    inline val feGaussianBlur: DefaultSvgType
-        get() = "feGaussianBlur".unsafeCast<DefaultSvgType>()
+    inline val feFuncR: IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncRElement */>>
+        get() = "feFuncR".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEFuncRElement */>>>()
 
-    inline val feImage: DefaultSvgType
-        get() = "feImage".unsafeCast<DefaultSvgType>()
+    inline val feGaussianBlur: IntrinsicType<SVGAttributes<SVGElement /* SVGFEGaussianBlurElement */>>
+        get() = "feGaussianBlur".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEGaussianBlurElement */>>>()
 
-    inline val feMerge: DefaultSvgType
-        get() = "feMerge".unsafeCast<DefaultSvgType>()
+    inline val feImage: IntrinsicType<SVGAttributes<SVGElement /* SVGFEImageElement */>>
+        get() = "feImage".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEImageElement */>>>()
 
-    inline val feMergeNode: DefaultSvgType
-        get() = "feMergeNode".unsafeCast<DefaultSvgType>()
+    inline val feMerge: IntrinsicType<SVGAttributes<SVGElement /* SVGFEMergeElement */>>
+        get() = "feMerge".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEMergeElement */>>>()
 
-    inline val feMorphology: DefaultSvgType
-        get() = "feMorphology".unsafeCast<DefaultSvgType>()
+    inline val feMergeNode: IntrinsicType<SVGAttributes<SVGElement /* SVGFEMergeNodeElement */>>
+        get() = "feMergeNode".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEMergeNodeElement */>>>()
 
-    inline val feOffset: DefaultSvgType
-        get() = "feOffset".unsafeCast<DefaultSvgType>()
+    inline val feMorphology: IntrinsicType<SVGAttributes<SVGElement /* SVGFEMorphologyElement */>>
+        get() = "feMorphology".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEMorphologyElement */>>>()
 
-    inline val fePointLight: DefaultSvgType
-        get() = "fePointLight".unsafeCast<DefaultSvgType>()
+    inline val feOffset: IntrinsicType<SVGAttributes<SVGElement /* SVGFEOffsetElement */>>
+        get() = "feOffset".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEOffsetElement */>>>()
 
-    inline val feSpecularLighting: DefaultSvgType
-        get() = "feSpecularLighting".unsafeCast<DefaultSvgType>()
+    inline val fePointLight: IntrinsicType<SVGAttributes<SVGElement /* SVGFEPointLightElement */>>
+        get() = "fePointLight".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFEPointLightElement */>>>()
 
-    inline val feSpotLight: DefaultSvgType
-        get() = "feSpotLight".unsafeCast<DefaultSvgType>()
+    inline val feSpecularLighting: IntrinsicType<SVGAttributes<SVGElement /* SVGFESpecularLightingElement */>>
+        get() = "feSpecularLighting".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFESpecularLightingElement */>>>()
 
-    inline val feTile: DefaultSvgType
-        get() = "feTile".unsafeCast<DefaultSvgType>()
+    inline val feSpotLight: IntrinsicType<SVGAttributes<SVGElement /* SVGFESpotLightElement */>>
+        get() = "feSpotLight".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFESpotLightElement */>>>()
 
-    inline val feTurbulence: DefaultSvgType
-        get() = "feTurbulence".unsafeCast<DefaultSvgType>()
+    inline val feTile: IntrinsicType<SVGAttributes<SVGElement /* SVGFETileElement */>>
+        get() = "feTile".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFETileElement */>>>()
 
-    inline val filter: DefaultSvgType
-        get() = "filter".unsafeCast<DefaultSvgType>()
+    inline val feTurbulence: IntrinsicType<SVGAttributes<SVGElement /* SVGFETurbulenceElement */>>
+        get() = "feTurbulence".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFETurbulenceElement */>>>()
 
-    inline val foreignObject: DefaultSvgType
-        get() = "foreignObject".unsafeCast<DefaultSvgType>()
+    inline val filter: IntrinsicType<SVGAttributes<SVGElement /* SVGFilterElement */>>
+        get() = "filter".unsafeCast<IntrinsicType<SVGAttributes<SVGElement /* SVGFilterElement */>>>()
 
-    inline val g: DefaultSvgType
-        get() = "g".unsafeCast<DefaultSvgType>()
+    inline val foreignObject: IntrinsicType<SVGAttributes<SVGForeignObjectElement>>
+        get() = "foreignObject".unsafeCast<IntrinsicType<SVGAttributes<SVGForeignObjectElement>>>()
 
-    inline val image: DefaultSvgType
-        get() = "image".unsafeCast<DefaultSvgType>()
+    inline val g: IntrinsicType<SVGAttributes<SVGGElement>>
+        get() = "g".unsafeCast<IntrinsicType<SVGAttributes<SVGGElement>>>()
 
-    inline val line: DefaultSvgType
-        get() = "line".unsafeCast<DefaultSvgType>()
+    inline val image: IntrinsicType<SVGAttributes<SVGImageElement>>
+        get() = "image".unsafeCast<IntrinsicType<SVGAttributes<SVGImageElement>>>()
 
-    inline val linearGradient: DefaultSvgType
-        get() = "linearGradient".unsafeCast<DefaultSvgType>()
+    inline val line: IntrinsicType<SVGAttributes<SVGLineElement>>
+        get() = "line".unsafeCast<IntrinsicType<SVGAttributes<SVGLineElement>>>()
 
-    inline val marker: DefaultSvgType
-        get() = "marker".unsafeCast<DefaultSvgType>()
+    inline val linearGradient: IntrinsicType<SVGAttributes<SVGLinearGradientElement>>
+        get() = "linearGradient".unsafeCast<IntrinsicType<SVGAttributes<SVGLinearGradientElement>>>()
 
-    inline val mask: DefaultSvgType
-        get() = "mask".unsafeCast<DefaultSvgType>()
+    inline val marker: IntrinsicType<SVGAttributes<SVGMarkerElement>>
+        get() = "marker".unsafeCast<IntrinsicType<SVGAttributes<SVGMarkerElement>>>()
 
-    inline val metadata: DefaultSvgType
-        get() = "metadata".unsafeCast<DefaultSvgType>()
+    inline val mask: IntrinsicType<SVGAttributes<SVGMaskElement>>
+        get() = "mask".unsafeCast<IntrinsicType<SVGAttributes<SVGMaskElement>>>()
 
-    inline val path: DefaultSvgType
-        get() = "path".unsafeCast<DefaultSvgType>()
+    inline val metadata: IntrinsicType<SVGAttributes<SVGMetadataElement>>
+        get() = "metadata".unsafeCast<IntrinsicType<SVGAttributes<SVGMetadataElement>>>()
 
-    inline val pattern: DefaultSvgType
-        get() = "pattern".unsafeCast<DefaultSvgType>()
+    inline val mpath: IntrinsicType<SVGAttributes<SVGElement>>
+        get() = "mpath".unsafeCast<IntrinsicType<SVGAttributes<SVGElement>>>()
 
-    inline val polygon: DefaultSvgType
-        get() = "polygon".unsafeCast<DefaultSvgType>()
+    inline val path: IntrinsicType<SVGAttributes<SVGPathElement>>
+        get() = "path".unsafeCast<IntrinsicType<SVGAttributes<SVGPathElement>>>()
 
-    inline val polyline: DefaultSvgType
-        get() = "polyline".unsafeCast<DefaultSvgType>()
+    inline val pattern: IntrinsicType<SVGAttributes<SVGPatternElement>>
+        get() = "pattern".unsafeCast<IntrinsicType<SVGAttributes<SVGPatternElement>>>()
 
-    inline val radialGradient: DefaultSvgType
-        get() = "radialGradient".unsafeCast<DefaultSvgType>()
+    inline val polygon: IntrinsicType<SVGAttributes<SVGPolygonElement>>
+        get() = "polygon".unsafeCast<IntrinsicType<SVGAttributes<SVGPolygonElement>>>()
 
-    inline val rect: DefaultSvgType
-        get() = "rect".unsafeCast<DefaultSvgType>()
+    inline val polyline: IntrinsicType<SVGAttributes<SVGPolylineElement>>
+        get() = "polyline".unsafeCast<IntrinsicType<SVGAttributes<SVGPolylineElement>>>()
 
-    inline val stop: DefaultSvgType
-        get() = "stop".unsafeCast<DefaultSvgType>()
+    inline val radialGradient: IntrinsicType<SVGAttributes<SVGRadialGradientElement>>
+        get() = "radialGradient".unsafeCast<IntrinsicType<SVGAttributes<SVGRadialGradientElement>>>()
 
-    inline val svg: DefaultSvgType
-        get() = "svg".unsafeCast<DefaultSvgType>()
+    inline val rect: IntrinsicType<SVGAttributes<SVGRectElement>>
+        get() = "rect".unsafeCast<IntrinsicType<SVGAttributes<SVGRectElement>>>()
 
-    inline val switch: DefaultSvgType
-        get() = "switch".unsafeCast<DefaultSvgType>()
+    inline val stop: IntrinsicType<SVGAttributes<SVGStopElement>>
+        get() = "stop".unsafeCast<IntrinsicType<SVGAttributes<SVGStopElement>>>()
 
-    inline val symbol: DefaultSvgType
-        get() = "symbol".unsafeCast<DefaultSvgType>()
+    inline val switch: IntrinsicType<SVGAttributes<SVGSwitchElement>>
+        get() = "switch".unsafeCast<IntrinsicType<SVGAttributes<SVGSwitchElement>>>()
 
-    inline val text: DefaultSvgType
-        get() = "text".unsafeCast<DefaultSvgType>()
+    inline val symbol: IntrinsicType<SVGAttributes<SVGSymbolElement>>
+        get() = "symbol".unsafeCast<IntrinsicType<SVGAttributes<SVGSymbolElement>>>()
 
-    inline val textPath: DefaultSvgType
-        get() = "textPath".unsafeCast<DefaultSvgType>()
+    inline val text: IntrinsicType<SVGAttributes<SVGTextElement>>
+        get() = "text".unsafeCast<IntrinsicType<SVGAttributes<SVGTextElement>>>()
 
-    inline val tspan: DefaultSvgType
-        get() = "tspan".unsafeCast<DefaultSvgType>()
+    inline val textPath: IntrinsicType<SVGAttributes<SVGTextPathElement>>
+        get() = "textPath".unsafeCast<IntrinsicType<SVGAttributes<SVGTextPathElement>>>()
 
-    inline val use: DefaultSvgType
-        get() = "use".unsafeCast<DefaultSvgType>()
+    inline val tspan: IntrinsicType<SVGAttributes<SVGTSpanElement>>
+        get() = "tspan".unsafeCast<IntrinsicType<SVGAttributes<SVGTSpanElement>>>()
 
-    inline val view: DefaultSvgType
-        get() = "view".unsafeCast<DefaultSvgType>()
+    inline val use: IntrinsicType<SVGAttributes<SVGUseElement>>
+        get() = "use".unsafeCast<IntrinsicType<SVGAttributes<SVGUseElement>>>()
+
+    inline val view: IntrinsicType<SVGAttributes<SVGViewElement>>
+        get() = "view".unsafeCast<IntrinsicType<SVGAttributes<SVGViewElement>>>()
 }
