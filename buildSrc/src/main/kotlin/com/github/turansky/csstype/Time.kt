bf43d56f716d64ef67d3e599be87b6e488ea70bf
Victor Turansky <victor.turansky@gmail.com>
2021-11-04 02:28:37 +0300

    `Time` factory extensions

--- buildSrc/src/main/kotlin/com/github/turansky/csstype/Time.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/csstype/Time.kt
@@ -3,6 +3,11 @@ package com.github.turansky.csstype
 private const val TIME = "Time"
 
 internal fun Time(): ConversionResult {
-    val body = "sealed external interface $TIME: $TIME_PROPERTY"
+    val body = sequenceOf(
+        "sealed external interface $TIME: $TIME_PROPERTY",
+        unitsExtension(TIME, "ms", "ms"),
+        unitsExtension(TIME, "s", "s"),
+    ).joinToString("\n\n")
+
     return ConversionResult(TIME, body)
 }
