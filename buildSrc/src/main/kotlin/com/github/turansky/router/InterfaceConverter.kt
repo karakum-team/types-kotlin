6c46a279186f5a2dcb3e2b2d41ca0dd5ec1f7537
Victor Turansky <victor.turansky@gmail.com>
2021-11-14 20:43:33 +0300

    Initial `interface` parsing

--- buildSrc/src/main/kotlin/com/github/turansky/router/InterfaceConverter.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/router/InterfaceConverter.kt
@@ -1,8 +1,51 @@
 package com.github.turansky.router
 
+private val CONVERTABLE = setOf(
+    "Path",
+)
+
 internal fun convertInterface(
     name: String,
     source: String,
 ): String {
-    return source
+    if (name !in CONVERTABLE)
+        return source
+
+    val declaration = source.substringBefore(" {")
+        .replace("interface ", "external interface ")
+        .replace(" extends ", " : ")
+
+    val members = source.substringAfter(" {\n")
+        .substringBefore(";\n}")
+        .trimIndent()
+        .splitToSequence(";\n")
+        .joinToString("\n", transform = ::convertMember)
+
+    return "$declaration {\n$members\n}"
+}
+
+private fun convertMember(
+    source: String,
+): String {
+    val comment = source.substringBeforeLast("\n")
+    val body = source.substringAfterLast("\n")
+
+    val declaration = convertParameter(body)
+
+    return sequenceOf(comment, declaration)
+        .filter { it.isNotEmpty() }
+        .joinToString("\n")
+}
+
+private fun convertParameter(
+    source: String,
+): String {
+    val name = source.substringBefore(": ")
+        .substringBefore("?:")
+
+    var type = source.substringAfter(": ")
+    if ("?: " in source)
+        type += "?"
+
+    return "var $name: $type"
 }
