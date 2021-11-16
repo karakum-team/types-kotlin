21dbb37bdb155a0331050f9740de024b34ae4cbb
Victor Turansky <victor.turansky@gmail.com>
2021-11-16 04:06:16 +0300

    Update declarations

--- react-router-dom-kotlin/src/main/kotlin/react/router/dom/useSearchParams.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A convenient wrapper for reading and writing search parameters via the
- * URLSearchParams interface.
- */
-function useSearchParams(defaultInit?: URLSearchParamsInit): readonly [URLSearchParams, (nextInit: URLSearchParamsInit, navigateOptions?: {
-    replace?: boolean | undefined;
-    state?: any;
-} | undefined) => void];
