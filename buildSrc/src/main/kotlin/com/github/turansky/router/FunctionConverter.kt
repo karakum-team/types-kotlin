699c89d63424ffc5efefc1ccb1bd1afc105e5d25
Victor Turansky <victor.turansky@gmail.com>
2021-11-16 03:04:51 +0300

    Separate conversion method for component

--- buildSrc/src/main/kotlin/com/github/turansky/router/FunctionConverter.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/router/FunctionConverter.kt
@@ -4,16 +4,22 @@ internal fun convertFunction(
     name: String,
     source: String,
 ): String {
-    if ("Props): " in source) {
-        var propsType = source
-            .substringBefore("): ")
-            .substringAfterLast(": ")
-
-        if (name == "Route")
-            propsType = "react.Props /* $propsType */"
-
-        return "external val $name: react.FC<$propsType>"
-    }
+    if ("Props): " in source)
+        return convertComponent(name, source)
 
     return source
+}
+
+private fun convertComponent(
+    name: String,
+    source: String,
+): String {
+    var propsType = source
+        .substringBefore("): ")
+        .substringAfterLast(": ")
+
+    if (name == "Route")
+        propsType = "react.Props /* $propsType */"
+
+    return "external val $name: react.FC<$propsType>"
 }
