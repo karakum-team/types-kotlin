bc7b4bffdcbb4e6c75868130ba4c98de88b8559f
Victor Turansky <victor.turansky@gmail.com>
2022-01-16 05:24:32 +0300

    TypeScript. Split sources

--- buildSrc/src/main/kotlin/karakum/typescript/Converter.kt
+++ buildSrc/src/main/kotlin/karakum/typescript/Converter.kt
@@ -9,9 +9,60 @@ internal data class ConversionResult(
 
 internal fun convertDefinitions(
     definitionsFile: File,
-): Sequence<ConversionResult> {
-    val types = definitionsFile.readText()
-    println("TS source: " + types.split("\n").size)
+): Sequence<ConversionResult> =
+    definitionsFile.readText()
+        .splitToSequence("declare namespace ts {\n")
+        .map { it.substringBefore("\n}\n") }
+        .map { it.trimIndent() }
+        .flatMap { convertDefinitions(it) }
 
-    return emptySequence()
+private const val DELIMITER = "<!--DELIMITER-->"
+private val KEYWORDS = setOf(
+    "const",
+    "function",
+    "type",
+    "interface",
+    "class",
+)
+
+private fun convertDefinitions(
+    source: String,
+): Sequence<ConversionResult> {
+    var content = source.replace("\n/**", "\n$DELIMITER/**")
+    for (keyword in KEYWORDS) {
+        content = content
+            .replace("\nexport $keyword ", "\n${DELIMITER}$keyword ")
+            .replace("\n$keyword ", "\n${DELIMITER}$keyword ")
+    }
+
+    var comment: String? = null
+    val results = mutableListOf<ConversionResult>()
+    for (part in content.splitToSequence(DELIMITER).drop(1)) {
+        if (part.startsWith("/**")) {
+            comment = part
+        } else {
+            results += convertDefinition(comment, part)
+        }
+    }
+
+    return results.asSequence()
+}
+
+private fun convertDefinition(
+    comment: String?,
+    source: String,
+): ConversionResult {
+    val name = source.substringAfter(" ")
+        .substringBefore(" ")
+        .substringBefore("<")
+        .substringBefore("(")
+        .substringBefore(":")
+
+    val body = sequenceOf(comment, source)
+        .filterNotNull()
+        .joinToString("\n") {
+            it.removeSuffix("\n")
+        }
+
+    return ConversionResult(name, body)
 }
