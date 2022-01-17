da864824b2cdfc8313be712e00d8ca4dc586a736
Victor Turansky <victor.turansky@gmail.com>
2022-01-17 05:57:59 +0300

    Convert simple string unions

--- typescript-kotlin/src/main/kotlin/typescript/RefactorTriggerReason.kt
+++ typescript-kotlin/src/main/kotlin/typescript/RefactorTriggerReason.kt
@@ -1,9 +1,20 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
 package typescript
 
 /**
  * A set of edits to make in response to a refactor action, plus an optional
  * location where renaming should be invoked from
  */
-typealias RefactorTriggerReason = Any /* "implicit" | "invoked" */
+// language=JavaScript
+@JsName("""(/*union*/{implicit: 'implicit', invoked: 'invoked'}/*union*/)""")
+external enum class RefactorTriggerReason {
+    implicit,
+    invoked,
+
+    ;
+}
--- typescript-kotlin/src/main/kotlin/typescript/TypeOfTag.kt
+++ typescript-kotlin/src/main/kotlin/typescript/TypeOfTag.kt
@@ -1,9 +1,26 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
 package typescript
 
 /**
  * Unique identifier with a package name and version.
  * If changing this, remember to change `packageIdIsEqual`.
  */
-typealias TypeOfTag = Any /* "undefined" | "number" | "bigint" | "boolean" | "string" | "symbol" | "object" | "function" */
+// language=JavaScript
+@JsName("""(/*union*/{undefined: 'undefined', number: 'number', bigint: 'bigint', boolean: 'boolean', string: 'string', symbol: 'symbol', object: 'object', function: 'function'}/*union*/)""")
+external enum class TypeOfTag {
+    undefined,
+    number,
+    bigint,
+    boolean,
+    string,
+    symbol,
+    `object`,
+    function,
+
+    ;
+}
