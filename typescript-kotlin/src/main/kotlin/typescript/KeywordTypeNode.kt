4c7df2236fe0da45800f07dd15201781bcb1f45c
Victor Turansky <victor.turansky@gmail.com>
2022-01-18 03:50:33 +0300

    Update declarations

---
+++ typescript-kotlin/src/main/kotlin/typescript/KeywordTypeNode.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package typescript
+
+external interface KeywordTypeNode<TKind : KeywordTypeSyntaxKind> : KeywordToken<TKind>, TypeNode {
+    /*
+    readonly kind: TKind;
+    */
+}
