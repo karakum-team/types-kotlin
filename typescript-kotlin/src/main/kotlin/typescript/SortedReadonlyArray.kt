a50bfb94717361adfc0bbb8686b8d5cc6fc57b9d
Victor Turansky <victor.turansky@gmail.com>
2022-01-18 03:43:24 +0300

    Update declarations

---
+++ typescript-kotlin/src/main/kotlin/typescript/SortedReadonlyArray.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "EXTERNAL_TYPE_EXTENDS_NON_EXTERNAL_TYPE",
+    "INTERFACE_WITH_SUPERCLASS",
+    "FINAL_SUPERTYPE",
+)
+
+package typescript
+
+external interface SortedReadonlyArray<T> : ReadonlyArray<T> {
+    /*
+    " __sortedArrayBrand": any;
+    */
+}
