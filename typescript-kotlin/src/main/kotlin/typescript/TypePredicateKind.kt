4fc5df368828af09aeaf435663f66accd50b4aac
Victor Turansky <victor.turansky@gmail.com>
2022-01-18 14:50:59 +0300

    Update declarations

--- typescript-kotlin/src/main/kotlin/typescript/TypePredicateKind.kt
+++ typescript-kotlin/src/main/kotlin/typescript/TypePredicateKind.kt
@@ -2,17 +2,17 @@
 
 @file:Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package typescript
 
 // language=JavaScript
 @JsName("""(/*union*/{This: 0, Identifier: 1, AssertsThis: 2, AssertsIdentifier: 3}/*union*/)""")
-external enum class TypePredicateKind {
-    This,
-    Identifier,
-    AssertsThis,
-    AssertsIdentifier,
+sealed external interface TypePredicateKind {
+    object This : TypePredicateKind
+    object Identifier : TypePredicateKind
+    object AssertsThis : TypePredicateKind
+    object AssertsIdentifier : TypePredicateKind
 
-    ;
 }
