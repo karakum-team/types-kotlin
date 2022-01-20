258b60c81dacd761929885fc77eb5757c75ce710
Victor Turansky <victor.turansky@gmail.com>
2022-01-20 03:13:43 +0300

    Update declarations

--- typescript-kotlin/src/main/kotlin/typescript/System.kt
+++ typescript-kotlin/src/main/kotlin/typescript/System.kt
@@ -3,46 +3,54 @@
 package typescript
 
 external interface System {
-    /*
-    args: string[];
-    newLine: string;
-    useCaseSensitiveFileNames: boolean;
-    write(s: string): void;
-    writeOutputIsTTY?(): boolean;
-    getWidthOfTerminal?(): number;
-    readFile(path: string, encoding?: string): string | undefined;
-    getFileSize?(path: string): number;
-    writeFile(path: string, data: string, writeByteOrderMark?: boolean): void;
+    var args: ReadonlyArray<String>
+    var newLine: String
+    var useCaseSensitiveFileNames: Boolean
+    fun write(s: String)
+    val writeOutputIsTTY: (() -> Boolean)?
+    val getWidthOfTerminal: (() -> Double)?
+    fun readFile(path: String, encoding: String = definedExternally): String?
+    val getFileSize: ((path: String) -> Double)?
+    fun writeFile(path: String, data: String, writeByteOrderMark: Boolean = definedExternally)
+
     /**
      * @pollingInterval - this parameter is used in polling-based watchers and ignored in watchers that
      * use native OS file watching
      */
-    watchFile?(path: string, callback: FileWatcherCallback, pollingInterval?: number, options?: WatchOptions): FileWatcher;
-    watchDirectory?(path: string, callback: DirectoryWatcherCallback, recursive?: boolean, options?: WatchOptions): FileWatcher;
-    resolvePath(path: string): string;
-    fileExists(path: string): boolean;
-    directoryExists(path: string): boolean;
-    createDirectory(path: string): void;
-    getExecutingFilePath(): string;
-    getCurrentDirectory(): string;
-    getDirectories(path: string): string[];
-    readDirectory(path: string, extensions?: readonly string[], exclude?: readonly string[], include?: readonly string[], depth?: number): string[];
-    getModifiedTime?(path: string): Date | undefined;
-    setModifiedTime?(path: string, time: Date): void;
-    deleteFile?(path: string): void;
+    val watchFile: ((path: String, callback: FileWatcherCallback, pollingInterval: Double?, options: WatchOptions?) -> FileWatcher)?
+    val watchDirectory: ((path: String, callback: DirectoryWatcherCallback, recursive: Boolean?, options: WatchOptions?) -> FileWatcher)?
+    fun resolvePath(path: String): String
+    fun fileExists(path: String): Boolean
+    fun directoryExists(path: String): Boolean
+    fun createDirectory(path: String)
+    fun getExecutingFilePath(): String
+    fun getCurrentDirectory(): String
+    fun getDirectories(path: String): ReadonlyArray<String>
+    fun readDirectory(
+        path: String,
+        extensions: ReadonlyArray<String> = definedExternally,
+        exclude: ReadonlyArray<String> = definedExternally,
+        include: ReadonlyArray<String> = definedExternally,
+        depth: Double = definedExternally,
+    ): ReadonlyArray<String>
+
+    val getModifiedTime: ((path: String) -> kotlin.js.Date?)?
+    val setModifiedTime: ((path: String, time: kotlin.js.Date) -> Unit)?
+    val deleteFile: ((path: String) -> Unit)?
+
     /**
      * A good implementation is node.js' `crypto.createHash`. (https://nodejs.org/api/crypto.html#crypto_crypto_createhash_algorithm)
      */
-    createHash?(data: string): string;
+    val createHash: ((data: String) -> String)?
+
     /** This must be cryptographically secure. Only implement this method using `crypto.createHash("sha256")`. */
-    createSHA256Hash?(data: string): string;
-    getMemoryUsage?(): number;
-    exit(exitCode?: number): void;
-    realpath?(path: string): string;
-    setTimeout?(callback: (...args: any[]) => void, ms: number, ...args: any[]): any;
-    clearTimeout?(timeoutId: any): void;
-    clearScreen?(): void;
-    base64decode?(input: string): string;
-    base64encode?(input: string): string;
-    */
+    val createSHA256Hash: ((data: String) -> String)?
+    val getMemoryUsage: (() -> Double)?
+    fun exit(exitCode: Double = definedExternally)
+    val realpath: ((path: String) -> String)?
+    val setTimeout: ((callback: Function<Unit>, ms: Double, /* vararg */ args: dynamic) -> Any)?
+    val clearTimeout: ((timeoutId: Any) -> Unit)?
+    val clearScreen: (() -> Unit)?
+    val base64decode: ((input: String) -> String)?
+    val base64encode: ((input: String) -> String)?
 }
