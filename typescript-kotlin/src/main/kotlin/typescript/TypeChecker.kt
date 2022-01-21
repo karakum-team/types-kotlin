81b0c4fcdebdb03e1626d2360eae9ed669e35427
Victor Turansky <victor.turansky@gmail.com>
2022-01-22 02:46:59 +0300

    Update declarations

--- typescript-kotlin/src/main/kotlin/typescript/TypeChecker.kt
+++ typescript-kotlin/src/main/kotlin/typescript/TypeChecker.kt
@@ -3,50 +3,129 @@
 package typescript
 
 external interface TypeChecker {
-    /*
-    getTypeOfSymbolAtLocation(symbol: Symbol, node: Node): Type;
-    getDeclaredTypeOfSymbol(symbol: Symbol): Type;
-    getPropertiesOfType(type: Type): Symbol[];
-    getPropertyOfType(type: Type, propertyName: string): Symbol | undefined;
-    getPrivateIdentifierPropertyOfType(leftType: Type, name: string, location: Node): Symbol | undefined;
-    getIndexInfoOfType(type: Type, kind: IndexKind): IndexInfo | undefined;
-    getIndexInfosOfType(type: Type): readonly IndexInfo[];
-    getSignaturesOfType(type: Type, kind: SignatureKind): readonly Signature[];
-    getIndexTypeOfType(type: Type, kind: IndexKind): Type | undefined;
-    getBaseTypes(type: InterfaceType): BaseType[];
-    getBaseTypeOfLiteralType(type: Type): Type;
-    getWidenedType(type: Type): Type;
-    getReturnTypeOfSignature(signature: Signature): Type;
-    getNullableType(type: Type, flags: TypeFlags): Type;
-    getNonNullableType(type: Type): Type;
-    getTypeArguments(type: TypeReference): readonly Type[];
+    fun getTypeOfSymbolAtLocation(
+        symbol: Symbol,
+        node: Node,
+    ): Type
+
+    fun getDeclaredTypeOfSymbol(symbol: Symbol): Type
+    fun getPropertiesOfType(type: Type): ReadonlyArray<Symbol>
+    fun getPropertyOfType(
+        type: Type,
+        propertyName: String,
+    ): Symbol?
+
+    fun getPrivateIdentifierPropertyOfType(
+        leftType: Type,
+        name: String,
+        location: Node,
+    ): Symbol?
+
+    fun getIndexInfoOfType(
+        type: Type,
+        kind: IndexKind,
+    ): IndexInfo?
+
+    fun getIndexInfosOfType(type: Type): ReadonlyArray<IndexInfo>
+    fun getSignaturesOfType(
+        type: Type,
+        kind: SignatureKind,
+    ): ReadonlyArray<Signature>
+
+    fun getIndexTypeOfType(
+        type: Type,
+        kind: IndexKind,
+    ): Type?
+
+    fun getBaseTypes(type: InterfaceType): ReadonlyArray<BaseType>
+    fun getBaseTypeOfLiteralType(type: Type): Type
+    fun getWidenedType(type: Type): Type
+    fun getReturnTypeOfSignature(signature: Signature): Type
+    fun getNullableType(
+        type: Type,
+        flags: TypeFlags,
+    ): Type
+
+    fun getNonNullableType(type: Type): Type
+    fun getTypeArguments(type: TypeReference): ReadonlyArray<Type>
+
     /** Note that the resulting nodes cannot be checked. */
-    typeToTypeNode(type: Type, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): TypeNode | undefined;
+    fun typeToTypeNode(
+        type: Type,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): TypeNode?
+
     /** Note that the resulting nodes cannot be checked. */
-    signatureToSignatureDeclaration(signature: Signature, kind: SyntaxKind, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): SignatureDeclaration & {
-        typeArguments?: NodeArray<TypeNode>;
-    } | undefined;
+    fun signatureToSignatureDeclaration(
+        signature: Signature,
+        kind: SyntaxKind,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): SignatureDeclaration /* SignatureDeclaration & { typeArguments?: NodeArray<TypeNode>; } */?
+
     /** Note that the resulting nodes cannot be checked. */
-    indexInfoToIndexSignatureDeclaration(indexInfo: IndexInfo, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): IndexSignatureDeclaration | undefined;
+    fun indexInfoToIndexSignatureDeclaration(
+        indexInfo: IndexInfo,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): IndexSignatureDeclaration?
+
     /** Note that the resulting nodes cannot be checked. */
-    symbolToEntityName(symbol: Symbol, meaning: SymbolFlags, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): EntityName | undefined;
+    fun symbolToEntityName(
+        symbol: Symbol,
+        meaning: SymbolFlags,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): EntityName?
+
     /** Note that the resulting nodes cannot be checked. */
-    symbolToExpression(symbol: Symbol, meaning: SymbolFlags, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): Expression | undefined;
+    fun symbolToExpression(
+        symbol: Symbol,
+        meaning: SymbolFlags,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): Expression?
+
     /** Note that the resulting nodes cannot be checked. */
-    symbolToTypeParameterDeclarations(symbol: Symbol, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): NodeArray<TypeParameterDeclaration> | undefined;
+    fun symbolToTypeParameterDeclarations(
+        symbol: Symbol,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): NodeArray<TypeParameterDeclaration>?
+
     /** Note that the resulting nodes cannot be checked. */
-    symbolToParameterDeclaration(symbol: Symbol, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): ParameterDeclaration | undefined;
+    fun symbolToParameterDeclaration(
+        symbol: Symbol,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): ParameterDeclaration?
+
     /** Note that the resulting nodes cannot be checked. */
-    typeParameterToDeclaration(parameter: TypeParameter, enclosingDeclaration: Node | undefined, flags: NodeBuilderFlags | undefined): TypeParameterDeclaration | undefined;
-    getSymbolsInScope(location: Node, meaning: SymbolFlags): Symbol[];
-    getSymbolAtLocation(node: Node): Symbol | undefined;
-    getSymbolsOfParameterPropertyDeclaration(parameter: ParameterDeclaration, parameterName: string): Symbol[];
+    fun typeParameterToDeclaration(
+        parameter: TypeParameter,
+        enclosingDeclaration: Node?,
+        flags: NodeBuilderFlags?,
+    ): TypeParameterDeclaration?
+
+    fun getSymbolsInScope(
+        location: Node,
+        meaning: SymbolFlags,
+    ): ReadonlyArray<Symbol>
+
+    fun getSymbolAtLocation(node: Node): Symbol?
+    fun getSymbolsOfParameterPropertyDeclaration(
+        parameter: ParameterDeclaration,
+        parameterName: String,
+    ): ReadonlyArray<Symbol>
+
     /**
      * The function returns the value (local variable) symbol of an identifier in the short-hand property assignment.
      * This is necessary as an identifier in short-hand property assignment can contains two meaning: property name and property value.
      */
-    getShorthandAssignmentValueSymbol(location: Node | undefined): Symbol | undefined;
-    getExportSpecifierLocalTargetSymbol(location: ExportSpecifier | Identifier): Symbol | undefined;
+    fun getShorthandAssignmentValueSymbol(location: Node?): Symbol?
+    fun getExportSpecifierLocalTargetSymbol(location: dynamic /* ExportSpecifier | Identifier */): Symbol?
+
     /**
      * If a symbol is a local symbol with an associated exported symbol, returns the exported symbol.
      * Otherwise returns its input.
@@ -55,51 +134,95 @@ external interface TypeChecker {
      *     - But the result of `getSymbolsInScope` will contain the *local* symbol for `T`, not the exported symbol.
      *     - Calling `getExportSymbolOfSymbol` on that local symbol will return the exported symbol.
      */
-    getExportSymbolOfSymbol(symbol: Symbol): Symbol;
-    getPropertySymbolOfDestructuringAssignment(location: Identifier): Symbol | undefined;
-    getTypeOfAssignmentPattern(pattern: AssignmentPattern): Type;
-    getTypeAtLocation(node: Node): Type;
-    getTypeFromTypeNode(node: TypeNode): Type;
-    signatureToString(signature: Signature, enclosingDeclaration?: Node, flags?: TypeFormatFlags, kind?: SignatureKind): string;
-    typeToString(type: Type, enclosingDeclaration?: Node, flags?: TypeFormatFlags): string;
-    symbolToString(symbol: Symbol, enclosingDeclaration?: Node, meaning?: SymbolFlags, flags?: SymbolFormatFlags): string;
-    typePredicateToString(predicate: TypePredicate, enclosingDeclaration?: Node, flags?: TypeFormatFlags): string;
-    getFullyQualifiedName(symbol: Symbol): string;
-    getAugmentedPropertiesOfType(type: Type): Symbol[];
-    getRootSymbols(symbol: Symbol): readonly Symbol[];
-    getSymbolOfExpando(node: Node, allowDeclaration: boolean): Symbol | undefined;
-    getContextualType(node: Expression): Type | undefined;
+    fun getExportSymbolOfSymbol(symbol: Symbol): Symbol
+    fun getPropertySymbolOfDestructuringAssignment(location: Identifier): Symbol?
+    fun getTypeOfAssignmentPattern(pattern: AssignmentPattern): Type
+    fun getTypeAtLocation(node: Node): Type
+    fun getTypeFromTypeNode(node: TypeNode): Type
+    fun signatureToString(
+        signature: Signature,
+        enclosingDeclaration: Node = definedExternally,
+        flags: TypeFormatFlags = definedExternally,
+        kind: SignatureKind = definedExternally,
+    ): String
+
+    fun typeToString(
+        type: Type,
+        enclosingDeclaration: Node = definedExternally,
+        flags: TypeFormatFlags = definedExternally,
+    ): String
+
+    fun symbolToString(
+        symbol: Symbol,
+        enclosingDeclaration: Node = definedExternally,
+        meaning: SymbolFlags = definedExternally,
+        flags: SymbolFormatFlags = definedExternally,
+    ): String
+
+    fun typePredicateToString(
+        predicate: TypePredicate,
+        enclosingDeclaration: Node = definedExternally,
+        flags: TypeFormatFlags = definedExternally,
+    ): String
+
+    fun getFullyQualifiedName(symbol: Symbol): String
+    fun getAugmentedPropertiesOfType(type: Type): ReadonlyArray<Symbol>
+    fun getRootSymbols(symbol: Symbol): ReadonlyArray<Symbol>
+    fun getSymbolOfExpando(
+        node: Node,
+        allowDeclaration: Boolean,
+    ): Symbol?
+
+    fun getContextualType(node: Expression): Type?
+
     /**
      * returns unknownSignature in the case of an error.
      * returns undefined if the node is not valid.
      * @param argumentCount Apparent number of arguments, passed in case of a possibly incomplete call. This should come from an ArgumentListInfo. See `signatureHelp.ts`.
      */
-    getResolvedSignature(node: CallLikeExpression, candidatesOutArray?: Signature[], argumentCount?: number): Signature | undefined;
-    getSignatureFromDeclaration(declaration: SignatureDeclaration): Signature | undefined;
-    isImplementationOfOverload(node: SignatureDeclaration): boolean | undefined;
-    isUndefinedSymbol(symbol: Symbol): boolean;
-    isArgumentsSymbol(symbol: Symbol): boolean;
-    isUnknownSymbol(symbol: Symbol): boolean;
-    getConstantValue(node: EnumMember | PropertyAccessExpression | ElementAccessExpression): string | number | undefined;
-    isValidPropertyAccess(node: PropertyAccessExpression | QualifiedName | ImportTypeNode, propertyName: string): boolean;
+    fun getResolvedSignature(
+        node: CallLikeExpression,
+        candidatesOutArray: ReadonlyArray<Signature> = definedExternally,
+        argumentCount: Int = definedExternally,
+    ): Signature?
+
+    fun getSignatureFromDeclaration(declaration: SignatureDeclaration): Signature?
+    fun isImplementationOfOverload(node: SignatureDeclaration): Boolean?
+    fun isUndefinedSymbol(symbol: Symbol): Boolean
+    fun isArgumentsSymbol(symbol: Symbol): Boolean
+    fun isUnknownSymbol(symbol: Symbol): Boolean
+    fun getConstantValue(node: dynamic /* EnumMember | PropertyAccessExpression | ElementAccessExpression */): dynamic /* string | number */
+    fun isValidPropertyAccess(
+        node: dynamic, /* PropertyAccessExpression | QualifiedName | ImportTypeNode */
+        propertyName: String,
+    ): Boolean
+
     /** Follow all aliases to get the original symbol. */
-    getAliasedSymbol(symbol: Symbol): Symbol;
+    fun getAliasedSymbol(symbol: Symbol): Symbol
+
     /** Follow a *single* alias to get the immediately aliased symbol. */
-    getImmediateAliasedSymbol(symbol: Symbol): Symbol | undefined;
-    getExportsOfModule(moduleSymbol: Symbol): Symbol[];
-    getJsxIntrinsicTagNamesAt(location: Node): Symbol[];
-    isOptionalParameter(node: ParameterDeclaration): boolean;
-    getAmbientModules(): Symbol[];
-    tryGetMemberInModuleExports(memberName: string, moduleSymbol: Symbol): Symbol | undefined;
-    getApparentType(type: Type): Type;
-    getBaseConstraintOfType(type: Type): Type | undefined;
-    getDefaultFromTypeParameter(type: Type): Type | undefined;
-    getTypePredicateOfSignature(signature: Signature): TypePredicate | undefined;
+    fun getImmediateAliasedSymbol(symbol: Symbol): Symbol?
+    fun getExportsOfModule(moduleSymbol: Symbol): ReadonlyArray<Symbol>
+    fun getJsxIntrinsicTagNamesAt(location: Node): ReadonlyArray<Symbol>
+    fun isOptionalParameter(node: ParameterDeclaration): Boolean
+    fun getAmbientModules(): ReadonlyArray<Symbol>
+    fun tryGetMemberInModuleExports(
+        memberName: String,
+        moduleSymbol: Symbol,
+    ): Symbol?
+
+    fun getApparentType(type: Type): Type
+    fun getBaseConstraintOfType(type: Type): Type?
+    fun getDefaultFromTypeParameter(type: Type): Type?
+    fun getTypePredicateOfSignature(signature: Signature): TypePredicate?
+
     /**
      * Depending on the operation performed, it may be appropriate to throw away the checker
      * if the cancellation token is triggered. Typically, if it is used for error checking
      * and the operation is cancelled, then it should be discarded, otherwise it is safe to keep.
      */
-    runWithCancellationToken<T>(token: CancellationToken, cb: (checker: TypeChecker) => T): T;
-    */
+    fun <T> runWithCancellationToken(
+        token: CancellationToken,
+        cb: (checker: TypeChecker) -> T,
+    ): T
 }
