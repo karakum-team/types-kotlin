6c3b93bbcca173db75eb806ac3c4f3d868a0d983
Victor Turansky <victor.turansky@gmail.com>
2022-01-28 02:22:51 +0300

    Update declarations

---
+++ typescript-kotlin/src/main/kotlin/typescript/isPropertyAccessOrQualifiedName.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("typescript")
+@file:JsNonModule
+
+package typescript
+
+external fun isPropertyAccessOrQualifiedName(node: Node): Boolean /* node is PropertyAccessExpression | QualifiedName */
