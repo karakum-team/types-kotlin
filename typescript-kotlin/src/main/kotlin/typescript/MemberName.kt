682f5076df7f4ad1ccc0c12e5fb7b6ffbc4e4a9f
Victor Turansky <victor.turansky@gmail.com>
2022-01-30 04:58:02 +0300

    Update declarations

--- typescript-kotlin/src/main/kotlin/typescript/MemberName.kt
+++ typescript-kotlin/src/main/kotlin/typescript/MemberName.kt
@@ -2,4 +2,4 @@
 
 package typescript
 
-sealed external interface MemberName : PrimaryExpression /* Identifier | PrivateIdentifier */
+sealed external interface MemberName : PrimaryExpression, DeclarationName /* Identifier | PrivateIdentifier */
--- typescript-kotlin/src/main/kotlin/typescript/PropertyName.kt
+++ typescript-kotlin/src/main/kotlin/typescript/PropertyName.kt
@@ -2,4 +2,4 @@
 
 package typescript
 
-sealed external interface PropertyName : Node /* Identifier | StringLiteral | NumericLiteral | ComputedPropertyName | PrivateIdentifier */
+sealed external interface PropertyName : DeclarationName /* Identifier | StringLiteral | NumericLiteral | ComputedPropertyName | PrivateIdentifier */
