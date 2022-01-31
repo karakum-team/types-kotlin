4ea251dd3e9688c669f7225b8855b1fb2fad2942
Victor Turansky <victor.turansky@gmail.com>
2022-02-01 01:02:42 +0300

    WebRTC subproject

---
+++ webrtc-kotlin/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.github.turansky.kfc.library")
+    // `webrtc-declarations`
+}
+
+val webrtcTypesVersion = property("webrtc-types.version") as String
+
+dependencies {
+    implementation(npm("@types/webrtc", webrtcTypesVersion))
+}
