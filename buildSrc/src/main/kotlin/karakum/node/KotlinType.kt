a33a2e1e7007d3612a2abf959d8b7f6c3f04a2a7
Victor Turansky <victor.turansky@gmail.com>
2022-02-08 02:17:23 +0300

    Add kotlin type util

---
+++ buildSrc/src/main/kotlin/karakum/node/KotlinType.kt
@@ -0,0 +1,74 @@
+package karakum.node
+
+internal const val DYNAMIC = "dynamic"
+internal const val UNIT = "Unit"
+
+internal const val STRING = "String"
+
+private val STANDARD_TYPE_MAP = mapOf(
+    "any" to "Any",
+    "object" to "Any",
+    "{}" to "Any",
+
+    "boolean" to "Boolean",
+    "string" to STRING,
+
+    "never" to "Nothing",
+
+    "number" to "Number",
+
+    "void" to UNIT,
+    "null" to "Nothing?",
+    "undefined" to "Nothing?",
+
+    "Date" to "kotlin.js.Date",
+
+    "Event" to "org.w3c.dom.events.Event",
+    "MessageEvent" to "org.w3c.dom.MessageEvent",
+
+    "MediaStream" to "org.w3c.dom.mediacapture.MediaStream",
+    "MediaStreamTrack" to "org.w3c.dom.mediacapture.MediaStreamTrack",
+
+    "ArrayBuffer" to "org.khronos.webgl.ArrayBuffer",
+    "ArrayBufferView" to "org.khronos.webgl.ArrayBufferView",
+
+    "Blob" to "org.w3c.files.Blob",
+
+    "() => void" to "() -> Unit",
+)
+
+private val TYPED = setOf(
+    "ReadonlyArray",
+)
+
+internal fun kotlinType(
+    type: String,
+    name: String,
+): String {
+    STANDARD_TYPE_MAP[type]
+        ?.also { return it }
+
+    if (type.endsWith(" | undefined"))
+        return kotlinType(type.removeSuffix(" | undefined"), name)
+
+    if (type.endsWith(" | null"))
+        return kotlinType(type.removeSuffix(" | null"), name) + "?"
+
+    if (" | " in type)
+        return "$DYNAMIC /* $type */"
+
+    if (type.endsWith("[]"))
+        return "ReadonlyArray<${kotlinType(type.removeSuffix("[]"), name)}>"
+
+    for (typedType in TYPED) {
+        if (type.startsWith("$typedType<"))
+            return "$typedType<${kotlinType(type.removeSurrounding("$typedType<", ">"), name)}>"
+    }
+
+    if (type.startsWith("Promise<")) {
+        val parameter = kotlinType(type.removeSurrounding("Promise<", ">"), name)
+        return "kotlin.js.Promise<$parameter>"
+    }
+
+    return type
+}
