6679f2c94fa8143479763cb9b508501b4525074b
Victor Turansky <victor.turansky@gmail.com>
2022-02-07 03:27:25 +0300

    Initial member converter

---
+++ buildSrc/src/main/kotlin/karakum/webrtc/MembersConverter.kt
@@ -0,0 +1,128 @@
+package karakum.webrtc
+
+internal fun convertMembers(
+    source: String,
+): String {
+    if (source.isEmpty())
+        return ""
+
+    return source.trimIndent()
+        .splitToSequence("\n")
+        .filter { it.isNotEmpty() }
+        .map { convertMember(it) }
+        .joinToString("\n")
+}
+
+private fun convertMember(
+    source: String,
+): String {
+    val body = source.substringBefore(";")
+    val content = if (isProperty(body)) {
+        convertProperty(body)
+    } else {
+        convertMethod(body)
+    }
+
+    val comment = source.substringAfter("; // ")
+    return sequenceOf(content, comment)
+        .filter { it.isNotEmpty() }
+        .joinToString(" // ")
+}
+
+private fun isProperty(
+    source: String,
+): Boolean =
+    ("(" !in source) || (source.indexOf(":") < source.indexOf("("))
+
+private fun convertProperty(
+    source: String,
+): String {
+    val modifier = if (source.startsWith("readonly ")) "val" else "var"
+    val body = source.removePrefix("readonly ")
+
+    val name = body.substringBefore(": ").removeSuffix("?")
+    var type = kotlinType(body.substringAfter(": "), name)
+
+    if (body.startsWith("$name?"))
+        type = "$type?"
+
+    return "$modifier $name: $type"
+}
+
+internal fun convertMethod(
+    source: String,
+): String {
+    val name = source.substringBefore("(")
+        .substringBefore("<")
+        .removeSuffix("?")
+        .ifEmpty { "/* native */ invoke" }
+
+    val typeParameters = source.substringBefore("(")
+        .substringAfter("<", "")
+        .let { if (it.isNotEmpty()) "<$it" else "" }
+        .replace(" extends ", " : ")
+
+    val parametersSource = source
+        .substringAfter("(")
+        .substringBeforeLast("): ")
+
+    val optional = source.startsWith("$name?")
+    val parameters = when {
+        parametersSource == "action: (value: V, key: K) => void" || parametersSource == "action: (value: T, key: T) => void"
+        -> parametersSource.replace(" => void", " -> $UNIT")
+
+        parametersSource == "cb: (elem: T, key: string, mode: ModuleKind.CommonJS | ModuleKind.ESNext | undefined) => void"
+        -> "cb: (elem: T, key: String, mode: ResolutionMode?) -> Unit"
+
+        parametersSource.isNotEmpty()
+        -> parametersSource
+            .splitToSequence(", ")
+            .joinToString(",\n") {
+                convertParameter(it, optional)
+            }
+
+        else -> ""
+    }
+
+    val returnType = kotlinType(source.substringAfterLast("): "), name)
+
+    if (optional)
+        return "val $name: (($parameters) -> $returnType)?"
+
+    val isOperator = when (name) {
+        "get" -> parameters.count { it == ':' } == 1
+        "set" -> parameters.count { it == ':' } == 2
+        else -> false
+    }
+    val keyword = if (isOperator) "operator fun" else "fun"
+
+    val returnDeclaration = if (returnType != UNIT) {
+        ": $returnType"
+    } else ""
+
+    var result = "$keyword $typeParameters $name($parameters)$returnDeclaration"
+    if (name == "createToken" && typeParameters.isNotEmpty())
+        result = "    // TODO: restore after alias update\n    // $result"
+
+    return result
+}
+
+internal fun convertParameter(
+    source: String,
+    lambdaMode: Boolean,
+): String {
+    val name = source
+        .substringBefore(": ")
+        .removeSuffix("?")
+
+    val type = kotlinType(source.substringAfter(": "), name)
+    val declaration = if (source.startsWith("$name?:")) {
+        if (lambdaMode) {
+            "$type?" // TODO: check
+        } else {
+            "$type = definedExternally"
+        }
+    } else type
+
+    return "$name: $declaration"
+}
