71fd2bcbd32a565349282ede9502805242de4159
Victor Turansky <victor.turansky@gmail.com>
2022-02-07 03:48:21 +0300

    Add `extensions` dependency

--- webrtc-kotlin/build.gradle.kts
+++ webrtc-kotlin/build.gradle.kts
@@ -4,7 +4,11 @@ plugins {
 }
 
 val webrtcTypesVersion = property("webrtc-types.version") as String
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
 
 dependencies {
     implementation(npm("@types/webrtc", webrtcTypesVersion))
+
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions")
 }
