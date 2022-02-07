a24e1addbd81c109e45322dcd553b1a94beb7408
Victor Turansky <victor.turansky@gmail.com>
2022-02-07 04:53:23 +0300

    Convert unions

--- buildSrc/src/main/kotlin/karakum/webrtc/Converter.kt
+++ buildSrc/src/main/kotlin/karakum/webrtc/Converter.kt
@@ -1,5 +1,6 @@
 package karakum.webrtc
 
+import karakum.common.unionBody
 import java.io.File
 
 internal data class ConversionResult(
@@ -9,12 +10,22 @@ internal data class ConversionResult(
 
 internal fun convertDefinitions(
     definitionsFile: File,
-): Sequence<ConversionResult> =
-    definitionsFile.readText()
+): Sequence<ConversionResult> {
+    val interfaces = definitionsFile.readText()
         .splitToSequence("\ninterface ")
         .drop(1)
         .map { convertInterface(it) }
         .filter { it.name != "Window" }
+
+    val types = definitionsFile.readText()
+        .splitToSequence("\ntype ")
+        .drop(1)
+        .map { it.substringBefore(";\n") }
+        .filter { " | '" in it }
+        .map { convertType(it) }
+
+    return interfaces + types
+}
 
 private fun convertInterface(
     source: String,
@@ -34,5 +45,22 @@ private fun convertInterface(
     return ConversionResult(
         name = name,
         body = "external sealed interface $declaration {\n$body\n}",
+    )
+}
+
+private fun convertType(
+    source: String,
+): ConversionResult {
+    val (name, body) = source
+        .split(" =")
+
+    val values = body.removePrefix(" ")
+        .splitToSequence(" | ", "\n    | ")
+        .map { it.removeSurrounding("'") }
+        .toList()
+
+    return ConversionResult(
+        name = name,
+        body = unionBody(name, values),
     )
 }
