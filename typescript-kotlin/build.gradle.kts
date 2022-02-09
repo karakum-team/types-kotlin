e4f70c43127a8191fa0b12d435179b84f8edcc82
Victor Turansky <victor.turansky@gmail.com>
2022-02-09 21:25:32 +0300

    Add `kotlin-extensions` dependency

--- typescript-kotlin/build.gradle.kts
+++ typescript-kotlin/build.gradle.kts
@@ -9,6 +9,11 @@ kotlin {
     }
 }
 
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
 dependencies {
     implementation(npmv("typescript"))
+
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions")
 }
