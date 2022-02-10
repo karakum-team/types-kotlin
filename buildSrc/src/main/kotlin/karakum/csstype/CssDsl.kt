79f4b16a8d03005a2cbc1f75f4e60f81eef102c7
Victor Turansky <victor.turansky@gmail.com>
2022-02-11 00:25:35 +0300

    Update `CssDsl` annotation

--- buildSrc/src/main/kotlin/karakum/csstype/CssDsl.kt
+++ buildSrc/src/main/kotlin/karakum/csstype/CssDsl.kt
@@ -4,6 +4,7 @@ internal const val CSS_DSL = "CssDsl"
 
 private val CSS_DSL_BODY = """
 @DslMarker
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
 @Retention(AnnotationRetention.BINARY)
 annotation class $CSS_DSL
 """.trimIndent()
