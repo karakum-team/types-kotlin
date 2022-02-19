e0e22f2a94d119aebb1729c508ab319649faf1bf
Victor Turansky <victor.turansky@gmail.com>
2022-02-19 05:53:26 +0300

    Update declarations

--- csstype-kotlin/src/main/kotlin/csstype/NoneType.kt
+++ csstype-kotlin/src/main/kotlin/csstype/NoneType.kt
@@ -2,4 +2,28 @@
 
 package csstype
 
-sealed external interface NoneType
+sealed external interface NoneType :
+    AnimationName,
+    BackdropFilter,
+    BackgroundImage,
+    BorderImageSource,
+    BoxShadow,
+    CounterIncrement,
+    CounterReset,
+    CounterSet,
+    Filter,
+    Grid,
+    GridTemplate,
+    GridTemplateAreas,
+    LineClamp,
+    ListStyleImage,
+    ListStyleType,
+    MaskBorderSource,
+    MaskImage,
+    MaxLines,
+    Perspective,
+    Rotate,
+    Scale,
+    TextShadow,
+    Transform,
+    Translate
