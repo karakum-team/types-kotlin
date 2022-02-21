1eff605a5ed5465494b26b07bd28af41ebcd77c0
sergeigrishchenko <s.i.grishchenko@gmail.com>
2022-02-21 03:10:00 +0300

    KarakumConverterPlugin is temporarily disabled

--- karakum/karakum-gradle-plugin/src/main/kotlin/team/karakum/converter/gradle/plugin/KarakumConverterPlugin.kt
+++ karakum/karakum-gradle-plugin/src/main/kotlin/team/karakum/converter/gradle/plugin/KarakumConverterPlugin.kt
@@ -10,32 +10,32 @@ import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec
 
 class KarakumConverterPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        val jsResource = requireNotNull(KarakumConverterPlugin::class.java.getResource("/karakum-converter.js"))
-        val executableJs = project.layout.buildDirectory.file("karakum/karakum-converter.js").get().asFile
-
-        val kotlinJsTarget = project.kotlinExtension.targets.firstNotNullOf { it as? KotlinJsTarget }
-        val kotlinJsCompilation =
-            kotlinJsTarget.compilations.first { it.name == KotlinCompilation.MAIN_COMPILATION_NAME }
-
-        project.tasks.register("prepareExecutableJs") { task ->
-            task.group = KARAKUM_GRADLE_PLUGIN_GROUP
-
-            task.doLast {
-                executableJs.parentFile.mkdir()
-                executableJs.writeText(jsResource.readText())
-            }
-        }
-
-        NodeJsExec.create(kotlinJsCompilation, "convert") {
-            group = KARAKUM_GRADLE_PLUGIN_GROUP
-
-            dependsOn("prepareExecutableJs")
-
-            inputFileProperty.set(executableJs)
-
-            doLast {
-                println("Hello from plugin 'team.karakum.converter'")
-            }
-        }
+//        val jsResource = requireNotNull(KarakumConverterPlugin::class.java.getResource("/karakum-converter.js"))
+//        val executableJs = project.layout.buildDirectory.file("karakum/karakum-converter.js").get().asFile
+//
+//        val kotlinJsTarget = project.kotlinExtension.targets.firstNotNullOf { it as? KotlinJsTarget }
+//        val kotlinJsCompilation =
+//            kotlinJsTarget.compilations.first { it.name == KotlinCompilation.MAIN_COMPILATION_NAME }
+//
+//        project.tasks.register("prepareExecutableJs") { task ->
+//            task.group = KARAKUM_GRADLE_PLUGIN_GROUP
+//
+//            task.doLast {
+//                executableJs.parentFile.mkdir()
+//                executableJs.writeText(jsResource.readText())
+//            }
+//        }
+//
+//        NodeJsExec.create(kotlinJsCompilation, "convert") {
+//            group = KARAKUM_GRADLE_PLUGIN_GROUP
+//
+//            dependsOn("prepareExecutableJs")
+//
+//            inputFileProperty.set(executableJs)
+//
+//            doLast {
+//                println("Hello from plugin 'team.karakum.converter'")
+//            }
+//        }
     }
 }
