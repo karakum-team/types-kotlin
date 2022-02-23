613cb1ea61e467e4f843bfb80f8ecda0b76ca982
Victor Turansky <victor.turansky@gmail.com>
2022-02-24 01:13:56 +0300

    Update declarations

--- react-query-kotlin/src/main/kotlin/react/query/focusManager.core.kt
+++ react-query-kotlin/src/main/kotlin/react/query/focusManager.core.kt
@@ -3,11 +3,18 @@
 @file:JsModule("react-query")
 @file:JsNonModule
 
+@file:Suppress(
+    "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
+)
+
 package react.query
+
+typealias SetupFn = (setFocused: (focused: Boolean?) -> Unit) -> (() -> Unit)?
 
 open external class FocusManager : Subscribable<Listener> {
     override fun onSubscribe()
-    open fun setEventListener(setup: (setFocused: (focused: Boolean?) -> Unit) -> () -> Unit)
+    override fun onUnsubscribe()
+    open fun setEventListener(setup: SetupFn)
     open fun setFocused(focused: Boolean = definedExternally)
     open fun onFocus()
     open fun isFocused(): Boolean
