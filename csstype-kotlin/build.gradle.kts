0ff86856825afe6697d48b762c29f57ba6e5d6db
Victor Turansky <victor.turansky@gmail.com>
2022-03-07 02:00:07 +0300

    Add `kotlin-extensions` dependency

--- csstype-kotlin/build.gradle.kts
+++ csstype-kotlin/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
     `csstype-declarations`
 }
 
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
 dependencies {
     implementation(npmv("csstype"))
+
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions")
 }
