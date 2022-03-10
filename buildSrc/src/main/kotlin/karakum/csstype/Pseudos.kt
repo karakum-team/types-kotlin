db23067a98ac17515191e00c720fd994841253b9
Victor Turansky <victor.turansky@gmail.com>
2022-03-11 00:26:53 +0300

    Revert "More pseudos"
    This reverts commit aeae98c1724d601b3746e92b52fc55ffcc148dfe.

--- buildSrc/src/main/kotlin/karakum/csstype/Pseudos.kt
+++ buildSrc/src/main/kotlin/karakum/csstype/Pseudos.kt
@@ -25,24 +25,19 @@ internal fun convertPseudos(
 ): ConversionResult {
     val method: (String) -> String = when (name) {
         "SimplePseudos" -> { selector ->
-            val kotlinName = unionConstant(selector).kotlinName
             """
-            inline fun $kotlinName(
+            inline fun ${unionConstant(selector).kotlinName}(
                 block: T.() -> Unit,
             ) {
                 "$selector"(block)
             }
-            
-            inline val $SELECTOR.$kotlinName
-                get() = $SELECTOR("${'$'}{this}$selector")
             """.trimIndent()
         }
 
         "AdvancedPseudos" -> { selector ->
-            val kotlinName = unionConstant(selector).kotlinName
             sequenceOf(SELECTOR, "String").joinToString("\n\n") { type ->
                 """
-                inline fun $kotlinName(
+                inline fun ${unionConstant(selector).kotlinName}(
                     selector: $type,
                 ): $SELECTOR =
                     $SELECTOR("$selector(${'$'}selector)")
