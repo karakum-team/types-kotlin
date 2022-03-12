a5118d340bad20b251bff2e68be190f2394b09da
Victor Turansky <victor.turansky@gmail.com>
2022-03-12 13:47:04 +0300

    Update declarations

---
+++ csstype-kotlin/src/main/kotlin/csstype/ClassName.kt
@@ -0,0 +1,14 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+sealed external interface ClassName
+
+inline fun ClassName(
+    value: String,
+): ClassName =
+    value.unsafeCast<ClassName>()
