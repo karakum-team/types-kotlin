06acba29c90cebf44417551a45ce509d862e98d3
Victor Turansky <victor.turansky@gmail.com>
2022-04-14 00:32:27 +0400

    Add dependency

--- browser-kotlin/build.gradle.kts
+++ browser-kotlin/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("io.github.turansky.kfc.library")
     `browser-declarations`
 }
+
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
+dependencies {
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions")
+}
