664c4894938657cd7fe451143c66af2734b7a65a
Victor Turansky <victor.turansky@gmail.com>
2022-04-15 00:42:01 +0400

    Strict native events

--- buildSrc/src/main/kotlin/karakum/browser/EventTypeMap.kt
+++ buildSrc/src/main/kotlin/karakum/browser/EventTypeMap.kt
@@ -3,10 +3,7 @@ package karakum.browser
 internal val EVENT_TYPE_MAP = mapOf(
     "Event" to "org.w3c.dom.events.Event",
 
-    "AnimationEvent" to "org.w3c.dom.events.Event",
-
-    // TODO: update
-    "AnimationEvent" to "org.w3c.dom.events.Event",
+    "AnimationEvent" to "org.w3c.dom.events.AnimationEvent",
     "ClipboardEvent" to "org.w3c.dom.clipboard.ClipboardEvent",
     "CompositionEvent" to "org.w3c.dom.events.CompositionEvent",
     "DragEvent" to "org.w3c.dom.DragEvent",
@@ -15,9 +12,7 @@ internal val EVENT_TYPE_MAP = mapOf(
     "MouseEvent" to "org.w3c.dom.events.MouseEvent",
     "TouchEvent" to "org.w3c.dom.TouchEvent",
     "PointerEvent" to "org.w3c.dom.pointerevents.PointerEvent",
-
-    // TODO: update
-    "TransitionEvent" to "org.w3c.dom.events.Event",
+    "TransitionEvent" to "org.w3c.dom.events.TransitionEvent",
     "UIEvent" to "org.w3c.dom.events.UIEvent",
     "WheelEvent" to "org.w3c.dom.events.WheelEvent",
 
--- react-kotlin/build.gradle.kts
+++ react-kotlin/build.gradle.kts
@@ -10,5 +10,6 @@ dependencies {
     implementation(npm("@types/react", reactTypesVersion))
 
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-browser")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-core")
 }
