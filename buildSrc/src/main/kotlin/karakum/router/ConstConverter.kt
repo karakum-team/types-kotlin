3319f77b9c44f0de9d6dfd6fa947b4d0d7badada
Victor Turansky <victor.turansky@gmail.com>
2022-06-08 02:21:22 +0300

    Router. Fix context declarations

--- buildSrc/src/main/kotlin/karakum/router/ConstConverter.kt
+++ buildSrc/src/main/kotlin/karakum/router/ConstConverter.kt
@@ -4,10 +4,23 @@ internal fun convertConst(
     name: String,
     source: String,
 ): String {
-    if (name.endsWith("Context"))
-        return source
+    if (name.endsWith("Context")) {
+        val annotation = when (name) {
+            "NavigationContext",
+            "LocationContext",
+            "RouteContext",
+            -> "@JsName(\"UNSAFE_$name\")"
+
+            else -> null
+        }
+
+        val declaration = source
             .replace("const ", "external val ")
             .replace("React.", "react.")
+
+        return listOfNotNull(annotation, declaration)
+            .joinToString("\n")
+    }
 
     return "external val $name: react.FC<${name}Props>"
 }
