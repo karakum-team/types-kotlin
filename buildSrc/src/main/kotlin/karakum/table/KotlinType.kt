0c1effa7e1fec8602a8e4fe90d1b7eba90b6becd
Victor Turansky <victor.turansky@gmail.com>
2022-07-06 02:11:05 +0300

    React Table. Convert array handlers

--- buildSrc/src/main/kotlin/karakum/table/KotlinType.kt
+++ buildSrc/src/main/kotlin/karakum/table/KotlinType.kt
@@ -17,15 +17,26 @@ private val STANDARD_TYPE_MAP = mapOf(
     "string" to STRING,
 
     "number" to "Number",
+    "null | number" to "Number?",
 
     "void" to UNIT,
     "null" to "Nothing?",
 
+    "string[]" to "ReadonlyArray<String>",
+    "[string, number][]" to "ReadonlyArray<JsPair<String, Number>>",
+
     "() => boolean" to "() -> Boolean",
     "() => number" to "() -> Number",
 
+    "Column<TData>[]" to "ReadonlyArray<Column<TData>>",
+    "ColumnDef<TData>[]" to "ReadonlyArray<ColumnDef<TData>>",
+    "Header<TData>[]" to "ReadonlyArray<Header<TData>>",
+    "HeaderGroup<TData>[]" to "ReadonlyArray<HeaderGroup<TData>>",
+    "Row<TData>[]" to "ReadonlyArray<Row<TData>>",
     "() => Column<TData>[]" to "() -> ReadonlyArray<Column<TData>>",
     "() => Cell<TData>[]" to "() -> ReadonlyArray<Cell<TData>>",
+    "() => Header<TData>[]" to "() -> ReadonlyArray<Header<TData>>",
+    "() => HeaderGroup<TData>[]" to "() -> ReadonlyArray<HeaderGroup<TData>>",
 )
 
 internal fun kotlinType(
