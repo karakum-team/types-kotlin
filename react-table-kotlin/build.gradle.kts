088a635eaf83bb310bc6964f39cc075e21fc1cd6
Victor Turansky <victor.turansky@gmail.com>
2022-07-05 23:12:06 +0300

    React Table. Empty subproject

---
+++ react-table-kotlin/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("io.github.turansky.kfc.library")
+    id("io.github.turansky.seskar")
+    // `react-declarations`
+}
+
+val reactTableVersion = property("react-table.version") as String
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
+dependencies {
+    implementation(npm("@tanstack/react-table", reactTableVersion))
+
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-browser")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-core")
+}
