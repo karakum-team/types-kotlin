874ebfed0787472b448b942759d06e308a69db87
Victor Turansky <victor.turansky@gmail.com>
2022-07-06 01:37:31 +0300

    React Table. Initial kotlin type calculation

---
+++ buildSrc/src/main/kotlin/karakum/table/KotlinType.kt
@@ -0,0 +1,33 @@
+package karakum.table
+
+internal const val DYNAMIC = "dynamic"
+internal const val UNIT = "Unit"
+
+internal const val STRING = "String"
+
+internal const val INT = "Int"
+internal const val DOUBLE = "Double"
+
+private val STANDARD_TYPE_MAP = mapOf(
+    "any" to "Any",
+    "object" to "Any",
+
+    "boolean" to "Boolean",
+    "string" to STRING,
+
+    "void" to UNIT,
+    "null" to "Nothing?",
+)
+
+internal fun kotlinType(
+    type: String,
+    name: String,
+): String {
+    if (type.endsWith(" | undefined"))
+        return kotlinType(type.removeSuffix(" | undefined"), name)
+
+    STANDARD_TYPE_MAP[type]
+        ?.also { return it }
+
+    return type
+}
