1b6ff867e0538c9ce7b9c0ce69f464119534f83b
Victor Turansky <victor.turansky@gmail.com>
2022-07-06 21:39:16 +0300

    Update declarations

--- react-table-kotlin/src/main/kotlin/tanstack/table/core/CoreOptions.kt
+++ react-table-kotlin/src/main/kotlin/tanstack/table/core/CoreOptions.kt
@@ -6,7 +6,7 @@ import kotlinx.js.ReadonlyArray
 
 external interface CoreOptions<TData : RowData> {
     var data: ReadonlyArray<TData>
-    var state: Partial<TableState>
+    var state: TableState /* Partial */
     var onStateChange: (updater: Updater<TableState>) -> Unit
     var debugAll: Boolean?
     var debugTable: Boolean?
@@ -15,12 +15,12 @@ external interface CoreOptions<TData : RowData> {
     var debugRows: Boolean?
     var initialState: InitialTableState?
     var autoResetAll: Boolean?
-    var mergeOptions: ((defaultOptions: TableOptions<TData>, options: Partial<TableOptions<TData>>) -> TableOptions<TData>)?
+    var mergeOptions: ((defaultOptions: TableOptions<TData>, options: TableOptions<TData> /* Partial */) -> TableOptions<TData>)?
     var meta: Any?
     var getCoreRowModel: (table: Table<*>) -> () -> RowModel<*>
-    var getSubRows: ((originalRow: TData, index: Number) -> undefined | TData[])?
-    var getRowId: ((originalRow: TData, index: Number, parent?: Row<TData>) -> string)?
+    var getSubRows: ((originalRow: TData, index: Number) -> ReadonlyArray<TData>)?
+    var getRowId: ((originalRow: TData, index: Number, parent: Row<TData>?) -> String)?
     var columns: ReadonlyArray<ColumnDef<TData>>
-    var defaultColumn: Partial<ColumnDef<TData>>?
+    var defaultColumn: ColumnDef<TData> /* Partial */?
     var renderFallbackValue: Any
 }
