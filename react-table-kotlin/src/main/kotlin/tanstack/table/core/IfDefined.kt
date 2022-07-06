2460fdf774c0caa45a5531d836f94180ed892681
Victor Turansky <victor.turansky@gmail.com>
2022-07-06 05:02:38 +0300

    Update declarations

--- react-table-kotlin/src/main/kotlin/tanstack/table/core/IfDefined.kt
+++
@@ -1,5 +0,0 @@
-// Automatically generated - do not modify!
-
-package tanstack.table.core
-
-typealias IfDefined<T, N> = 0 : 1 & T ? N : T : {} ? T : N
--- react-table-kotlin/src/main/kotlin/tanstack/table/core/memo.kt
+++
@@ -1,9 +0,0 @@
-// Automatically generated - do not modify!
-
-package tanstack.table.core
-
-external fun <TDeps : readonly any[], TResult> memo(getDeps: () -> [...TDeps], fn: (...args: NoInfer<[...TDeps]>) -> TResult, opts: {
-    key: any
-    debug ?: () -> any
-    onChange ?: (result: TResult) -> Unit
-}): () -> TResult
