90865b4db5bf59ffe07fa1250dfa31a2b44a4e5e
Victor Turansky <victor.turansky@gmail.com>
2022-07-07 03:29:00 +0300

    React Virtual. Subproject

---
+++ react-virtual-kotlin/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("io.github.turansky.kfc.library")
+    id("io.github.turansky.seskar")
+    // `react-virtual-declarations`
+}
+
+val reactTableVersion = property("react-virtual.version") as String
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
+dependencies {
+    implementation(npm("@tanstack/react-virtual", reactTableVersion))
+
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-browser")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-core")
+}
