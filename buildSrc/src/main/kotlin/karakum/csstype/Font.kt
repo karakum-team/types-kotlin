f3322b8d738b2f58cc2be9f3ae8e93e33737a2e9
Victor Turansky <victor.turansky@gmail.com>
2022-08-04 04:03:49 +0300

    `Font` factories

--- buildSrc/src/main/kotlin/karakum/csstype/Font.kt
+++ buildSrc/src/main/kotlin/karakum/csstype/Font.kt
@@ -2,6 +2,22 @@ package karakum.csstype
 
 internal const val FONT = "Font"
 
+private val COMBINATIONS = listOf(
+    listOf(0),
+    listOf(1),
+    listOf(2),
+    listOf(3),
+    listOf(0, 1),
+    listOf(0, 2),
+    listOf(0, 3),
+    listOf(1, 2),
+    listOf(1, 3),
+    listOf(0, 1, 2),
+    listOf(0, 1, 3),
+    listOf(1, 2, 3),
+    listOf(0, 1, 2, 3),
+)
+
 private val PARAMETERS = listOf(
     "style" to "FontStyle",
     "variant" to "FontVariant",
@@ -13,8 +29,14 @@ private val PARAMETERS = listOf(
 )
 
 internal fun fontFactories(): String =
-    sequenceOf(
-        PARAMETERS.toTypedArray(),
-    ).joinToString("\n\n") { parameters ->
-        factory(FONT, parameters)
-    }
+    COMBINATIONS.asSequence()
+        .flatMap {
+            sequenceOf(
+                it + listOf(4, 6),
+                it + listOf(4, 5, 6),
+            )
+        }
+        .map { it.map(PARAMETERS::get).toTypedArray() }
+        .joinToString("\n\n") { parameters ->
+            factory(FONT, parameters)
+        }
