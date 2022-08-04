c350947f9affde0c73543e2d7e998ae8ae739efc
Victor Turansky <victor.turansky@gmail.com>
2022-08-04 17:25:28 +0300

    Update declarations

--- csstype-kotlin/src/main/kotlin/csstype/Font.kt
+++ csstype-kotlin/src/main/kotlin/csstype/Font.kt
@@ -23,6 +23,218 @@ sealed external interface Font {
 
 inline fun Font(
     style: FontStyle,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$style $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$variant $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$variant $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    weight: FontWeight,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$weight $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    weight: FontWeight,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$weight $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    stretch: FontStretch,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$stretch $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    stretch: FontStretch,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$stretch $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $variant $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$style $variant $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    weight: FontWeight,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $weight $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    weight: FontWeight,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$style $weight $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    stretch: FontStretch,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $stretch $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    stretch: FontStretch,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$style $stretch $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    weight: FontWeight,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$variant $weight $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    weight: FontWeight,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$variant $weight $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    stretch: FontStretch,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$variant $stretch $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    stretch: FontStretch,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$variant $stretch $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    weight: FontWeight,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $variant $weight $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    weight: FontWeight,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$style $variant $weight $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    stretch: FontStretch,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $variant $stretch $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    stretch: FontStretch,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$style $variant $stretch $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    weight: FontWeight,
+    stretch: FontStretch,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$variant $weight $stretch $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    variant: FontVariant,
+    weight: FontWeight,
+    stretch: FontStretch,
+    size: FontSize,
+    lineHeight: LineHeight,
+    family: FontFamily,
+): Font =
+    "$variant $weight $stretch $size/$lineHeight $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
+    variant: FontVariant,
+    weight: FontWeight,
+    stretch: FontStretch,
+    size: FontSize,
+    family: FontFamily,
+): Font =
+    "$style $variant $weight $stretch $size $family".unsafeCast<Font>()
+
+inline fun Font(
+    style: FontStyle,
     variant: FontVariant,
     weight: FontWeight,
     stretch: FontStretch,
