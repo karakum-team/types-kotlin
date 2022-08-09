95cbe634d151f4a61721d754b6cc23e01161b34e
Victor Turansky <victor.turansky@gmail.com>
2022-08-09 19:05:29 +0300

    Common `Package` logic

--- buildSrc/src/main/kotlin/karakum/node/Package.kt
+++ buildSrc/src/main/kotlin/karakum/node/Package.kt
@@ -3,8 +3,10 @@ package karakum.node
 internal data class Package(
     val name: String,
 ) {
-    val id: String = "node:$name"
-    val pkg: String = "package node.${name.removeSuffix("/promises")}"
+    private val root = name.substringBefore("/")
 
-    val path: String = "node/${name.removeSuffix("/promises")}"
+    val id: String = "node:$name"
+    val pkg: String = "package node.$root"
+
+    val path: String = "node/$root"
 }
