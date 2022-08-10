f2cdfd455edf4d025889b0b861c09f11c8ee9d36
Victor Turansky <victor.turansky@gmail.com>
2022-08-11 00:41:41 +0300

    Update declarations

---
+++ node-kotlin/src/main/kotlin/node/os/NetworkInterfaceInfoIPv6.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package node.os
+
+sealed external interface NetworkInterfaceInfoIPv6 : NetworkInterfaceBase {
+    var family: 'IPv6'
+    var scopeid: Number
+}
