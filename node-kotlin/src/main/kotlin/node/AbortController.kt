ed666e6ce44750dcc3e947b0f434723e44db7ac5
Victor Turansky <victor.turansky@gmail.com>
2022-08-11 22:34:30 +0300

    Update declarations

---
+++ node-kotlin/src/main/kotlin/node/AbortController.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package node
+
+external class AbortController : kotlinx.js.AbortController {
+    override val signal: AbortSignal
+    override fun abort()
+}
---
+++ node-kotlin/src/main/kotlin/node/AbortSignal.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package node
+
+external class AbortSignal : kotlinx.js.AbortSignal {
+    override val aborted: Boolean
+    override var onabort: Function<Unit>?
+}
