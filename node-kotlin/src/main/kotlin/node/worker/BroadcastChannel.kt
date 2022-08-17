e0d1a8975dfdd73d4cd6edc643ce0386c76bf1ba
Victor Turansky <victor.turansky@gmail.com>
2022-08-17 13:27:02 +0300

    Update declarations

---
+++ node-kotlin/src/main/kotlin/node/worker/BroadcastChannel.kt
@@ -0,0 +1,39 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("node:worker_threads")
+@file:JsNonModule
+
+package node.worker
+
+sealed external interface BroadcastChannel : NodeJS.RefCounted
+
+
+external class BroadcastChannel {
+    val name: String
+
+    /**
+     * Invoked with a single \`MessageEvent\` argument when a message is received.
+     * @since v15.4.0
+     */
+    var onmessage: (message: Any?) -> Unit
+
+    /**
+     * Invoked with a received message cannot be deserialized.
+     * @since v15.4.0
+     */
+    var onmessageerror: (message: Any?) -> Unit
+
+    constructor(name: String)
+
+    /**
+     * Closes the `BroadcastChannel` connection.
+     * @since v15.4.0
+     */
+    fun close()
+
+    /**
+     * @since v15.4.0
+     * @param message Any cloneable JavaScript value.
+     */
+    fun postMessage(message: Any?)
+}
