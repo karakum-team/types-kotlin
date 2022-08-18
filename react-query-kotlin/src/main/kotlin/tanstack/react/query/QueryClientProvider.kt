bb8fe55cfffbae23522d357836314e969fa03a74
Victor Turansky <victor.turansky@gmail.com>
2022-08-18 16:39:12 +0300

    Update declarations

--- react-query-kotlin/src/main/kotlin/tanstack/react/query/QueryClientProvider.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/react/query/QueryClientProvider.kt
@@ -3,25 +3,16 @@
 @file:JsModule("react-query")
 @file:JsNonModule
 
-@file:Suppress(
-    "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
-)
-
 package tanstack.react.query
 
 import tanstack.query.core.QueryClient
-import tanstack.query.core.Union
 
 external val defaultContext: react.Context<QueryClient?>
 
 external val useQueryClient: (options: ContextOptions?) -> QueryClient
 
-typealias QueryClientProviderPropsBase = Any
-
-typealias QueryClientProviderPropsWithContext = Any
-
-typealias QueryClientProviderPropsWithContextSharing = Any
-
-typealias QueryClientProviderProps = Union /* QueryClientProviderPropsWithContext | QueryClientProviderPropsWithContextSharing */
+external interface QueryClientProviderProps : react.PropsWithChildren {
+    var client: QueryClient
+}
 
 external val QueryClientProvider: react.FC<QueryClientProviderProps>
