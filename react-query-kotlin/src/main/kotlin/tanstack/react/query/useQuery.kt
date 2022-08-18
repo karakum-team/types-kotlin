1b13c85339a862fdf05720d1ee1e0d59bd13cad0
Victor Turansky <victor.turansky@gmail.com>
2022-08-18 12:41:29 +0300

    Update declarations

--- react-query-kotlin/src/main/kotlin/tanstack/react/query/useQuery.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/react/query/useQuery.kt
@@ -8,38 +8,12 @@ package tanstack.react.query
 import tanstack.query.core.QueryFunction
 import tanstack.query.core.QueryKey
 
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>): dynamic
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>): dynamic
-
 external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>): UseQueryResult<TData, TError>
 
 external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(
     queryKey: TQueryKey,
     options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey> = definedExternally,
-): dynamic
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(
-    queryKey: TQueryKey,
-    options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey> = definedExternally,
-): dynamic
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(
-    queryKey: TQueryKey,
-    options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey> = definedExternally,
 ): UseQueryResult<TData, TError>
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(
-    queryKey: TQueryKey,
-    queryFn: QueryFunction<TQueryFnData, TQueryKey>,
-    options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey> = definedExternally,
-): dynamic
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(
-    queryKey: TQueryKey,
-    queryFn: QueryFunction<TQueryFnData, TQueryKey>,
-    options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey> = definedExternally,
-): dynamic
 
 external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey> useQuery(
     queryKey: TQueryKey,
