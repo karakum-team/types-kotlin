d497d52af711c0019470b6765a8cc2330a68e44f
Victor Turansky <victor.turansky@gmail.com>
2022-08-18 13:39:28 +0300

    Update declarations

--- react-query-kotlin/src/main/kotlin/tanstack/react/query/useIsFetching.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/react/query/useIsFetching.kt
@@ -8,15 +8,15 @@ package tanstack.react.query
 import tanstack.query.core.QueryFilters
 import tanstack.query.core.QueryKey
 
-external interface Options : ContextOptions
+external interface UseIsFetchingOptions : ContextOptions
 
 external fun useIsFetching(
     filters: QueryFilters = definedExternally,
-    options: Options = definedExternally,
+    options: UseIsFetchingOptions = definedExternally,
 ): Int
 
 external fun useIsFetching(
     queryKey: QueryKey = definedExternally,
     filters: QueryFilters = definedExternally,
-    options: Options = definedExternally,
+    options: UseIsFetchingOptions = definedExternally,
 ): Int
--- react-query-kotlin/src/main/kotlin/tanstack/react/query/useIsMutating.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/react/query/useIsMutating.kt
@@ -8,15 +8,15 @@ package tanstack.react.query
 import tanstack.query.core.MutationFilters
 import tanstack.query.core.MutationKey
 
-external interface Options : ContextOptions
+external interface UseIsMutatingOptions : ContextOptions
 
 external fun useIsMutating(
     filters: MutationFilters = definedExternally,
-    options: Options = definedExternally,
+    options: UseIsMutatingOptions = definedExternally,
 ): Int
 
 external fun useIsMutating(
     mutationKey: MutationKey = definedExternally,
     filters: MutationFilters = definedExternally,
-    options: Options = definedExternally,
+    options: UseIsMutatingOptions = definedExternally,
 ): Int
