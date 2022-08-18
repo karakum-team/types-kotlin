4e6d27c94cffbd98d7df897f6383d43bd43dea7b
Victor Turansky <victor.turansky@gmail.com>
2022-08-18 16:58:42 +0300

    Update declarations

--- react-query-kotlin/src/main/kotlin/tanstack/query/core/queriesObserver.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/query/core/queriesObserver.kt
@@ -1,6 +1,6 @@
 // Automatically generated - do not modify!
 
-@file:JsModule("react-query")
+@file:JsModule("@tanstack/query-core")
 @file:JsNonModule
 
 @file:Suppress(
--- react-query-kotlin/src/main/kotlin/tanstack/react/query/useQueries.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/react/query/useQueries.kt
@@ -1,6 +1,6 @@
 // Automatically generated - do not modify!
 
-@file:JsModule("react-query")
+@file:JsModule("@tanstack/react-query")
 @file:JsNonModule
 
 @file:Suppress(
