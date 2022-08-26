13fe996c34f625806768a2a515dcef948cf2881c
Victor Turansky <victor.turansky@gmail.com>
2022-08-26 03:17:49 +0300

    Update declarations

--- typescript-kotlin/src/main/kotlin/typescript/PropertySignature.kt
+++ typescript-kotlin/src/main/kotlin/typescript/PropertySignature.kt
@@ -4,7 +4,7 @@ package typescript
 
 sealed external interface PropertySignature : TypeElement, JSDocContainer, Union.PropertySignature_ {
     override val kind: SyntaxKind.PropertySignature
-    val modifiers: NodeArray<Modifier>?
+    override val modifiers: NodeArray<Modifier>?
     override val name: PropertyName
     override val questionToken: QuestionToken?
     val type: TypeNode?
