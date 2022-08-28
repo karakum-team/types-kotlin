ca98cc802ac0b24bfff2ecdbca7c66b73ae4809a
Victor Turansky <victor.turansky@gmail.com>
2022-08-28 18:53:12 +0300

    Initial interface conversion

--- buildSrc/src/main/kotlin/karakum/browser/WebGL.kt
+++ buildSrc/src/main/kotlin/karakum/browser/WebGL.kt
@@ -5,22 +5,48 @@ import java.io.File
 internal fun webglDeclarations(
     definitionsFile: File,
 ): Sequence<ConversionResult> =
-    Regex("""interface EXT_.+? \{[\s\S]+?\}""")
+    Regex("""interface (EXT|OES|WEBGL)_.+? \{[\s\S]+?\}""")
         .findAll(definitionsFile.readText())
         .map { it.value }
-        .map { source ->
-            val name = source
-                .substringAfter(" ")
-                .substringBefore(" ")
-
-            val body = "sealed external " + source
-                .replace("readonly ", "val ")
-                .replace(";", "")
-
-            ConversionResult(
-                name = name,
-                body = body,
-            )
-        }
+        .map { convertInterface(it) }
         .plus(Aliases())
         .plus(Numbers())
+
+private fun convertInterface(
+    source: String,
+): ConversionResult {
+    val name = source
+        .substringAfter(" ")
+        .substringBefore(" ")
+
+    val declaration = source.substringBefore(" {\n")
+
+    val memberSource = source
+        .substringAfter(" {\n")
+        .removeSuffix("}")
+        .removeSuffix(";\n")
+        .trimIndent()
+
+    val members = if (memberSource.isNotEmpty()) {
+        memberSource
+            .splitToSequence(";\n")
+            .joinToString("\n") { convertMember(it) }
+    } else ""
+
+    val body = "sealed external $declaration {\n$members\n}"
+
+    return ConversionResult(
+        name = name,
+        body = body,
+    )
+}
+
+private fun convertMember(
+    source: String,
+): String {
+    if ("(" !in source)
+        return source.replace("readonly ", "val ")
+
+    return "fun " + source
+        .replace(" | null", "?")
+}
