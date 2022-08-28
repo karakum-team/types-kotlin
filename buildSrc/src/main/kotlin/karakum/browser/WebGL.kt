9930d4fb17e938891e03e2a8a0dc8b823aaaaa5a
Victor Turansky <victor.turansky@gmail.com>
2022-08-29 00:26:15 +0300

    WebGL classes

--- buildSrc/src/main/kotlin/karakum/browser/WebGL.kt
+++ buildSrc/src/main/kotlin/karakum/browser/WebGL.kt
@@ -4,15 +4,25 @@ import java.io.File
 
 internal fun webglDeclarations(
     definitionsFile: File,
-): Sequence<ConversionResult> =
-    Regex("""interface ((EXT_|OES_|WEBGL_|WebGL).+?) \{[\s\S]+?\}""")
-        .findAll(definitionsFile.readText())
+): Sequence<ConversionResult> {
+    val content = definitionsFile.readText()
+
+    val interfaces = Regex("""interface ((EXT_|OES_|WEBGL_|WebGL).+?) \{[\s\S]+?\}""")
+        .findAll(content)
         .map { it.value }
         .mapNotNull { convertInterface(it) }
+
+    val classes = Regex("""declare var WebGL.+?: \{[\s\S]+?\}""")
+        .findAll(content)
+        .map { it.value }
+        .mapNotNull { convertCompanion(it) }
+
+    return merge(interfaces + classes)
         .plus(Aliases())
         .plus(BufferSource())
         .plus(Lists())
         .plus(Numbers())
+}
 
 private fun convertInterface(
     source: String,
@@ -40,6 +50,38 @@ private fun convertInterface(
     } else ""
 
     val body = "sealed external $declaration {\n$members\n}"
+
+    return ConversionResult(
+        name = name,
+        body = body,
+    )
+}
+
+private fun convertCompanion(
+    source: String,
+): ConversionResult? {
+    val name = source
+        .removePrefix("declare var ")
+        .substringBefore(": ")
+
+    if (name in OLD_WEBGL_TYPES)
+        return null
+
+    val memberSource = source
+        .substringAfter(" {\n")
+        .removeSuffix("}")
+        .removeSuffix(";\n")
+        .trimIndent()
+        .splitToSequence(";\n")
+        .minus("prototype: $name")
+        .minus("new(): $name")
+        .joinToString("\n")
+
+    val body = if (memberSource.isNotEmpty()) {
+        "companion object {\n" +
+                memberSource.replace("readonly ", "val ") +
+                "\n}"
+    } else ""
 
     return ConversionResult(
         name = name,
@@ -107,3 +149,28 @@ private fun convertFunction(
 
     return "fun $name($params)$result"
 }
+
+private fun merge(
+    source: Sequence<ConversionResult>,
+): Sequence<ConversionResult> =
+    source.groupBy { it.name }
+        .values
+        .asSequence()
+        .map { items ->
+            when (items.size) {
+                1 -> items.single()
+                2 -> {
+                    val (ib, cb) = items.map { it.body }
+                    var body = ib.replaceFirst(" interface ", " class ")
+                    if (cb.isNotEmpty())
+                        body = body.replaceFirst("\n}", "\n$cb\n}")
+
+                    ConversionResult(
+                        name = items.first().name,
+                        body = body,
+                    )
+                }
+
+                else -> TODO()
+            }
+        }
