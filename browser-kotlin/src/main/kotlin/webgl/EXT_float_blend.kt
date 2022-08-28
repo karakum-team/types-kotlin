9b1ecaeab5b25060b762df68cfeea6258ce6b604
Victor Turansky <victor.turansky@gmail.com>
2022-08-28 18:16:30 +0300

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/webgl/EXT_float_blend.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package webgl
+
+sealed external interface EXT_float_blend
+
+/** The EXT_frag_depth extension is part of the WebGL API and enables to set a depth value of a fragment from within the fragment shader. */
+interface EXT_frag_depth
