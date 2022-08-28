761c6d4265595bcb4348c8df55166ce0972436d9
Victor Turansky <victor.turansky@gmail.com>
2022-08-28 18:53:38 +0300

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/webgl/WEBGL_lose_context.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package webgl
+
+sealed external interface WEBGL_lose_context {
+    fun loseContext(): void
+    fun restoreContext(): void
+}
---
+++ browser-kotlin/src/main/kotlin/webgl/WEBGL_multi_draw.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package webgl
+
+sealed external interface WEBGL_multi_draw {
+    fun multiDrawArraysInstancedWEBGL(
+        mode: GLenum,
+        firstsList: Int32Array | GLint[], firstsOffset: GLuint, countsList: Int32Array | GLsizei[], countsOffset: GLuint, instanceCountsList: Int32Array | GLsizei[], instanceCountsOffset: GLuint, drawcount: GLsizei): void
+    fun multiDrawArraysWEBGL(
+        mode: GLenum,
+        firstsList: Int32Array | GLint[], firstsOffset: GLuint, countsList: Int32Array | GLsizei[], countsOffset: GLuint, drawcount: GLsizei): void
+    fun multiDrawElementsInstancedWEBGL(
+        mode: GLenum,
+        countsList: Int32Array | GLint[], countsOffset: GLuint, type: GLenum, offsetsList: Int32Array | GLsizei[], offsetsOffset: GLuint, instanceCountsList: Int32Array | GLsizei[], instanceCountsOffset: GLuint, drawcount: GLsizei): void
+    fun multiDrawElementsWEBGL(
+        mode: GLenum,
+        countsList: Int32Array | GLint[], countsOffset: GLuint, type: GLenum, offsetsList: Int32Array | GLsizei[], offsetsOffset: GLuint, drawcount: GLsizei): void
+}
