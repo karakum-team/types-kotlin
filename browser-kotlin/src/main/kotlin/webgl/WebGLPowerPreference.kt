56a87dddaab1b24470e92cf67b74c6747fab2cbf
Victor Turansky <victor.turansky@gmail.com>
2022-08-29 23:01:43 +0300

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/webgl/WebGLPowerPreference.kt
@@ -0,0 +1,13 @@
+// Automatically generated - do not modify!
+
+package webgl
+
+// language=JavaScript
+@JsName("""(/*union*/{default: 'default', highPerformance: 'high-performance', lowPower: 'low-power'}/*union*/)""")
+external enum class WebGLPowerPreference {
+    default,
+    highPerformance,
+    lowPower,
+
+    ;
+}
