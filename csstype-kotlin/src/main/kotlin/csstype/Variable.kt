66aa980a3144e2d1f44ff59b2385912ed42da23d
Victor Turansky <victor.turansky@gmail.com>
2022-09-14 03:31:18 +0300

    Update declarations

---
+++ csstype-kotlin/src/main/kotlin/csstype/Variable.kt
@@ -0,0 +1,14 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+sealed external interface Variable
+
+inline fun Variable(
+    name: String,
+): Variable =
+    name.unsafeCast<Variable>()
