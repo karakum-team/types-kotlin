6ae4d90a8ec318cf30f44c6b2fd53feed536c08d
Victor Turansky <victor.turansky@gmail.com>
2022-09-18 15:03:06 +0300

    Update declarations

---
+++ csstype-kotlin/src/main/kotlin/csstype/At.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package csstype
+
+// language=JavaScript
+@JsName("""(/*union*/{at: 'at'}/*union*/)""")
+external enum class At {
+    at,
+
+    ;
+}
