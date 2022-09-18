875e5053dafd0c1c2307a4549997a9c18a83b749
Victor Turansky <victor.turansky@gmail.com>
2022-09-18 17:09:51 +0300

    Update declarations

---
+++ csstype-kotlin/src/main/kotlin/csstype/GeometryPosition.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package csstype
+
+// language=JavaScript
+@JsName("""(/*union*/{bottom: 'bottom', center: 'center', left: 'left', right: 'right', top: 'top'}/*union*/)""")
+external enum class GeometryPosition {
+    bottom,
+    center,
+    left,
+    right,
+    top,
+
+    ;
+}
