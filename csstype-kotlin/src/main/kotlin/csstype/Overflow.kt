09252a67c3b486898bcfbe7c61940eff645d9619
Arjen Keller <81708198+ArjenKellerICS@users.noreply.github.com>
2022-09-19 17:26:10 +0200

    Update Overflow.kt
    added overlay, inherit and auto

--- csstype-kotlin/src/main/kotlin/csstype/Overflow.kt
+++ csstype-kotlin/src/main/kotlin/csstype/Overflow.kt
@@ -8,12 +8,15 @@
 package csstype
 
 // language=JavaScript
-@JsName("""(/*union*/{clip: 'clip', hidden: 'hidden', scroll: 'scroll', visible: 'visible'}/*union*/)""")
+@JsName("""(/*union*/{clip: 'clip', hidden: 'hidden', scroll: 'scroll', visible: 'visible', overlay: 'overlay', inherit: 'inherit', auto: 'auto'}/*union*/)""")
 sealed external interface Overflow {
     companion object {
         val clip: Overflow
         val hidden: Overflow
         val scroll: Overflow
         val visible: Overflow
+        val overlay: Overflow
+        val inherit: Overflow
+        val auto: Overflow
     }
 }
