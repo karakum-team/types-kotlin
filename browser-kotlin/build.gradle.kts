3bc3675bc9b2b1eb844b52e58b736becfcc204e1
Victor Turansky <victor.turansky@gmail.com>
2022-09-25 19:09:24 +0300

    Remove `webrtc` subproject

--- browser-kotlin/build.gradle.kts
+++ browser-kotlin/build.gradle.kts
@@ -4,8 +4,11 @@ plugins {
     `browser-declarations`
 }
 
+val webrtcTypesVersion = property("webrtc-types.version") as String
+
 dependencies {
     implementation(npmv("typescript"))
+    implementation(npm("@types/webrtc", webrtcTypesVersion))
 
     implementation(enforcedPlatform(kotlinwBom()))
     implementation(kotlinw("js"))
