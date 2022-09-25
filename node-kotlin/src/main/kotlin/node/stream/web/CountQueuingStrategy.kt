dc9bd3a0169c600e4cadc342bf1712ce5b483246
Victor Turansky <victor.turansky@gmail.com>
2022-09-26 00:20:16 +0300

    Update declarations

--- node-kotlin/src/main/kotlin/node/stream/web/CountQueuingStrategy.kt
+++
@@ -1,8 +0,0 @@
-// Automatically generated - do not modify!
-
-package node.stream.web
-
-sealed external interface CountQueuingStrategy : QueuingStrategy {
-    val highWaterMark: Number
-    val size: QueuingStrategySize
-}
