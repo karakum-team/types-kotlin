97cad1e4ae08a57217fcd2571f7b5cc9a30c6bfc
Victor Turansky <victor.turansky@gmail.com>
2022-09-28 00:04:36 +0300

    Remove `@types/webrtc` dependency

--- browser-kotlin/build.gradle.kts
+++ browser-kotlin/build.gradle.kts
@@ -4,11 +4,8 @@ plugins {
     `browser-declarations`
 }
 
-val webrtcTypesVersion = property("webrtc-types.version") as String
-
 dependencies {
     implementation(npmv("typescript"))
-    implementation(npm("@types/webrtc", webrtcTypesVersion))
 
     implementation(enforcedPlatform(kotlinwBom()))
     implementation(kotlinw("js"))
