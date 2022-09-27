d86c5e51f48bace46a3b4d34f50d1c26264ae120
Victor Turansky <victor.turansky@gmail.com>
2022-09-27 23:51:43 +0300

    Update declarations

--- browser-kotlin/src/main/kotlin/webrtc/RTCIceServer.kt
+++ browser-kotlin/src/main/kotlin/webrtc/RTCIceServer.kt
@@ -4,6 +4,7 @@ package webrtc
 
 sealed external interface RTCIceServer {
     var credential: String?
+    var credentialType: RTCIceCredentialType?
     var urls: dynamic /* string | string[] */
     var username: String?
 }
