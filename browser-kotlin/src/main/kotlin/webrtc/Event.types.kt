e4fad49f4d23ca0fbdc8c0e9d30c9e5528a9446c
Victor Turansky <victor.turansky@gmail.com>
2022-10-07 18:37:14 +0300

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/webrtc/Event.types.kt
@@ -0,0 +1,45 @@
+// Automatically generated - do not modify!
+
+package webrtc
+
+import org.w3c.dom.events.Event
+import web.events.EventType
+
+inline val Event.Companion.BUFFERED_AMOUNT_LOW: EventType<Event>
+    get() = EventType("bufferedamountlow")
+
+inline val Event.Companion.CLOSE: EventType<Event>
+    get() = EventType("close")
+
+inline val Event.Companion.CLOSING: EventType<Event>
+    get() = EventType("closing")
+
+inline val Event.Companion.ERROR: EventType<Event>
+    get() = EventType("error")
+
+inline val Event.Companion.OPEN: EventType<Event>
+    get() = EventType("open")
+
+inline val Event.Companion.STATE_CHANGE: EventType<Event>
+    get() = EventType("statechange")
+
+inline val Event.Companion.GATHERING_STATE_CHANGE: EventType<Event>
+    get() = EventType("gatheringstatechange")
+
+inline val Event.Companion.CONNECTION_STATE_CHANGE: EventType<Event>
+    get() = EventType("connectionstatechange")
+
+inline val Event.Companion.ICE_CANDIDATE_ERROR: EventType<Event>
+    get() = EventType("icecandidateerror")
+
+inline val Event.Companion.ICE_CONNECTION_STATE_CHANGE: EventType<Event>
+    get() = EventType("iceconnectionstatechange")
+
+inline val Event.Companion.ICE_GATHERING_STATE_CHANGE: EventType<Event>
+    get() = EventType("icegatheringstatechange")
+
+inline val Event.Companion.NEGOTIATION_NEEDED: EventType<Event>
+    get() = EventType("negotiationneeded")
+
+inline val Event.Companion.SIGNALING_STATE_CHANGE: EventType<Event>
+    get() = EventType("signalingstatechange")
