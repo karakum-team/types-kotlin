09708a9037f846d0b4699e3ca11587bb2c68be00
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 01:52:29 +0300

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/web/clipboard/PresentationStyle.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.clipboard
+
+// language=JavaScript
+@JsName("""(/*union*/{attachment: 'attachment', inline: 'inline', unspecified: 'unspecified'}/*union*/)""")
+external enum class PresentationStyle {
+    attachment,
+    inline,
+    unspecified,
+
+    ;
+}
