c6ad73165048fd560ea05dfdbca5c98a66a4310f
Victor Turansky <victor.turansky@gmail.com>
2022-10-27 21:22:00 +0300

    Deprecated members removed

--- buildSrc/src/main/kotlin/karakum/react/Deprecated.kt
+++ buildSrc/src/main/kotlin/karakum/react/Deprecated.kt
@@ -1,4 +1,4 @@
 package karakum.react
 
 internal fun String.removeDeprecatedMembers(): String =
-    replace(Regex("""\n        \/\*\* @deprecated \*\/\n        .+?\n"""), "\n")
+    replace(Regex("""        \/\*\* @deprecated \*\/\n        .+?\n"""), "")
