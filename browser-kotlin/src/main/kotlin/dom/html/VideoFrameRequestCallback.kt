3455832a17799fd4100d4d4c70fffa465e087467
Victor Turansky <victor.turansky@gmail.com>
2022-10-27 12:29:08 +0300

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/dom/html/VideoFrameRequestCallback.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package dom.html
+
+import kotlinx.js.HighResTimeStamp
+
+typealias VideoFrameRequestCallback = (now: HighResTimeStamp, metadata: VideoFrameMetadata) -> Unit
