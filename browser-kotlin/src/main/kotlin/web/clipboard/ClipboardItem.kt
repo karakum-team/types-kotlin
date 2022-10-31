4b2faa17b5615d20ebbb0ddfa9ce37f7c453caeb
Victor Turansky <victor.turansky@gmail.com>
2022-10-31 02:00:18 +0200

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/web/clipboard/ClipboardItem.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+package web.clipboard
+
+import kotlinx.js.ReadonlyArray
+import kotlinx.js.Record
+import web.buffer.Blob
+import kotlin.js.Promise
+
+
+external class ClipboardItem(
+    items: Record<String, Any /* String | Blob | PromiseLike<String | Blob> */>,
+    options: ClipboardItemOptions = definedExternally,
+) {
+    val types: ReadonlyArray<String>
+    fun getType(type: String): Promise<Blob>
+}
---
+++ browser-kotlin/src/main/kotlin/web/clipboard/ClipboardItems.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package web.clipboard
+
+import kotlinx.js.ReadonlyArray
+
+typealias ClipboardItems = ReadonlyArray<ClipboardItem>
