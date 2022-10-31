dd04b3d985f6dd64fd783b607be6e6001deb9d4e
Victor Turansky <victor.turansky@gmail.com>
2022-10-31 19:31:36 +0200

    Update declarations

--- browser-kotlin/src/main/kotlin/web/geolocation/PositionOptions.kt
+++ browser-kotlin/src/main/kotlin/web/geolocation/PositionOptions.kt
@@ -4,6 +4,6 @@ package web.geolocation
 
 sealed external interface PositionOptions {
     var enableHighAccuracy: Boolean?
-    var maximumAge: Double?
-    var timeout: Double?
+    var maximumAge: Double /* Int64 */?
+    var timeout: Double /* Int64 */?
 }
