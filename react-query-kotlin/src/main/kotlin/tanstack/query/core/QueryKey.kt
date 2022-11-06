d4f0a86b3a691512fc5eedc4c4dcc72bffb3fdb9
Victor Turansky <victor.turansky@gmail.com>
2022-11-07 00:17:54 +0200

    Update declarations

--- react-query-kotlin/src/main/kotlin/tanstack/query/core/QueryKey.kt
+++ react-query-kotlin/src/main/kotlin/tanstack/query/core/QueryKey.kt
@@ -6,6 +6,15 @@
 
 package tanstack.query.core
 
+import kotlinx.js.ReadonlyArray
+
 inline fun <T : QueryKey> QueryKey(
-    vararg keys: Any,
-): T = keys.unsafeCast<T>()
+    vararg keys: Comparable<*>,
+): T =
+    keys.unsafeCast<T>()
+
+fun <T : QueryKey> QueryKey(
+    parentKey: QueryKey,
+    vararg keys: Comparable<*>,
+): T =
+    (parentKey.unsafeCast<ReadonlyArray<*>>() + keys).unsafeCast<T>()
