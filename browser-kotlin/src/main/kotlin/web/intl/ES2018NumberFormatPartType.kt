ab791584c7f9003ea8a350718e74e71ad52f253a
Victor Turansky <victor.turansky@gmail.com>
2022-11-15 20:56:05 +0200

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/web/intl/ES2018NumberFormatPartType.kt
@@ -0,0 +1,29 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.intl
+
+// language=JavaScript
+@JsName("""(/*union*/{literal: 'literal', nan: 'nan', infinity: 'infinity', percent: 'percent', integer: 'integer', group: 'group', decimal: 'decimal', fraction: 'fraction', plusSign: 'plusSign', minusSign: 'minusSign', percentSign: 'percentSign', currency: 'currency', code: 'code', symbol: 'symbol', htmlName: 'name'}/*union*/)""")
+external enum class ES2018NumberFormatPartType {
+    literal,
+    nan,
+    infinity,
+    percent,
+    integer,
+    group,
+    decimal,
+    fraction,
+    plusSign,
+    minusSign,
+    percentSign,
+    currency,
+    code,
+    symbol,
+    htmlName,
+
+    ;
+}
---
+++ browser-kotlin/src/main/kotlin/web/intl/ES2020NumberFormatPartType.kt
@@ -0,0 +1,20 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.intl
+
+// language=JavaScript
+@JsName("""(/*union*/{compact: 'compact', exponentInteger: 'exponentInteger', exponentMinusSign: 'exponentMinusSign', exponentSeparator: 'exponentSeparator', unit: 'unit', unknown: 'unknown'}/*union*/)""")
+external enum class ES2020NumberFormatPartType {
+    compact,
+    exponentInteger,
+    exponentMinusSign,
+    exponentSeparator,
+    unit,
+    unknown,
+
+    ;
+}
