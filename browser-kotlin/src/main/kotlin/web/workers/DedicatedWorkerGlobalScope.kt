0898a7982ff8a8b276e5b9b0b9097153e6e90f1f
Victor Turansky <victor.turansky@gmail.com>
2022-11-22 11:10:36 +0200

    Update declarations

--- browser-kotlin/src/main/kotlin/web/workers/DedicatedWorkerGlobalScope.kt
+++ browser-kotlin/src/main/kotlin/web/workers/DedicatedWorkerGlobalScope.kt
@@ -4,26 +4,21 @@ package web.workers
 
 import kotlinx.js.ReadonlyArray
 import web.events.EventHandler
-import web.events.EventTarget
 import web.messaging.MessageEvent
 import web.messaging.StructuredSerializeOptions
 import web.messaging.Transferable
-import web.url.URL
 
-open external class Worker(
-    scriptURL: String,
-    options: WorkerOptions = definedExternally,
-) : EventTarget,
-    AbstractWorker {
-    constructor(
-        scriptURL: URL,
-        options: WorkerOptions = definedExternally,
-    )
-
+sealed external class DedicatedWorkerGlobalScope :
+    WorkerGlobalScope {
+    /** Returns dedicatedWorkerGlobal's name, i.e. the value given to the Worker constructor. Primarily useful for debugging. */
+    val name: String
     var onmessage: EventHandler<MessageEvent<*>>?
     var onmessageerror: EventHandler<MessageEvent<*>>?
 
-    /** Clones message and transmits it to worker's global environment. transfer can be passed as a list of objects that are to be transferred rather than cloned. */
+    /** Aborts dedicatedWorkerGlobal. */
+    fun close()
+
+    /** Clones message and transmits it to the Worker object associated with dedicatedWorkerGlobal. transfer can be passed as a list of objects that are to be transferred rather than cloned. */
     fun postMessage(
         message: Any?,
         transfer: ReadonlyArray<Transferable>,
@@ -33,7 +28,4 @@ open external class Worker(
         message: Any?,
         options: StructuredSerializeOptions = definedExternally,
     )
-
-    /** Aborts worker's associated global environment. */
-    fun terminate()
 }
