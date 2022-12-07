4991dba7992530a1cf81b208e3d24c3d0a96964a
Victor Turansky <victor.turansky@gmail.com>
2022-12-07 03:54:18 +0200

    Update declarations

---
+++ react-virtual-kotlin/src/main/kotlin/tanstack/virtual/core/ScrollOptions.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package tanstack.virtual.core
+
+external interface ScrollOptions {
+    var adjustments: Int?
+    var behavior: ScrollBehavior?
+    var sync: Boolean
+}
