887eb06e37a97f89cfad948c7e77413061d4f795
Victor Turansky <victor.turansky@gmail.com>
2022-12-30 14:16:07 +0200

    Cleanup

--- buildSrc/src/main/kotlin/karakum/browser/DomAliases.kt
+++ buildSrc/src/main/kotlin/karakum/browser/DomAliases.kt
@@ -1,10 +1,5 @@
 package karakum.browser
 
-private val CLASSES = listOf(
-    "DOMStringMap",
-    "DOMTokenList",
-)
-
 internal fun domAliases(): List<ConversionResult> =
     listOf(
         ConversionResult(
