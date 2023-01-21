685fabcef089272e0b5de8a35d71f9e7a61a54e6
Victor Turansky <victor.turansky@gmail.com>
2023-01-22 01:45:26 +0200

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/web/html/EnterKeyHint.kt
@@ -0,0 +1,21 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.html
+
+// language=JavaScript
+@JsName("""(/*union*/{enter: 'enter', done: 'done', go: 'go', next: 'next', previous: 'previous', search: 'search', send: 'send'}/*union*/)""")
+external enum class EnterKeyHint {
+    enter,
+    done,
+    go,
+    next,
+    previous,
+    search,
+    send,
+
+    ;
+}
---
+++ browser-kotlin/src/main/kotlin/web/html/Loading.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.html
+
+// language=JavaScript
+@JsName("""(/*union*/{eager: 'eager', lazy: 'lazy'}/*union*/)""")
+external enum class Loading {
+    eager,
+    lazy,
+
+    ;
+}
