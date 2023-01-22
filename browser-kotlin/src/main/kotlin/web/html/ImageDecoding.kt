12004632a1e83d782de130621d00ed58560329f6
Victor Turansky <victor.turansky@gmail.com>
2023-01-22 02:08:41 +0200

    Update declarations

---
+++ browser-kotlin/src/main/kotlin/web/html/ImageDecoding.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.html
+
+// language=JavaScript
+@JsName("""(/*union*/{async: 'async', sync: 'sync', auto: 'auto'}/*union*/)""")
+external enum class ImageDecoding {
+    async,
+    sync,
+    auto,
+
+    ;
+}
