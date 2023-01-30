2d0110dc2b05589bbb5092b24a3f5e848721d92f
Victor Turansky <victor.turansky@gmail.com>
2023-01-30 04:46:29 +0200

    Actions. Add NPM dependencies

--- actions-kotlin/build.gradle.kts
+++ actions-kotlin/build.gradle.kts
@@ -6,7 +6,15 @@ plugins {
 }
 
 dependencies {
-    // implementation(npmv("@types/web"))
+    implementation(npmv("@actions/artifact"))
+    implementation(npmv("@actions/cache"))
+    implementation(npmv("@actions/core"))
+    implementation(npmv("@actions/exec"))
+    implementation(npmv("@actions/github"))
+    implementation(npmv("@actions/glob"))
+    implementation(npmv("@actions/http-client"))
+    implementation(npmv("@actions/io"))
+    implementation(npmv("@actions/tool-cache"))
 
     implementation(wrappers("node"))
 }
