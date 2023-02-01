64bb52ebc2025ec158d8a0da754777a7ff1fce48
Victor Turansky <victor.turansky@gmail.com>
2023-02-02 00:45:54 +0200

    Update declarations

--- actions-kotlin/src/main/kotlin/actions/github/utils.d.ts
+++ actions-kotlin/src/main/kotlin/actions/github/utils.d.ts
@@ -1,9 +1,17 @@
-/// <reference types="node" />
-import * as http from 'http';
+import * as Context from './context';
+import {Octokit} from '@octokit/core';
 import {OctokitOptions} from '@octokit/core/dist-types/types';
 
-export declare function getAuthString(token: string, options: OctokitOptions): string | undefined;
+export declare const context: Context.Context;
+export declare const defaults: OctokitOptions;
+export declare const GitHub: typeof Octokit & import("@octokit/core/dist-types/types").Constructor<import("@octokit/plugin-rest-endpoint-methods/dist-types/types").Api & {
+    paginate: import("@octokit/plugin-paginate-rest").PaginateInterface;
+}>;
 
-export declare function getProxyAgent(destinationUrl: string): http.Agent;
-
-export declare function getApiBaseUrl(): string;
+/**
+ * Convience function to correctly format Octokit Options to pass into the constructor.
+ *
+ * @param     token    the repo PAT or GITHUB_TOKEN
+ * @param     options  other options to set
+ */
+export declare function getOctokitOptions(token: string, options?: OctokitOptions): OctokitOptions;
