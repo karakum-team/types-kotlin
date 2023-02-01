5a9f231c40f04e2c01dafdc1015bf727e79f2144
Victor Turansky <victor.turansky@gmail.com>
2023-02-01 23:58:18 +0200

    Actions. Empty generator

---
+++ buildSrc/src/main/kotlin/karakum/actions/Generator.kt
@@ -0,0 +1,14 @@
+package karakum.actions
+
+import karakum.common.GENERATOR_COMMENT
+import karakum.common.Suppress
+import karakum.common.Suppress.*
+import karakum.common.fileSuppress
+import java.io.File
+
+fun generateKotlinDeclarations(
+    definitionsDir: File,
+    sourceDir: File,
+) {
+    // TODO: implement
+}
