6f3cf46c5f707ad19867af85377ea36aaa47dc5b
Victor Turansky <victor.turansky@gmail.com>
2023-02-02 00:52:30 +0200

    Remove local imports

--- buildSrc/src/main/kotlin/karakum/actions/Converter.kt
+++ buildSrc/src/main/kotlin/karakum/actions/Converter.kt
@@ -8,7 +8,16 @@ internal fun convert(
     return sequenceOf(
         ConversionResult(
             name = fileName,
-            body = content,
+            body = cleanup(content),
         )
     )
 }
+
+private fun cleanup(
+    content: String,
+): String =
+    content.splitToSequence("\n")
+        .filter { line ->
+            !(line.startsWith("import ") && " from '." in line)
+        }
+        .joinToString("\n")
