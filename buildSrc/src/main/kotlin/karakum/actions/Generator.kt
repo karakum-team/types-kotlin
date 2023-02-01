d7846ade10462736fe60b7737e45fa59fe37ee17
Victor Turansky <victor.turansky@gmail.com>
2023-02-02 00:32:08 +0200

    Actions. Copy declarations

--- buildSrc/src/main/kotlin/karakum/actions/Generator.kt
+++ buildSrc/src/main/kotlin/karakum/actions/Generator.kt
@@ -1,14 +1,48 @@
 package karakum.actions
 
-import karakum.common.GENERATOR_COMMENT
-import karakum.common.Suppress
-import karakum.common.Suppress.*
-import karakum.common.fileSuppress
 import java.io.File
+
+private class Library(
+    val name: String,
+) {
+    val moduleId: String = "@actions/$name"
+    val path: String = "actions/" + name.replace("-", "/")
+}
 
 fun generateKotlinDeclarations(
     definitionsDir: File,
     sourceDir: File,
 ) {
-    // TODO: implement
+    definitionsDir.listFiles()!!
+        .filter { it.isDirectory }
+        .forEach { dir ->
+            generate(
+                definitionsDir = dir,
+                sourceDir = sourceDir,
+            )
+        }
+}
+
+private fun generate(
+    definitionsDir: File,
+    sourceDir: File,
+) {
+    val library = Library(definitionsDir.name)
+    val files = sequenceOf("lib", "lib/internal")
+        .map { definitionsDir.resolve(it) }
+        .filter { it.exists() }
+        .mapNotNull { it.listFiles { file -> file.name.endsWith(".d.ts") } }
+        .flatMap { it.asSequence() }
+        .toList()
+
+    val dir = sourceDir.resolve(library.path)
+        .also { it.mkdirs() }
+
+    for (file in files) {
+        val name = file.name
+        val content = file.readText()
+
+        dir.resolve(name)
+            .writeText(content)
+    }
 }
