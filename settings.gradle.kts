5f6f51d3cfc0f42b6c6a97dc10c01b414ef4e7e8
Victor Turansky <victor.turansky@gmail.com>
2023-03-04 18:18:19 +0200

    Use `multiplatform` plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "types-kotlin"
 pluginManagement {
     plugins {
         val kotlinVersion = extra["kotlin.version"] as String
-        kotlin("js") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion
 
         val kfcVersion = extra["kfc.version"] as String
         id("io.github.turansky.kfc.library") version kfcVersion
