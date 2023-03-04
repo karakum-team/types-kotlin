62f3d212ab2a5ab5b8034f092e862b04b52eaadf
Victor Turansky <victor.turansky@gmail.com>
2023-03-04 18:05:02 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/html/ButtonType.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.html
+
+// language=JavaScript
+@JsName("""(/*union*/{submit: 'submit', reset: 'reset', button: 'button'}/*union*/)""")
+external enum class ButtonType {
+    submit,
+    reset,
+    button,
+
+    ;
+}
