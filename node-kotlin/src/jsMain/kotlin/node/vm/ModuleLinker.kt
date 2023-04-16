e71150b8a80984c589033e8680ca46235f54c141
Victor Turansky <victor.turansky@gmail.com>
2023-04-16 16:15:02 +0300

    Tanstack Query `4.29.2`

---
+++ node-kotlin/src/jsMain/kotlin/node/vm/ModuleLinker.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+package node.vm
+
+import js.promise.PromiseResult
+import node.Module
+
+typealias ModuleLinker = (
+ specifier: String,
+ referencingModule: Module,
+ extra: Any, /* { assert: Object }  */
+) -> PromiseResult<Module>
