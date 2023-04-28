3e9181c430897a3354ef4ff4d6e15f4f36701de6
Victor Turansky <victor.turansky@gmail.com>
2023-04-28 03:54:45 +0300

    Convert `FormAction`

--- buildSrc/src/main/kotlin/karakum/react/KotlinType.kt
+++ buildSrc/src/main/kotlin/karakum/react/KotlinType.kt
@@ -41,10 +41,16 @@ private val ANY_ALIASES = setOf(
     "number | Date",
 )
 
+private val FORM_ACTION =
+    "DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_FORM_ACTIONS[keyof DO_NOT_USE_OR_YOU_WILL_BE_FIRED_EXPERIMENTAL_FORM_ACTIONS]"
+
 internal fun kotlinType(
     type: String,
     name: String,
 ): String {
+    if (FORM_ACTION in type)
+        return "String /* FormAction */"
+
     if (type.endsWith(" | undefined"))
         return kotlinType(type.removeSuffix(" | undefined"), name)
 
