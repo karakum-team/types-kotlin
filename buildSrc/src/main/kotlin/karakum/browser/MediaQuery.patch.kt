c1feaa368db0a19846891f0143a15ddfb08a9c5b
Victor Turansky <victor.turansky@gmail.com>
2023-05-03 11:12:39 +0300

    Fix compilation

--- buildSrc/src/main/kotlin/karakum/browser/MediaQuery.patch.kt
+++ buildSrc/src/main/kotlin/karakum/browser/MediaQuery.patch.kt
@@ -1,6 +1,6 @@
 package karakum.browser
 
-private const val MEDIA_QUERY: String = "MediaQuery"
+internal const val MEDIA_QUERY: String = "MediaQuery"
 
 internal fun String.applyMediaQueryPatch(): String =
     replace("var media: String", "var media: $MEDIA_QUERY")
