50b4d6d9aa67340ba3a3244fd9004944a8c1b648
Victor Turansky <victor.turansky@gmail.com>
2023-06-09 15:44:05 +0300

    Strict resolution for `@types/node`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
+
 plugins {
     kotlin("multiplatform") apply false
 }
 
 tasks.wrapper {
     gradleVersion = "8.1.1"
+}
+
+plugins.withType<YarnPlugin> {
+    the<YarnRootExtension>().apply {
+        resolution("@types/node", version("node-types"))
+    }
 }
