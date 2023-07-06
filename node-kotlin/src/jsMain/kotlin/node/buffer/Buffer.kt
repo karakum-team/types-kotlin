a4be023e0c8f1e058d02e10be6491a95ad440b41
Victor Turansky <victor.turansky@gmail.com>
2023-07-06 11:19:02 +0300

    Update declarations

--- node-kotlin/src/jsMain/kotlin/node/buffer/Buffer.kt
+++ node-kotlin/src/jsMain/kotlin/node/buffer/Buffer.kt
@@ -5,7 +5,7 @@
 package node.buffer
 
 import js.core.BigInt
-import js.core.JsIterable
+import js.core.IterableIterator
 import js.core.JsTuple2
 import js.core.ReadonlyArray
 import js.typedarrays.TypedArray
@@ -1886,7 +1886,7 @@ external class Buffer : Uint8Array {
      * ```
      * @since v1.1.0
      */
-    override fun entries(): JsIterable.Iterator<JsTuple2<Int, Byte>>
+    override fun entries(): IterableIterator<JsTuple2<Int, Byte>>
 
     /**
      * Equivalent to `buf.indexOf() !== -1`.
@@ -1944,7 +1944,7 @@ external class Buffer : Uint8Array {
      * ```
      * @since v1.1.0
      */
-    override fun keys(): JsIterable.Iterator<Int>
+    override fun keys(): IterableIterator<Int>
 
     /**
      * Creates and returns an [iterator](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols) for `buf` values (bytes). This function is
@@ -1979,7 +1979,7 @@ external class Buffer : Uint8Array {
      * ```
      * @since v1.1.0
      */
-    override fun values(): JsIterable.Iterator<Byte>
+    override fun values(): IterableIterator<Byte>
 
     companion object {
         /**
