77208a48d922d402fee2b8ca72ebf01e1bea69be
Victor Turansky <victor.turansky@gmail.com>
2023-10-15 19:49:07 +0300

    Add `seskarCore` extension

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,3 +8,6 @@ private fun Project.kotlinx(
     name: String,
 ): String =
     "org.jetbrains.kotlinx:$projectName-$name:${version(projectName)}"
+
+fun Project.seskarCore(): String =
+    "io.github.turansky.seskar:seskar-core:${version("seskar")}"
