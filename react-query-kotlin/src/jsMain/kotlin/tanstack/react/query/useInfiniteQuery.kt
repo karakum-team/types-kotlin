e78fed1181783091aa77173ce9fc28c122a5a51c
Victor Turansky <victor.turansky@gmail.com>
2023-11-19 02:09:39 +0200

    Update declarations

--- react-query-kotlin/src/jsMain/kotlin/tanstack/react/query/useInfiniteQuery.kt
+++ react-query-kotlin/src/jsMain/kotlin/tanstack/react/query/useInfiniteQuery.kt
@@ -8,16 +8,6 @@ import tanstack.query.core.QueryClient
 import tanstack.query.core.QueryKey
 
 external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey, TPageParam> useInfiniteQuery(
-    options: UndefinedInitialDataInfiniteOptions<TQueryFnData, TError, TData, TQueryKey, TPageParam>,
-    queryClient: QueryClient = definedExternally,
-): UseInfiniteQueryResult<TData, TError>
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey, TPageParam> useInfiniteQuery(
-    options: DefinedInitialDataInfiniteOptions<TQueryFnData, TError, TData, TQueryKey, TPageParam>,
-    queryClient: QueryClient = definedExternally,
-): DefinedUseInfiniteQueryResult<TData, TError>
-
-external fun <TQueryFnData, TError, TData, TQueryKey : QueryKey, TPageParam> useInfiniteQuery(
     options: UseInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey, TPageParam>,
     queryClient: QueryClient = definedExternally,
 ): UseInfiniteQueryResult<TData, TError>
