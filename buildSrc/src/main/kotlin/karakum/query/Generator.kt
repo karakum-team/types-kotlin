d79ef6f3e73ab713ebfcc3d7b774727338901735
Victor Turansky <victor.turansky@gmail.com>
2023-11-19 22:32:27 +0200

    Add `DefinedInfiniteQueryObserverResult` import

--- buildSrc/src/main/kotlin/karakum/query/Generator.kt
+++ buildSrc/src/main/kotlin/karakum/query/Generator.kt
@@ -170,6 +170,7 @@ private fun generate(
             """
             import tanstack.query.core.InfiniteQueryObserverOptions
             import tanstack.query.core.InfiniteQueryObserverResult
+            import tanstack.query.core.DefinedInfiniteQueryObserverResult
             import tanstack.query.core.DefinedQueryObserverResult
             import tanstack.query.core.HydrateOptions
             import tanstack.query.core.QueryClient
