34cc4dcaae2c11270dec1f34811b6abca8931325
Victor Turansky <victor.turansky@gmail.com>
2023-11-20 00:12:41 +0200

    Update declarations

--- react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/QueryStore.kt
+++ react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/QueryStore.kt
@@ -7,7 +7,7 @@ import js.iterable.IterableIterator
 external interface QueryStore {
     var has: (queryKey: String) -> Boolean
     var set: (queryKey: String, query: Query<*, *, *, *>) -> Unit
-    var get: (queryKey: String) -> Query?
+    var get: (queryKey: String) -> Query<*, *, *, *>?
     var delete: (queryKey: String) -> Unit
     var values: () -> IterableIterator<Query<*, *, *, *>>
 }
