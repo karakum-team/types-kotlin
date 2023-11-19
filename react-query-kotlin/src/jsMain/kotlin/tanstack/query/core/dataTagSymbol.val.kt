75f2180b0a17aa0c93e4f8e2c9d26e33ac075ea2
Victor Turansky <victor.turansky@gmail.com>
2023-11-20 00:33:49 +0200

    Update declarations

--- react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/dataTagSymbol.val.kt
+++ react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/dataTagSymbol.val.kt
@@ -1,9 +1,7 @@
 // Automatically generated - do not modify!
 
-@file:JsModule("@tanstack/query-core")
-
 package tanstack.query.core
 
 import js.core.Symbol
 
-external val dataTagSymbol: Symbol /* unique symbol */
+external object dataTagSymbol : Symbol
