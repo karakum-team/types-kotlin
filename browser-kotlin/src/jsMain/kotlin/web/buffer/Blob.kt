dc361f1fa6abdd93befcb4d9e75a324388791461
Victor Turansky <victor.turansky@gmail.com>
2023-11-26 15:59:09 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/buffer/Blob.kt
@@ -0,0 +1,41 @@
+// Automatically generated - do not modify!
+
+package web.buffer
+
+import js.buffer.ArrayBuffer
+import js.core.ReadonlyArray
+import js.promise.Promise
+import js.typedarrays.Uint8Array
+import web.streams.ReadableStream
+
+/**
+ * A file-like object of immutable, raw data. Blobs represent data that isn't necessarily in a JavaScript-native format. The File interface is based on Blob, inheriting blob functionality and expanding it to support files on the user's system.
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob)
+ */
+open external class Blob(
+    blobParts: ReadonlyArray<BlobPart> = definedExternally,
+    options: BlobPropertyBag = definedExternally,
+) {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob/size) */
+    val size: Int
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob/type) */
+    val type: String
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob/arrayBuffer) */
+    fun arrayBuffer(): Promise<ArrayBuffer>
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob/slice) */
+    fun slice(
+        start: Number = definedExternally,
+        end: Number = definedExternally,
+        contentType: String = definedExternally,
+    ): Blob
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob/stream) */
+    fun stream(): ReadableStream<Uint8Array>
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Blob/text) */
+    fun text(): Promise<String>
+}
---
+++ browser-kotlin/src/jsMain/kotlin/web/buffer/BlobPropertyBag.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package web.buffer
+
+external interface BlobPropertyBag {
+    var endings: EndingType?
+    var type: String?
+}
