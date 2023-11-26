895c4da00e0b94dac66e71b39de9281a5f36efab
Victor Turansky <victor.turansky@gmail.com>
2023-11-26 15:25:45 +0200

    Add `syncKotlinBrowser` task

--- browser-kotlin/build.gradle.kts
+++ browser-kotlin/build.gradle.kts
@@ -17,3 +17,111 @@ dependencies {
 
     jsMainImplementation(seskarCore())
 }
+
+val JS_INCLUDE = setOf(
+    "js",
+)
+
+val WEB_INCLUDE = setOf(
+    "web/abort",
+    "web/compression",
+    "web/console",
+    "web/crypto",
+    "web/encoding",
+    "web/events",
+    "web/file/File.kt",
+    "web/file/FilePropertyBag.kt",
+    "web/http",
+    "web/messaging",
+    "web/performance",
+    "web/streams",
+    "web/url",
+    "webassembly",
+)
+
+val BROWSER_INCLUDE = setOf(
+    "web",
+    "web/events/ProgressEvent.kt",
+    "web/file",
+
+    "webgl",
+    "webrtc",
+    "websockets",
+    "webvtt",
+)
+
+enum class WrapperProject {
+    JS,
+    WEB,
+    BROWSER,
+
+    ;
+}
+
+fun getWrapperProject(path: String): WrapperProject? =
+    when (path) {
+        in JS_INCLUDE -> WrapperProject.JS
+        in WEB_INCLUDE -> WrapperProject.WEB
+        in BROWSER_INCLUDE -> WrapperProject.BROWSER
+        else -> if ("/" in path) getWrapperProject(path.substringBeforeLast("/")) else null
+    }
+
+fun isDirFromWrapperProject(
+    path: String,
+    wp: WrapperProject,
+): Boolean {
+    val included = when (wp) {
+        WrapperProject.JS -> JS_INCLUDE
+        WrapperProject.WEB -> WEB_INCLUDE
+        WrapperProject.BROWSER -> BROWSER_INCLUDE
+    }
+
+    return path in included || included.any { it.startsWith("$path/") }
+}
+
+fun isFromWrapperProject(wp: WrapperProject): Spec<FileTreeElement> {
+    return Spec<FileTreeElement> { element ->
+        val path = element.path
+        if (element.isDirectory) {
+            isDirFromWrapperProject(path, wp)
+        } else {
+            getWrapperProject(path) == wp
+        }
+    }
+}
+
+val syncKotlinWeb by tasks.creating(Sync::class) {
+    val generatedDir = project.layout.projectDirectory.dir("src/jsMain/kotlin")
+
+    val kotlinWrappersDir = project.rootProject.layout.projectDirectory.dir("../kotlin-wrappers")
+    val webDir = kotlinWrappersDir.dir("kotlin-web/src/jsMain/generated")
+
+    from(generatedDir) {
+        include(isFromWrapperProject(WrapperProject.WEB))
+
+        // TEMP
+        preserve {
+            include("web/events/EventType.kt")
+        }
+    }
+
+    into(webDir.asFile)
+}
+
+val syncKotlinBrowser by tasks.creating(Sync::class) {
+    val generatedDir = project.layout.projectDirectory.dir("src/jsMain/kotlin")
+
+    val kotlinWrappersDir = project.rootProject.layout.projectDirectory.dir("../kotlin-wrappers")
+    val browserDir = kotlinWrappersDir.dir("kotlin-browser/src/jsMain/generated")
+
+    from(generatedDir) {
+        include(isFromWrapperProject(WrapperProject.BROWSER))
+    }
+
+    into(browserDir.asFile)
+}
+
+val syncWithWrappers by tasks.creating {
+    dependsOn(syncKotlinWeb)
+    dependsOn(syncKotlinBrowser)
+}
