e3c6ae28cac092b7109c7b103463218d52152614
Victor Turansky <victor.turansky@gmail.com>
2023-11-27 00:30:57 +0200

    Atomics. Initial

--- buildSrc/src/main/kotlin/karakum/browser/Atomics.kt
+++ buildSrc/src/main/kotlin/karakum/browser/Atomics.kt
@@ -5,19 +5,54 @@ import java.io.File
 internal fun atomicsDeclarations(
     definitionsDir: File,
 ): ConversionResult {
-    val source = atomicsContent(definitionsDir)
+    val members = atomicsContent(definitionsDir)
+        .splitToSequence("\n")
+        .map { line -> if (line.isEmpty() || "*" in line) line else atomicsFunction(line) }
+        .joinToString("\n")
 
     val body = """
     external object Atomics {
-        $source
+        $members
     }    
     """.trimIndent()
 
     return ConversionResult(
         name = "Atomics",
         body = body,
-        pkg = "js.atomics",
+        pkg = "js.atomic",
     )
+}
+
+private val TYPED_ARRAYS = "Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array"
+
+internal fun atomicsFunction(
+    source: String,
+): String {
+    var content = source
+        .removeSuffix(";")
+        .replace(
+            "{ async: false; value: \"not-equal\" | \"timed-out\"; } | { async: true; value: Promise<\"ok\" | \"timed-out\">; }",
+            "WaitResult",
+        )
+        .replace(""""ok" | "not-equal" | "timed-out"""", "WaitStatus")
+        .replace(", ", ",\n")
+        .replace("index: number", "index: Int")
+        .replace(": bigint", ": BigInt")
+        .replace("count?: number", "count: Int = definedExternally")
+        .replace("timeout?: number", "timeout: Int = definedExternally")
+
+    if (TYPED_ARRAYS in content) {
+        content = "<T : Comparable<T>> " +
+                content.replace(TYPED_ARRAYS, "TypedArray<*, T>")
+                    .replace("value: number", "value: T")
+                    .replace("expectedValue: number", "expectedValue: T")
+                    .replace("replacementValue: number", "replacementValue: T")
+                    .replace("): number", "): T")
+    }
+
+    content = content.replace("value: number", "value: Int")
+
+    return "fun " + content
 }
 
 private fun atomicsContent(
@@ -33,4 +68,7 @@ private fun atomicsContent(
                 .substringBefore("\n}")
                 .trimIndent()
         }
-        .joinToString("\n")
+        .flatMap { it.splitToSequence("\n\n") }
+        .filter { ": BigInt64Array | BigUint64Array," !in it }
+        .filter { "[Symbol.toStringTag]" !in it }
+        .joinToString("\n\n")
