a16c3cf957378fe5bbb60b98945dfd01920ee24d
Victor Turansky <victor.turansky@gmail.com>
2023-11-26 16:41:17 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/events/CustomEvent.kt
@@ -0,0 +1,22 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
+)
+
+package web.events
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent) */
+open external class CustomEvent<out T>(
+    override val type: EventType<CustomEvent<T>>,
+    init: CustomEventInit<T> = definedExternally,
+) : Event {
+    /**
+     * Returns any custom data event was created with. Typically used for synthetic events.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent/detail)
+     */
+    val detail: T
+
+    companion object
+}
