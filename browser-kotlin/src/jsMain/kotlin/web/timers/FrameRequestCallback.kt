41d27143d1189b15f57d5df6906fe20fb2e43122
Victor Turansky <victor.turansky@gmail.com>
2023-11-28 22:58:12 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/timers/FrameRequestCallback.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package web.timers
+
+import js.core.DOMHighResTimeStamp
+
+typealias FrameRequestCallback = (
+    time: DOMHighResTimeStamp,
+) -> Unit
---
+++ browser-kotlin/src/jsMain/kotlin/web/timers/IdleRequestCallback.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package web.timers
+
+typealias IdleRequestCallback = (
+    deadline: IdleDeadline,
+) -> Unit
