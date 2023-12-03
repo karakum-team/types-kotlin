c882be9dfd9266956b19563d02ea2b35298a5a34
Victor Turansky <victor.turansky@gmail.com>
2023-12-03 23:44:11 +0200

    Update declarations

--- browser-kotlin/src/jsMain/kotlin/web/messaging/Transferable.kt
+++ browser-kotlin/src/jsMain/kotlin/web/messaging/Transferable.kt
@@ -1,6 +1,18 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package web.messaging
 
-// OffscreenCanvas | ImageBitmap | MessagePort | ReadableStream | WritableStream | TransformStream | VideoFrame | ArrayBuffer
-external interface Transferable            
+import js.buffer.ArrayBuffer
+
+// OffscreenCanvas | ImageBitmap | MessagePort | ReadableStream | WritableStream | TransformStream | VideoFrame | ArrayBuffer    
+external interface Transferable
+
+inline fun ArrayBuffer.asTransferable(): Transferable =
+    unsafeCast<Transferable>()
+
+inline fun Transferable.asArrayBufferOrNull(): ArrayBuffer? =
+    asDynamic() as? ArrayBuffer    
