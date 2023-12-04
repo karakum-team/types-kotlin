5b97f603cca5ff7a6e16f9eebb274cdfedfb907c
Victor Turansky <victor.turansky@gmail.com>
2023-12-05 00:59:38 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/canvas/createImageBitmap.kt
@@ -0,0 +1,20 @@
+// Automatically generated - do not modify!
+
+package web.canvas
+
+import js.promise.Promise
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/createImageBitmap) */
+external fun createImageBitmap(
+    image: ImageBitmapSource,
+    options: ImageBitmapOptions? = definedExternally,
+): Promise<ImageBitmap>
+
+external fun createImageBitmap(
+    image: ImageBitmapSource,
+    sx: Int,
+    sy: Int,
+    sw: Int,
+    sh: Int,
+    options: ImageBitmapOptions? = definedExternally,
+): Promise<ImageBitmap>
