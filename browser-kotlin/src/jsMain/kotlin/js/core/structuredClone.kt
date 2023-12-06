a988858a62d4d1a3626c16d6c051d7d6bc470ed4
Victor Turansky <victor.turansky@gmail.com>
2023-12-06 20:14:23 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/js/core/structuredClone.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package js.core
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/structuredClone) */
+external fun <T> structuredClone(
+    value: T,
+    options: StructuredSerializeOptions? = definedExternally,
+): T
