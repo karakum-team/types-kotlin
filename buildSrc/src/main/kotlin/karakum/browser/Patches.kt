46f2544422009667b223155966bde2b922f822e7
Victor Turansky <victor.turansky@gmail.com>
2023-12-23 05:17:09 +0200

    Strict error reason

--- buildSrc/src/main/kotlin/karakum/browser/Patches.kt
+++ buildSrc/src/main/kotlin/karakum/browser/Patches.kt
@@ -23,6 +23,7 @@ internal fun String.applyPatches(): String =
             "// abort(reason?: any): AbortSignal; - To be re-added in the future",
             "abort(reason?: any): AbortSignal;",
         )
+        .replace("    reason?: any;", "    reason?: JsError;")
         .replace("readonly reason: any;", "readonly reason: JsError | undefined;")
         .replace("(reason?: any)", "(reason?: JsError)")
         .replace("\n    exportKey(format: KeyFormat, key: CryptoKey): Promise<ArrayBuffer | JsonWebKey>;", "")
