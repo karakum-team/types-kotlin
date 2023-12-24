164d6dc3f48f10f69879a64c587dd84b83774a37
Victor Turansky <victor.turansky@gmail.com>
2023-12-24 23:47:49 +0200

    Form. `CustomFormControl`

--- buildSrc/src/main/kotlin/karakum/browser/Form.kt
+++ buildSrc/src/main/kotlin/karakum/browser/Form.kt
@@ -1,7 +1,14 @@
 package karakum.browser
 
+import karakum.common.unionBody
+
 internal const val VALIDATION_TARGET = "ValidationTarget"
 internal const val FORM_CONTROL = "FormControl"
+
+internal const val CUSTOM_FORM_CONTROL = "CustomFormControl"
+internal const val CUSTOM_FORM_CONTROL_CALLBACKS = "CustomFormControlCallbacks"
+
+internal const val FORM_STATE_RESTORE_MODE = "FormStateRestoreMode"
 
 internal val WELL_KNOWN_FORM_CONTROL = listOf(
     "HTMLButtonElement",
@@ -11,6 +18,13 @@ internal val WELL_KNOWN_FORM_CONTROL = listOf(
     "HTMLOutputElement",
     "HTMLSelectElement",
     "HTMLTextAreaElement",
+)
+
+private val CALLBACKS = mapOf(
+    "formAssociatedCallback" to "(form: HTMLFormElement?) -> Unit",
+    "formDisabledCallback" to "(disabled: Boolean) -> Unit",
+    "formResetCallback" to "() -> Unit",
+    "formStateRestoreCallback" to "(state: Any? /* File | string | FormData */, mode: $FORM_STATE_RESTORE_MODE) -> Unit",
 )
 
 private val VALIDATION_TARGET_MEMBERS = """
@@ -63,4 +77,56 @@ internal fun formTypes(): Sequence<ConversionResult> =
             """.trimIndent(),
             pkg = "web.form"
         ),
+        ConversionResult(
+            name = FORM_STATE_RESTORE_MODE,
+            body = unionBody(
+                FORM_STATE_RESTORE_MODE,
+                listOf("restore", "autocomplete")
+            ),
+            pkg = "web.form"
+        ),
+        CustomFormControlCallbacks(),
+        ConversionResult(
+            name = CUSTOM_FORM_CONTROL,
+            body = """
+            external interface $CUSTOM_FORM_CONTROL:
+                $FORM_CONTROL,
+                $CUSTOM_FORM_CONTROL_CALLBACKS
+            """.trimIndent(),
+            pkg = "web.form"
+        ),
+    ).plus(
+        CALLBACKS.map { (propertyName, typeSource) ->
+            val aliasName = propertyName.replaceFirstChar(Char::uppercase)
+            val aliasBody = if ("()" !in typeSource) {
+                typeSource.replaceFirst("(", "(\n")
+                    .replaceFirst(", ", ",\n")
+            } else typeSource
+
+            ConversionResult(
+                name = aliasName,
+                body = "typealias $aliasName = " + aliasBody,
+                pkg = "web.form",
+            )
+        }
     )
+
+private fun CustomFormControlCallbacks(): ConversionResult {
+    val members = CALLBACKS.keys.joinToString("\n") { name ->
+        """
+        var $name: ${name.replaceFirstChar(Char::uppercase)}?
+            get() = definedExternally
+            set(value) = definedExternally
+        """.trimIndent()
+    }
+
+    val body = "external interface $CUSTOM_FORM_CONTROL_CALLBACKS {\n" +
+            members +
+            "\n}"
+
+    return ConversionResult(
+        name = CUSTOM_FORM_CONTROL_CALLBACKS,
+        body = body,
+        pkg = "web.form"
+    )
+}
