d73dea3892a335dde87e4adec0e276661660ca81
Victor Turansky <victor.turansky@gmail.com>
2023-12-24 23:48:00 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/form/CustomFormControlCallbacks.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package web.form
+
+external interface CustomFormControlCallbacks {
+    var formAssociatedCallback: FormAssociatedCallback?
+        get() = definedExternally
+        set(value) = definedExternally
+    var formDisabledCallback: FormDisabledCallback?
+        get() = definedExternally
+        set(value) = definedExternally
+    var formResetCallback: FormResetCallback?
+        get() = definedExternally
+        set(value) = definedExternally
+    var formStateRestoreCallback: FormStateRestoreCallback?
+        get() = definedExternally
+        set(value) = definedExternally
+}
