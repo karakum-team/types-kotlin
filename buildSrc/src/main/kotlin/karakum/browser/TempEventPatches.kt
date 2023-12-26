7f0a6daa8f834ddf71a3c937dd563801655be0e3
Victor Turansky <victor.turansky@gmail.com>
2023-12-26 18:59:15 +0200

    Fix event type

--- buildSrc/src/main/kotlin/karakum/browser/TempEventPatches.kt
+++ buildSrc/src/main/kotlin/karakum/browser/TempEventPatches.kt
@@ -15,18 +15,43 @@ private val TYPE_MAP = listOf(
     EventCorrection("icecandidateerror", "RTCPeerConnectionIceErrorEvent"),
 
     EventCorrection("rtctransform", "RTCTransformEvent"),
+
+    EventCorrection("remove", "AnimationPlaybackEvent", "Animation"),
+    EventCorrection("statechange", "MIDIConnectionEvent", "MIDIAccess"),
+    EventCorrection("error", "ErrorEvent", "MediaRecorder"),
+    EventCorrection("error", "RTCErrorEvent", "RTCDataChannel"),
+    EventCorrection("error", "RTCErrorEvent", "RTCDtlsTransport"),
 )
 
 private data class EventCorrection(
     val name: String,
     val type: String,
+    val className: String? = null,
 )
 
 internal fun String.applyTempEventPatches(): String =
-    TYPE_MAP.fold(this) { acc, (name, type) ->
-        val result = acc.replaceFirst(""""$name": Event;""", """"$name": $type;""")
-
-        val handler = " on$name: "
-        val before = handler + result.substringAfter(handler).substringBefore("\n")
-        result.replaceFirst(before, before.replaceFirst(": Event)", ": $type)"))
+    TYPE_MAP.fold(this) { acc, correction ->
+        val className = correction.className
+        if (className != null) {
+            sequenceOf("", "EventMap")
+                .map { "\ninterface ${className}${it} " }
+                .map { prefix -> prefix + acc.substringAfter(prefix).substringBefore("\n}\n") }
+                .fold(acc) { localAcc, before ->
+                    localAcc.replace(before, applyCorrection(before, correction))
+                }
+        } else {
+            applyCorrection(acc, correction)
+        }
     }
+
+private fun applyCorrection(
+    source: String,
+    correction: EventCorrection,
+): String {
+    val (name, type) = correction
+    val result = source.replaceFirst(""""$name": Event;""", """"$name": $type;""")
+
+    val handler = " on$name: "
+    val before = handler + result.substringAfter(handler).substringBefore("\n")
+    return result.replaceFirst(before, before.replaceFirst(": Event)", ": $type)"))
+}
