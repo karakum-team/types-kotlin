d90fc26079cbf6c5471b5431a7d18a0bf935f9d2
Victor Turansky <victor.turansky@gmail.com>
2024-01-06 19:53:14 +0200

    Initial parameter type calculation

--- buildSrc/src/main/kotlin/karakum/browser/IDL.kt
+++ buildSrc/src/main/kotlin/karakum/browser/IDL.kt
@@ -2,7 +2,14 @@ package karakum.browser
 
 import java.io.File
 
-object IDLRegistry {
+internal data class ParameterData(
+    val className: String,
+    val methodName: String,
+    val parameterName: String,
+    val parameterType: String,
+)
+
+internal object IDLRegistry {
     lateinit var rootDirectory: File
 
     private val idlData: List<String> by lazy {
@@ -31,6 +38,62 @@ object IDLRegistry {
 
     private val typesWithHtmlConstructors: Set<String> by lazy {
         hasContent("[HTMLConstructor] constructor();")
+    }
+
+    /* private */ val parameterData: List<ParameterData> by lazy {
+        idlData.flatMap { content ->
+            content
+                .splitToSequence("\ninterface ", "\npartial interface ")
+                .drop(1)
+                .map { it.substringBefore("\n};") }
+                .flatMap { classBody ->
+                    val className = classBody.substringBefore(" ")
+                    classBody.substringAfter(" {\n")
+                        .splitToSequence("\n")
+                        .flatMap { line -> getParameterData(className = className, line = line) }
+                }
+        }
+    }
+
+    private fun getParameterData(
+        className: String,
+        line: String,
+    ): Sequence<ParameterData> {
+        val source = line
+            .substringAfter("(", "")
+            .substringBefore(")", "")
+            .ifEmpty { return emptySequence() }
+
+        val methodName = line
+            .substringBefore("(")
+            .substringAfterLast(" ")
+
+        return source
+            .splitToSequence(", ")
+            .mapNotNull { psource ->
+                val type = when (psource.substringBeforeLast(" ")) {
+                    "double",
+                    "unrestricted double",
+                    -> "Double"
+
+                    "long",
+                    "unsigned long",
+                    -> "Int"
+
+                    "long long",
+                    "unsigned long long",
+                    -> "JsLong"
+
+                    else -> return@mapNotNull null
+                }
+
+                ParameterData(
+                    className = className,
+                    methodName = methodName,
+                    parameterName = psource.substringAfterLast(" "),
+                    parameterType = type,
+                )
+            }
     }
 
     fun hasEmptyConstructor(type: String): Boolean =
