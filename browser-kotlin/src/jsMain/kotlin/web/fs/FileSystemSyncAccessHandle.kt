86ebc75489081729860fae9e24f104128a8b594c
Victor Turansky <victor.turansky@gmail.com>
2024-01-07 02:38:10 +0200

    Update declarations

--- browser-kotlin/src/jsMain/kotlin/web/fs/FileSystemSyncAccessHandle.kt
+++ browser-kotlin/src/jsMain/kotlin/web/fs/FileSystemSyncAccessHandle.kt
@@ -32,7 +32,7 @@ sealed external class FileSystemSyncAccessHandle {
     fun read(
         buffer: AllowSharedBufferSource,
         options: FileSystemReadWriteOptions = definedExternally,
-    ): Number
+    ): JsLong
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/FileSystemSyncAccessHandle/truncate)
@@ -45,5 +45,5 @@ sealed external class FileSystemSyncAccessHandle {
     fun write(
         buffer: AllowSharedBufferSource,
         options: FileSystemReadWriteOptions = definedExternally,
-    ): Number
+    ): JsLong
 }
