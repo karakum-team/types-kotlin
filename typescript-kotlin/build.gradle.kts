8e2fdd9cd98bca6a6bd26c106babb1e0f50a31a0
Victor Turansky <victor.turansky@gmail.com>
2024-01-13 16:03:20 +0200

    TypeScript. Add sync task

--- typescript-kotlin/build.gradle.kts
+++ typescript-kotlin/build.gradle.kts
@@ -18,3 +18,13 @@ dependencies {
     jsMainImplementation(wrappers("js"))
     jsMainImplementation(seskarCore())
 }
+
+val syncWithWrappers by tasks.creating(Sync::class) {
+    val generatedDir = project.layout.projectDirectory.dir("src/jsMain/kotlin")
+
+    val kotlinWrappersDir = project.rootProject.layout.projectDirectory.dir("../kotlin-wrappers")
+    val typescriptDir = kotlinWrappersDir.dir("kotlin-typescript/src/jsMain/generated")
+
+    from(generatedDir)
+    into(typescriptDir.asFile)
+}
