9a2a0183488d0e14bc7a7835a77b9175b3695341
Victor Turansky <victor.turansky@gmail.com>
2024-01-15 04:39:50 +0200

    Update declarations

--- typescript-kotlin/src/jsMain/kotlin/typescript/JsxNamespacedName.kt
+++ typescript-kotlin/src/jsMain/kotlin/typescript/JsxNamespacedName.kt
@@ -4,6 +4,6 @@ package typescript
 
 sealed external interface JsxNamespacedName : Node, Union.JsxNamespacedName_ {
     override val kind: SyntaxKind.JsxNamespacedName
-    override val name: Identifier
+    val name: Identifier
     val namespace: Identifier
 }
