80f933a3196798476c7b6936db4b2dbbbbd9efcc
Victor Turansky <victor.turansky@gmail.com>
2024-01-21 22:41:50 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/abort/Abortable.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+package web.abort
+
+@JsExternalInheritorsOnly
+external interface Abortable {
+    /**
+     * When provided the corresponding `AbortController` can be used to cancel an asynchronous action.
+     */
+    var signal: AbortSignal?
+}
