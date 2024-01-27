7c322708dbeffa32eaad158be6310a39d93f32a4
Victor Turansky <victor.turansky@gmail.com>
2024-01-28 01:59:58 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/scheduling/cancelIdleCallback.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package web.scheduling
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/cancelIdleCallback)
+ */
+external fun cancelIdleCallback(
+    handle: Int,
+)
---
+++ browser-kotlin/src/jsMain/kotlin/web/scheduling/requestIdleCallback.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+package web.scheduling
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/requestIdleCallback)
+ */
+external fun requestIdleCallback(
+    callback: IdleRequestCallback,
+    options: IdleRequestOptions? = definedExternally,
+): Int
