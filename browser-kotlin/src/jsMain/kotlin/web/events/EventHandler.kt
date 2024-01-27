9dbcdc6d75b722d43d7aeb6ef162c959290dee88
Victor Turansky <victor.turansky@gmail.com>
2024-01-27 15:27:57 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/events/EventHandler.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package web.events
+
+typealias EventHandler<T /* : Event */> = (
+    event: T,
+) -> Unit
---
+++ browser-kotlin/src/jsMain/kotlin/web/events/EventTarget.kt
@@ -0,0 +1,86 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+    "INLINE_EXTERNAL_DECLARATION",
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+    "DECLARATION_CANT_BE_INLINED",
+    "NOTHING_TO_INLINE",
+)
+
+package web.events
+
+open external class EventTarget {
+    fun <T : Event> addEventListener(
+        type: EventType<T>,
+        callback: EventHandler<T>,
+        options: AddEventListenerOptions? = definedExternally,
+    )
+
+    inline fun <T : Event> addEventHandler(
+        type: EventType<T>,
+        noinline handler: EventHandler<T>,
+    ): () -> Unit =
+        addEventHandler(
+            target = this,
+            type = type,
+            handler = handler,
+        )
+
+    inline fun <T : Event> addEventHandler(
+        type: EventType<T>,
+        options: AddEventListenerOptions?,
+        noinline handler: EventHandler<T>,
+    ): () -> Unit =
+        addEventHandler(
+            target = this,
+            type = type,
+            handler = handler,
+            options = options,
+        )
+
+    fun <T : Event> removeEventListener(
+        type: EventType<T>,
+        callback: EventHandler<T>,
+        options: EventListenerOptions? = definedExternally,
+    )
+
+    fun dispatchEvent(
+        event: Event,
+    ): Boolean
+}
+
+@PublishedApi
+internal fun <T : Event> addEventHandler(
+    target: EventTarget,
+    type: EventType<T>,
+    handler: EventHandler<T>,
+): () -> Unit =
+    addEventHandler(
+        target = target,
+        type = type,
+        handler = handler,
+        options = undefined,
+    )
+
+@PublishedApi
+internal fun <T : Event> addEventHandler(
+    target: EventTarget,
+    type: EventType<T>,
+    handler: EventHandler<T>,
+    options: AddEventListenerOptions?,
+): () -> Unit {
+    target.addEventListener(
+        type = type,
+        callback = handler,
+        options = options,
+    )
+
+    return {
+        target.removeEventListener(
+            type = type,
+            callback = handler,
+            options = options,
+        )
+    }
+}
