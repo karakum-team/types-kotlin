bd175ef23588c9f2123c08f24ef52d1a57dcd9b6
Victor Turansky <victor.turansky@gmail.com>
2024-02-12 23:12:02 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/clipboard/ClipboardEventInit.temp.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package web.clipboard
+
+import web.data.DataTransfer
+import web.events.EventInitMutable
+
+external interface ClipboardEventInitMutable : EventInitMutable {
+    var clipboardData: DataTransfer?
+}
---
+++ browser-kotlin/src/jsMain/kotlin/web/uievents/UIEventInit.temp.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+package web.uievents
+
+import web.events.EventInitMutable
+import web.window.Window
+
+external interface UIEventInitMutable : EventInitMutable {
+    var detail: Int?
+    var view: Window?
+}
