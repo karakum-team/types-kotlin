47304d8ef3480c069d96702618c48ab525e3fdbb
Victor Turansky <victor.turansky@gmail.com>
2024-02-28 03:37:43 +0200

    Events. Strict `EventTarget`

--- buildSrc/src/main/kotlin/karakum/browser/EventTarget.kt
+++ buildSrc/src/main/kotlin/karakum/browser/EventTarget.kt
@@ -5,13 +5,13 @@ internal const val EVENT_TARGET = "EventTarget"
 // language=kotlin
 private val EVENT_TARGET_BODY: String = """
 open external class EventTarget {
-    fun <E : Event<*>> addEventListener(
+    fun <E : Event<EventTarget>> addEventListener(
         type: EventType<E>,
         callback: EventHandler<E>,
         options: AddEventListenerOptions? = definedExternally,
     )
 
-    inline fun <E : Event<*>> addEventHandler(
+    inline fun <E : Event<EventTarget>> addEventHandler(
         type: EventType<E>,
         noinline handler: EventHandler<E>,
     ): () -> Unit =
@@ -21,7 +21,7 @@ open external class EventTarget {
             handler = handler,
         )
 
-    inline fun <E : Event<*>> addEventHandler(
+    inline fun <E : Event<EventTarget>> addEventHandler(
         type: EventType<E>,
         options: AddEventListenerOptions?,
         noinline handler: EventHandler<E>,
@@ -33,7 +33,7 @@ open external class EventTarget {
             options = options,
         )
 
-    fun <E : Event<*>> removeEventListener(
+    fun <E : Event<EventTarget>> removeEventListener(
         type: EventType<E>,
         callback: EventHandler<E>,
         options: EventListenerOptions? = definedExternally,
@@ -45,7 +45,7 @@ open external class EventTarget {
 }
 
 @PublishedApi
-internal fun <E : Event<*>> addEventHandler(
+internal fun <E : Event<EventTarget>> addEventHandler(
     target: EventTarget,
     type: EventType<E>,
     handler: EventHandler<E>,
@@ -58,7 +58,7 @@ internal fun <E : Event<*>> addEventHandler(
     )
 
 @PublishedApi
-internal fun <E : Event<*>> addEventHandler(
+internal fun <E : Event<EventTarget>> addEventHandler(
     target: EventTarget,
     type: EventType<E>,
     handler: EventHandler<E>,
