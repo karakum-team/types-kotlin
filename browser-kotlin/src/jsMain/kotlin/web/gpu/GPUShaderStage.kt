c75aded3497a7c6e8947deb179fb1626d66d6b9e
Victor Turansky <victor.turansky@gmail.com>
2024-03-02 18:46:50 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/gpu/GPUShaderStage.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package web.gpu
+
+external interface GPUShaderStage {
+    companion object {
+        val VERTEX: GPUShaderStage              // 0x1
+        val FRAGMENT: GPUShaderStage            // 0x2
+        val COMPUTE: GPUShaderStage             // 0x4
+    }
+}
