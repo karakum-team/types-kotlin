ba8ff3ebc92672e9ff96cbbb3ecc7c7a591d6f77
Victor Turansky <victor.turansky@gmail.com>
2024-03-04 01:19:36 +0200

    Cesium. Sort imports

--- buildSrc/src/main/kotlin/karakum/cesium/Package.kt
+++ buildSrc/src/main/kotlin/karakum/cesium/Package.kt
@@ -3,8 +3,18 @@ package karakum.cesium
 internal val DEFAULT_PACKAGE: String = """
 package cesium
 
+import js.array.ReadonlyArray
+import js.buffer.ArrayBuffer
+import js.core.Void
+import js.errors.JsError
+import js.objects.ReadonlyRecord
+import js.objects.jso
 import js.promise.Promise
-import web.images.ImageData
+import js.typedarrays.Float32Array
+import js.typedarrays.Float64Array
+import js.typedarrays.Uint16Array
+import js.typedarrays.Uint8Array
+import web.blob.Blob
 import web.dom.Document
 import web.dom.Element
 import web.html.HTMLCanvasElement
@@ -12,17 +22,7 @@ import web.html.HTMLElement
 import web.html.HTMLIFrameElement
 import web.html.HTMLImageElement
 import web.html.HTMLVideoElement
+import web.images.ImageData
 import web.xml.XMLDocument
-import js.buffer.ArrayBuffer
-import js.objects.jso
-import js.array.ReadonlyArray
-import js.objects.ReadonlyRecord
-import js.core.Void
-import js.errors.JsError
-import js.typedarrays.Float32Array
-import js.typedarrays.Float64Array
-import js.typedarrays.Uint16Array
-import js.typedarrays.Uint8Array
-import web.blob.Blob
 
 """.trimStart()
