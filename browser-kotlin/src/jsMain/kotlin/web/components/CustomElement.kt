7ba5f6872ff3e8ed0912e994b4b1b364eeada811
Victor Turansky <victor.turansky@gmail.com>
2024-03-09 02:18:10 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/components/CustomElement.kt
@@ -0,0 +1,43 @@
+// Automatically generated - do not modify!
+
+package web.components
+
+external interface CustomElement {
+    interface WithFormCallbacks :
+        WithConnectedCallback,
+        WithDisconnectedCallback,
+        WithAdoptedCallback,
+        WithAttributeChangedCallback
+
+    interface WithConnectedCallback {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_components#connectedcallback)
+         */
+        fun connectedCallback()
+    }
+
+    interface WithDisconnectedCallback {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_components#disconnectedcallback)
+         */
+        fun disconnectedCallback()
+    }
+
+    interface WithAdoptedCallback {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_components#adoptedcallback)
+         */
+        fun adoptedCallback()
+    }
+
+    interface WithAttributeChangedCallback {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_components#attributechangedcallback)
+         */
+        fun attributeChangedCallback(
+            name: String,
+            oldValue: Any?,
+            newValue: Any?,
+        )
+    }
+}
