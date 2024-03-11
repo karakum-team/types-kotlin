a53efecdf991a76ed83622d2f55d9190411fa202
Victor Turansky <victor.turansky@gmail.com>
2024-03-11 03:06:49 +0200

    Cesium. Add sync task

--- cesium-kotlin/build.gradle.kts
+++ cesium-kotlin/build.gradle.kts
@@ -10,3 +10,13 @@ dependencies {
 
     jsMainImplementation(wrappers("browser"))
 }
+
+val syncWithWrappers by tasks.creating(Sync::class) {
+    val generatedDir = project.layout.projectDirectory.dir("src/jsMain/kotlin")
+
+    val kotlinWrappersDir = project.rootProject.layout.projectDirectory.dir("../kotlin-wrappers")
+    val typescriptDir = kotlinWrappersDir.dir("kotlin-cesium/src/jsMain/generated")
+
+    from(generatedDir)
+    into(typescriptDir.asFile)
+}
