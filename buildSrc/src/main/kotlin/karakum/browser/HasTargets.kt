d3b6396b9b8aca2e50b08c846b996461b8df2d14
Victor Turansky <victor.turansky@gmail.com>
2024-03-16 20:25:11 +0200

    Events. Strict `HasTargets.target`

--- buildSrc/src/main/kotlin/karakum/browser/HasTargets.kt
+++ buildSrc/src/main/kotlin/karakum/browser/HasTargets.kt
@@ -5,6 +5,7 @@ internal const val HAS_TARGETS = "HasTargets"
 private val HAS_TARGETS_BODY: String = """
 external interface $HAS_TARGETS<C : EventTarget> {
     val currentTarget: C
+    val target: EventTarget
 }
 """.trimIndent()
 
