d5c5896876271256c9f5514f7086d3ef3f5ede62
Victor Turansky <victor.turansky@gmail.com>
2024-03-17 02:19:40 +0200

    Build. Serialization in `buildSrc`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,21 @@
+import java.util.*
+
 plugins {
     `kotlin-dsl`
 }
 
 repositories {
     mavenCentral()
+}
+
+val props = Properties().apply {
+    file("../gradle.properties").inputStream().use { load(it) }
+}
+
+fun version(target: String): String =
+    props.getProperty("${target}.version")
+
+dependencies {
+    implementation(kotlin("serialization", version("kotlin")))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${version("kotlinx-serialization")}")
 }
