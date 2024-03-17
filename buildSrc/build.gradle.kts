984a701a457f056c39cf33665c527c8b661b845f
Victor Turansky <victor.turansky@gmail.com>
2024-03-17 02:39:57 +0200

    Update serialization plugin configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,8 @@ import java.util.*
 
 plugins {
     `kotlin-dsl`
+    // TEMP
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 repositories {
@@ -16,6 +18,5 @@ fun version(target: String): String =
     props.getProperty("${target}.version")
 
 dependencies {
-    implementation(kotlin("serialization", version("kotlin")))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${version("kotlinx-serialization")}")
 }
