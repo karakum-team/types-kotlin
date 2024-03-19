fdf9d1001b5082cb8bbaf1d14421fcebccf1a035
Victor Turansky <victor.turansky@gmail.com>
2024-03-20 00:35:58 +0200

    Update declarations

--- react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/TableFeature.kt
+++ react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/TableFeature.kt
@@ -2,13 +2,13 @@
 
 package tanstack.table.core
 
-external interface TableFeature<TData : RowData = any>{
-    var createCell: ((cell: Cell<TData, unknown>, column: Column<TData>, row: Row<TData>, table: Table<TData>) -> Unit)?
+external interface TableFeature<TData : RowData> {
+    var createCell: ((cell: Cell<TData, *>, column: Column<TData, *>, row: Row<TData>, table: Table<TData>) -> Unit)?
     var createColumn: ((column: Column<TData, *>, table: Table<TData>) -> Unit)?
-    var createHeader: ((header: Header<TData, unknown>, table: Table<TData>) -> Unit)?
+    var createHeader: ((header: Header<TData, *>, table: Table<TData>) -> Unit)?
     var createRow: ((row: Row<TData>, table: Table<TData>) -> Unit)?
     var createTable: ((table: Table<TData>) -> Unit)?
-    var getDefaultColumnDef: (() -> Partial<ColumnDef<TData, unknown>>)?
-    var getDefaultOptions: ((table: Table<TData>) -> Partial<TableOptionsResolved<TData>>)?
-    var getInitialState: ((initialState: InitialTableState?) -> Partial<TableState>)?
+    var getDefaultColumnDef: (() -> ColumnDef<TData, *>)?
+    var getDefaultOptions: ((table: Table<TData>) -> TableOptionsResolved<TData>)?
+    var getInitialState: ((initialState: InitialTableState?) -> TableState)?
 }
