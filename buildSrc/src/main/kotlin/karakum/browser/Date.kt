16d77bbe3150e20b787dde8dc31ccebedb5f331e
Victor Turansky <victor.turansky@gmail.com>
2024-04-06 19:22:01 +0300

    Initial `Date` generation

---
+++ buildSrc/src/main/kotlin/karakum/browser/Date.kt
@@ -0,0 +1,56 @@
+package karakum.browser
+
+import java.io.File
+
+private const val DATE = "Date"
+
+internal fun dateDeclarations(
+    definitionsDir: File,
+): Sequence<ConversionResult> {
+    val rawMembers = dateRawContent(definitionsDir, DATE)
+    val rawStaticMembers = dateRawContent(definitionsDir, "DateConstructor")
+
+    val body = """
+    external class $DATE() {
+        $rawMembers
+        $rawStaticMembers
+    }
+    """.trimIndent()
+
+    return sequenceOf(
+        ConversionResult(
+            name = DATE,
+            body = body,
+            pkg = "js.date",
+        )
+    )
+}
+
+private fun dateRawContent(
+    definitionsDir: File,
+    interfaceName: String,
+): String =
+    definitionsDir.listFiles()!!
+        .filter { it.name.endsWith(".date.d.ts") || it.name.endsWith(".core.d.ts") || it.name == "lib.es5.d.ts" }
+        .sortedBy { file ->
+            file.name
+                .removePrefix("lib.es")
+                .substringBefore(".")
+                .toIntOrNull()
+                ?: 3000
+        }
+        .map {
+            it.readText()
+                .replace("\r\n", "\n")
+                .substringAfter("\ninterface $interfaceName {\n", "")
+                .substringBefore("\n}")
+                .trimIndent()
+                .replace("\n\n", "\n")
+        }
+        .filter { it.isNotEmpty() }
+        .joinToString("\n")
+        .splitUnion("Date | number | bigint")
+        .splitUnion(
+            "LocalesArgument",
+            "UnicodeBCP47LocaleIdentifier | Locale | UnicodeBCP47LocaleIdentifier[] | Locale[]",
+        )
