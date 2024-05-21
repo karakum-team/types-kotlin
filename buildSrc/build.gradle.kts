3670a75467f0a6f63e263ac85f62ea7c74584edf
Victor Turansky <victor.turansky@gmail.com>
2024-05-22 02:33:41 +0300

    Kotlin `2.0.0`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ import java.util.*
 plugins {
     `kotlin-dsl`
     // TEMP
-    kotlin("plugin.serialization") version "1.9.24"
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 repositories {
