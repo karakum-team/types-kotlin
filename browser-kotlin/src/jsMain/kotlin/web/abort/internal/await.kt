940237ab456a3ae3a2c6bc1ed33a11f9a0cd0411
Victor Turansky <victor.turansky@gmail.com>
2024-05-25 13:30:25 +0300

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/abort/internal/await.kt
@@ -0,0 +1,14 @@
+// Automatically generated - do not modify!
+
+package web.abort.internal
+
+internal fun patchAbortOptions(
+    options: Any?,
+    controller: Any,
+): Any = TODO("--placeholder--")
+
+@Suppress("RedundantSuppressModifier")
+internal suspend fun awaitPromiseLike(
+    promise: Any?,
+    controller: Any?,
+): Any = TODO("--placeholder--")
