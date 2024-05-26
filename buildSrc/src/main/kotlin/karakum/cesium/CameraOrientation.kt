0efa52738c531d2ca1173c13a20f53cf7474241b
Victor Turansky <victor.turansky@gmail.com>
2024-05-26 11:45:04 +0300

    Cesium. `CameraOrientation` factories removed

--- buildSrc/src/main/kotlin/karakum/cesium/CameraOrientation.kt
+++ buildSrc/src/main/kotlin/karakum/cesium/CameraOrientation.kt
@@ -2,31 +2,10 @@ package karakum.cesium
 
 // language=Kotlin
 private const val CODE: String = """
-import js.objects.jso
+import js.objects.JsPlainObject
 
+@JsPlainObject
 sealed external interface CameraOrientation
-
-fun CameraOrientation(
-    heading: Double,
-    pitch: Double,
-    roll: Double,
-): CameraOrientation {
-    val o: HeadingPitchRollValues = jso()
-    o.heading = heading
-    o.pitch = pitch
-    o.roll = roll
-    return o
-}
-
-fun CameraOrientation(
-    direction: Cartesian3,
-    up: Cartesian3,
-): CameraOrientation {
-    val o: DirectionUp = jso()
-    o.direction = direction
-    o.up = up
-    return o
-}
 """
 
 internal object CameraOrientation : Declaration() {
