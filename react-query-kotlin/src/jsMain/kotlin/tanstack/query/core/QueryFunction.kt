aca233aad41a096ddc9c9f7942e6e45138e7f543
Victor Turansky <victor.turansky@gmail.com>
2024-06-16 14:22:46 +0300

    Update declarations

--- react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/QueryFunction.kt
+++ react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/QueryFunction.kt
@@ -1,7 +1,16 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package tanstack.query.core
 
 import js.promise.PromiseResult
 
-typealias QueryFunction<T, TQueryKey, TPageParam> = (context: QueryFunctionContext<TQueryKey, TPageParam>) -> PromiseResult<T>
+sealed external interface QueryFunction<T, TQueryKey : QueryKey, TPageParam>
+
+inline fun <T, TQueryKey : QueryKey, TPageParam> QueryFunction(
+    noinline value: (context: QueryFunctionContext<TQueryKey, TPageParam>) -> PromiseResult<T>,
+): QueryFunction<T, TQueryKey, TPageParam> =
+    value.unsafeCast<QueryFunction<T, TQueryKey, TPageParam>>()
