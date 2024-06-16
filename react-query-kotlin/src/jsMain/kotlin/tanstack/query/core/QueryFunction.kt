23664182fb7579b7b8f187345ade48e422a7dfc2
Victor Turansky <victor.turansky@gmail.com>
2024-06-16 14:44:45 +0300

    Update declarations

--- react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/QueryFunction.kt
+++ react-query-kotlin/src/jsMain/kotlin/tanstack/query/core/QueryFunction.kt
@@ -8,7 +8,11 @@ package tanstack.query.core
 
 import js.promise.PromiseResult
 
-sealed external interface QueryFunction<T, TQueryKey : QueryKey, TPageParam>
+@JsExternalInheritorsOnly
+sealed external interface QueryFunctionOrSkipToken<out T, out TQueryKey : QueryKey, out TPageParam>
+
+sealed external interface QueryFunction<out T, out TQueryKey : QueryKey, out TPageParam> :
+    QueryFunctionOrSkipToken<T, TQueryKey, TPageParam>
 
 inline fun <T, TQueryKey : QueryKey, TPageParam> QueryFunction(
     noinline value: (context: QueryFunctionContext<TQueryKey, TPageParam>) -> PromiseResult<T>,
