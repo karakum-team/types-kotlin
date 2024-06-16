1fd9bb3c460d8a5f998b0d5a828ed125559ca587
Victor Turansky <victor.turansky@gmail.com>
2024-06-16 14:58:43 +0300

    Build. Fix `SyncWrappers` declaration

--- buildSrc/src/main/kotlin/SyncWrappers.kt
+++ buildSrc/src/main/kotlin/SyncWrappers.kt
@@ -1,8 +1,10 @@
 import org.gradle.api.file.Directory
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Sync
 import java.io.File
 
 open class SyncWrappers : Sync() {
+    @get:Internal
     val generatedDir: Directory
         get() = project.layout.projectDirectory.dir("src/jsMain/kotlin")
 
