03c7d0156ff9b8c287370534f756fd685e315c76
Victor Turansky <victor.turansky@gmail.com>
2024-06-16 13:24:41 +0300

    Build. Add `SyncWrappers.kotlinWrappersDir` function

--- buildSrc/src/main/kotlin/SyncWrappers.kt
+++ buildSrc/src/main/kotlin/SyncWrappers.kt
@@ -1,7 +1,17 @@
 import org.gradle.api.file.Directory
 import org.gradle.api.tasks.Sync
+import java.io.File
 
 class SyncWrappers : Sync() {
     val generatedDir: Directory
         get() = project.layout.projectDirectory.dir("src/jsMain/kotlin")
+
+    fun kotlinWrappersDir(
+        projectName: String,
+    ): File =
+        project.rootProject.layout.projectDirectory
+            .dir("../kotlin-wrappers")
+            .dir(projectName)
+            .dir("src/jsMain/generated")
+            .asFile
 }
