c975cad7c5f6b5a1b92c0ea4001b2c85b97108be
porotkin <preload737773@gmail.com>
2024-07-06 12:34:13 +0300

    Common. Inline quote consts. Use Typography const

--- buildSrc/src/main/kotlin/karakum/common/Strings.kt
+++ buildSrc/src/main/kotlin/karakum/common/Strings.kt
@@ -6,9 +6,6 @@ private val SLASH_LETTER = Regex("""\/(\w)""")
 private val UNDERSCORE_LETTER = Regex("""_(\w)""")
 private val SPACE_LETTER = Regex("""\s(\w)""")
 
-private val SINGLE_QUOTE_LETTER = "\'"
-private val DOUBLE_QUOTE_LETTER = "\""
-
 private val toUpperCase: (MatchResult) -> CharSequence = {
     it.groupValues[1].uppercase()
 }
@@ -32,8 +29,8 @@ internal fun String.replaceSuffix(
     return removeSuffix(from) + to
 }
 
-internal fun String.removeQuoteSurrounding(): String = removeSurrounding(SINGLE_QUOTE_LETTER)
-    .removeSurrounding(DOUBLE_QUOTE_LETTER)
+internal fun String.removeQuoteSurrounding(): String = removeSurrounding("'")
+    .removeSurrounding(Typography.quote.toString())
 
 internal fun Sequence<String>.removeQuoteSurrounding(): Sequence<String> =
     map { it.removeQuoteSurrounding() }
