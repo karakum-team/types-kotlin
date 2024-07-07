bfe9fa0a43723b941d55a993dcb52264cfbfd439
Victor Turansky <victor.turansky@gmail.com>
2024-07-07 15:57:25 +0300

    Events. Update `EventInstance`

--- buildSrc/src/main/kotlin/karakum/browser/EventInstance.kt
+++ buildSrc/src/main/kotlin/karakum/browser/EventInstance.kt
@@ -4,23 +4,18 @@ internal const val EVENT_INSTANCE = "EventInstance"
 
 // language=kotlin
 private val EVENT_INSTANCE_BODY: String = """
+import kotlinx.coroutines.*
+import kotlin.coroutines.resume
+
 class EventInstance<out E : Event, out C : EventTarget, out T : EventTarget>(
     internal val target: C,
     internal val type: EventType<E, C>,
 )
 
-// event handler
+// addHandler
 fun <E : Event, C : EventTarget, T : EventTarget> EventInstance<E, C, T>.addHandler(
     handler: EventHandler<E, C, T>,
-): () -> Unit =
-    target.addEventHandler(
-        type = type,
-        handler = handler,
-    )
-
-fun <E : Event, C : EventTarget, T : EventTarget> EventInstance<E, C, T>.addHandler(
-    options: AddEventListenerOptions?,
-    handler: EventHandler<E, C, T>,
+    options: AddEventListenerOptions? = undefined,
 ): () -> Unit =
     target.addEventHandler(
         type = type,
@@ -28,15 +23,13 @@ fun <E : Event, C : EventTarget, T : EventTarget> EventInstance<E, C, T>.addHand
         handler = handler,
     )
 
-// event + targets
 fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C, T>.addHandler(
     handler: (D) -> Unit,
 ): () -> Unit
         where D : E,
               D : HasTargets<C, T> =
-    target.addEventHandler(
-        type = type,
-        handler = handler,
+    addHandler(
+        handler = EventHandler(handler),
     )
 
 fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C, T>.addHandler(
@@ -45,11 +38,61 @@ fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C, T>.addH
 ): () -> Unit
         where D : E,
               D : HasTargets<C, T> =
-    target.addEventHandler(
-        type = type,
+    addHandler(
+        handler = EventHandler(handler),
         options = options,
-        handler = handler,
     )
+
+// subscribe
+suspend fun <E : Event, C : EventTarget, T : EventTarget> EventInstance<E, C, T>.subscribe(
+    handler: EventHandler<E, C, T>,
+    options: AddEventListenerOptions? = undefined,
+): Job =
+    CoroutineScope(currentCoroutineContext()).launch {
+        suspendCancellableCoroutine<Nothing> { continuation ->
+            val unsubscribe = addHandler(handler, options)
+
+            continuation.invokeOnCancellation {
+                unsubscribe()
+            }
+        }
+    }
+
+suspend fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C, T>.subscribe(
+    handler: (D) -> Unit,
+): Job
+        where D : E,
+              D : HasTargets<C, T> =
+    subscribe(
+        handler = EventHandler(handler),
+    )
+
+suspend fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C, T>.subscribe(
+    options: AddEventListenerOptions?,
+    handler: (D) -> Unit,
+): Job
+        where D : E,
+              D : HasTargets<C, T> =
+    subscribe(
+        handler = EventHandler(handler),
+        options = options,
+    )
+
+// once
+suspend fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C, T>.once(): D
+        where D : E,
+              D : HasTargets<C, T> {
+    return suspendCancellableCoroutine { continuation ->
+        val unsubscribe = addHandler(
+            handler = continuation::resume,
+            options = AddEventListenerOptions(once = true),
+        )
+
+        continuation.invokeOnCancellation {
+            unsubscribe()
+        }
+    }
+}
 """.trimIndent()
 
 internal fun EventInstance(): ConversionResult =
