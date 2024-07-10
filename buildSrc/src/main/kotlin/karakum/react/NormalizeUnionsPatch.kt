ce6e47a8796d327a0457b9e945ff746ef28dfac4
porotkin <preload737773@gmail.com>
2024-07-10 15:52:47 +0300

    React DOM. Fix regex

--- buildSrc/src/main/kotlin/karakum/react/NormalizeUnionsPatch.kt
+++ buildSrc/src/main/kotlin/karakum/react/NormalizeUnionsPatch.kt
@@ -1,7 +1,7 @@
 package karakum.react
 
 private fun String.normalizeUnions(): String =
-    replace(Regex("\n *\\|"), " |")
+    replace(Regex("""\n\s+\| """), " | ")
 
 internal fun String.applyNormalizeUnionsPatch(): String = this
     .normalizeUnions()
