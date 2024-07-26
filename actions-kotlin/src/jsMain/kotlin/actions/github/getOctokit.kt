5fe3098a826d6fda5a3fdd83807c7a63a0d3cc50
Victor Turansky <victor.turansky@gmail.com>
2024-07-27 02:27:55 +0300

    Update declarations

--- actions-kotlin/src/jsMain/kotlin/actions/github/getOctokit.kt
+++ actions-kotlin/src/jsMain/kotlin/actions/github/getOctokit.kt
@@ -4,11 +4,10 @@
 
 package actions.github
 
-import js.array.ReadonlyArray
 import js.objects.ReadonlyRecord
 
 external fun getOctokit(
     token: String,
     options: ReadonlyRecord<String, Any> /* OctokitOptions */ = definedExternally,
-    vararg additionalPlugins: ReadonlyArray<Function<Any> /* OctokitPlugin */>,
+    vararg additionalPlugins: Function<Any>, /* OctokitPlugin */
 ): dynamic /* InstanceType<typeof GitHub> */
