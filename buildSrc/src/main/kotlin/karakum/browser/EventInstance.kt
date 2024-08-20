9bdf44d1809befc7ca72e46da0ad9694fbade8bd
Victor Turansky <victor.turansky@gmail.com>
2024-08-20 16:41:18 +0300

    Events. Add `EventInstance` extensions

--- buildSrc/src/main/kotlin/karakum/browser/EventInstance.kt
+++ buildSrc/src/main/kotlin/karakum/browser/EventInstance.kt
@@ -4,7 +4,12 @@ internal const val EVENT_INSTANCE = "EventInstance"
 
 // language=kotlin
 private val EVENT_INSTANCE_BODY: String = """
+import js.iterable.SuspendableIterator
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.coroutines.resume
 
 class EventInstance<out E : Event, out C : EventTarget, out T : EventTarget>(
@@ -94,6 +99,32 @@ suspend fun <E : Event, C : EventTarget, T : EventTarget, D> EventInstance<E, C,
         }
     }
 }
+
+// iterator
+suspend operator fun <E : Event, T : EventTarget, D> EventInstance<E, *, T>.iterator(): SuspendableIterator<D>
+        where D : E,
+              D : HasTargets<*, T> =
+    SuspendableIterator(asChannel<_, _, D>().iterator())
+
+// channel
+internal suspend fun <E : Event, T : EventTarget, D> EventInstance<E, *, T>.asChannel(): ReceiveChannel<D>
+        where D : E,
+              D : HasTargets<*, T> {
+    val channel = Channel<D>()
+    val job = subscribe(channel::trySend)
+    channel.invokeOnClose { job.cancel() }
+    return channel
+}
+
+// asFlow
+fun <E : Event, T : EventTarget, D> EventInstance<E, *, T>.asFlow(): Flow<D>
+        where D : E,
+              D : HasTargets<*, T> =
+    flow {
+        for (event in asChannel<_, _, D>()) {
+            emit(event)
+        }
+    }
 """.trimIndent()
 
 internal fun EventInstance(): ConversionResult =
