09cd4570826d74d16edffab324f49db3c5ec964f
Victor Turansky <victor.turansky@gmail.com>
2024-10-19 15:27:10 +0300

    Update declarations

--- react-query-kotlin/src/jsMain/kotlin/tanstack/react/query/HydrationBoundaryProps.kt
+++ react-query-kotlin/src/jsMain/kotlin/tanstack/react/query/HydrationBoundaryProps.kt
@@ -6,7 +6,7 @@ import tanstack.query.core.HydrateOptions
 import tanstack.query.core.QueryClient
 
 external interface HydrationBoundaryProps : react.PropsWithChildren {
-    var state: Any??
+    var state: Any?
     var options: HydrateOptions /* OmitKeyof<HydrateOptions, 'defaultOptions'> & { defaultOptions?: OmitKeyof<Exclude<HydrateOptions['defaultOptions'], undefined>, 'mutations'>; } */?
     override var children: react.ReactNode?
     var queryClient: QueryClient?
