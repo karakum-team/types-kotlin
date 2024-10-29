fd519059c9bfd0bebe2a1e96dfee8501b24932dd
Victor Turansky <victor.turansky@gmail.com>
2024-10-30 01:15:11 +0200

    Update declarations

---
+++ browser-kotlin/src/jsMain/kotlin/web/events/JsEvent.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package web.events
+
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.BINARY)
+annotation class JsEvent(
+    val type: String,
+)
