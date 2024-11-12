891cb823d0dca98e3db3d1fc6bf652e3e3a940e2
Victor Turansky <victor.turansky@gmail.com>
2024-11-12 21:25:34 +0200

    Version Catalog. Fix names

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
         create("libs") {
             val kotlinVersion = extra["kotlin.version"] as String
             plugin("kotlin-multiplatform", "org.jetbrains.kotlin.multiplatform").version(kotlinVersion)
-            plugin("kotlin-js-plain-objects", "org.jetbrains.kotlin.plugin.js-plain-objects").version(kotlinVersion)
+            plugin("kotlin-jsPlainObjects", "org.jetbrains.kotlin.plugin.js-plain-objects").version(kotlinVersion)
 
             val seskarVersion = extra["seskar.version"] as String
             plugin("seskar", "io.github.turansky.seskar").version(seskarVersion)
