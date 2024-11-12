6740d6b8838e5034eafae11a641a0c01c83110f2
Victor Turansky <victor.turansky@gmail.com>
2024-11-12 22:51:36 +0200

    Fix aliases

--- buildSrc/src/main/kotlin/karakum/browser/Lists.kt
+++ buildSrc/src/main/kotlin/karakum/browser/Lists.kt
@@ -1,9 +1,9 @@
 package karakum.browser
 
 private val LISTS_BODY = """
-typealias Float32List = Float32Array /* | GLfloat[] */
-typealias Int32List = Int32Array /* | GLint[] */
-typealias Uint32List = Uint32Array /* | GLuint[] */
+typealias Float32List = Float32Array<*> /* | GLfloat[] */
+typealias Int32List = Int32Array<*> /* | GLint[] */
+typealias Uint32List = Uint32Array<*> /* | GLuint[] */
 """.trimIndent()
 
 internal fun Lists(): ConversionResult =
