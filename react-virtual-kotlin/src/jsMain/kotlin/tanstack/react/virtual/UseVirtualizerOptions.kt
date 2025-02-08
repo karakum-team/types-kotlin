0bb8b9e7ee307bc2555212aace57dba66876d412
Victor Turansky <victor.turansky@gmail.com>
2025-02-08 19:51:19 +0200

    Update declarations

---
+++ react-virtual-kotlin/src/jsMain/kotlin/tanstack/react/virtual/UseVirtualizerOptions.kt
@@ -0,0 +1,42 @@
+// Automatically generated - do not modify!
+
+package tanstack.react.virtual
+
+import js.array.ReadonlyArray
+import js.objects.JsPlainObject
+import tanstack.virtual.core.*
+import web.dom.Element
+import web.dom.observers.ResizeObserverEntry
+import web.events.EventTarget
+
+@JsPlainObject
+external interface UseVirtualizerOptions<TScrollElement : EventTarget, TItemElement : Element> {
+    val count: Int
+    val getScrollElement: () -> TScrollElement?
+    val estimateSize: (index: Int) -> Int
+    val scrollToFn: (offset: Int, options: ScrollOptions, instance: Virtualizer<TScrollElement, TItemElement>) -> Unit
+    val observeElementOffset: (instance: Virtualizer<TScrollElement, TItemElement>, cb: ObserveOffsetCallBack) -> (() -> Unit)?
+    val debug: Boolean?
+    val initialRect: Rect?
+    val onChange: ((instance: Virtualizer<TScrollElement, TItemElement>, sync: Boolean) -> Unit)?
+    val measureElement: ((element: TItemElement, entry: ResizeObserverEntry?, instance: Virtualizer<TScrollElement, TItemElement>) -> Int)?
+    val overscan: Int?
+    val horizontal: Boolean?
+    val paddingStart: Int?
+    val paddingEnd: Int?
+    val scrollPaddingStart: Int?
+    val scrollPaddingEnd: Int?
+    val initialOffset: Int /* | (() -> Int) */?
+    val getItemKey: ((index: Int) -> Key)?
+    val rangeExtractor: ((range: Range) -> ReadonlyArray<Int>)?
+    val scrollMargin: Int?
+    val gap: Int?
+    val indexAttribute: String?
+    val initialMeasurementsCache: ReadonlyArray<VirtualItem>?
+    val lanes: Int?
+    val isScrollingResetDelay: Int?
+    val useScrollendEvent: Boolean?
+    val enabled: Boolean?
+    val isRtl: Boolean?
+    val useAnimationFrameWithResizeObserver: Boolean?
+}
---
+++ react-virtual-kotlin/src/jsMain/kotlin/tanstack/react/virtual/UseWindowVirtualizerOptions.kt
@@ -0,0 +1,41 @@
+// Automatically generated - do not modify!
+
+package tanstack.react.virtual
+
+import js.array.ReadonlyArray
+import js.objects.JsPlainObject
+import tanstack.virtual.core.*
+import web.dom.Element
+import web.dom.observers.ResizeObserverEntry
+import web.window.Window
+
+@JsPlainObject
+external interface UseWindowVirtualizerOptions<TItemElement : Element> {
+    val count: Int
+    val estimateSize: (index: Int) -> Int
+    val scrollToFn: (offset: Int, options: ScrollOptions, instance: Virtualizer<Window, TItemElement>) -> Unit
+    val observeElementOffset: (instance: Virtualizer<Window, TItemElement>, cb: ObserveOffsetCallBack) -> (() -> Unit)?
+    val debug: Boolean?
+    val initialRect: Rect?
+    val onChange: ((instance: Virtualizer<Window, TItemElement>, sync: Boolean) -> Unit)?
+    val measureElement: ((element: TItemElement, entry: ResizeObserverEntry?, instance: Virtualizer<Window, TItemElement>) -> Int)?
+    val overscan: Int?
+    val horizontal: Boolean?
+    val paddingStart: Int?
+    val paddingEnd: Int?
+    val scrollPaddingStart: Int?
+    val scrollPaddingEnd: Int?
+    val initialOffset: Int /* | (() -> Int) */?
+    val getItemKey: ((index: Int) -> Key)?
+    val rangeExtractor: ((range: Range) -> ReadonlyArray<Int>)?
+    val scrollMargin: Int?
+    val gap: Int?
+    val indexAttribute: String?
+    val initialMeasurementsCache: ReadonlyArray<VirtualItem>?
+    val lanes: Int?
+    val isScrollingResetDelay: Int?
+    val useScrollendEvent: Boolean?
+    val enabled: Boolean?
+    val isRtl: Boolean?
+    val useAnimationFrameWithResizeObserver: Boolean?
+}
