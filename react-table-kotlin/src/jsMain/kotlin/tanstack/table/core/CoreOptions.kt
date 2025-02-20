19027111ccc360daa872e906c9afe45811223095
porotkin <preload737773@gmail.com>
2025-02-20 18:05:05 +0200

    Update declarations

--- react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/CoreOptions.kt
+++ react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/CoreOptions.kt
@@ -3,7 +3,6 @@
 package tanstack.table.core
 
 import js.array.ReadonlyArray
-import js.objects.ReadonlyRecord
 
 external interface CoreOptions<TData : RowData> {
     /**
@@ -126,7 +125,7 @@ external interface CoreOptions<TData : RowData> {
      * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#meta)
      * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
      */
-    var meta: ReadonlyRecord<String, Any>?
+    var meta: TableMeta?
 
     /**
      * The `onStateChange` option can be used to optionally listen to state changes within the table.
---
+++ react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/TableMeta.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package tanstack.table.core
+
+import js.objects.ReadonlyRecord
+
+typealias TableMeta = ReadonlyRecord<String, Any>
