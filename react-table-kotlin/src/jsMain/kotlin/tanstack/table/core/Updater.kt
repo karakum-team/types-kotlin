434d4de1167d6e2b504db209e40261e5cf200a0a
porotkin <preload737773@gmail.com>
2025-03-05 11:29:30 +0200

    Update declarations

--- react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/Updater.kt
+++ react-table-kotlin/src/jsMain/kotlin/tanstack/table/core/Updater.kt
@@ -4,14 +4,14 @@ package tanstack.table.core
 
 import js.reflect.unsafeCast
 
-sealed external interface Updater<T> /* T | ((old: T) -> T) */
+sealed external interface Updater<T : Any> /* T | ((old: T) -> T) */
 
-inline fun <T> Updater(
+inline fun <T : Any> Updater(
     source: T,
 ): Updater<T> =
     unsafeCast(source)
 
-inline fun <T> Updater(
+inline fun <T : Any> Updater(
     noinline source: (old: T) -> T,
 ): Updater<T> =
     unsafeCast(source)
