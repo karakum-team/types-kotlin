d45d5750563d64ae9aaf127a13aa359d36d3497f
Victor Turansky <victor.turansky@gmail.com>
2025-03-31 02:02:56 +0300

    TanStack Table. Fix nullability

--- buildSrc/src/main/kotlin/karakum/table/KotlinType.kt
+++ buildSrc/src/main/kotlin/karakum/table/KotlinType.kt
@@ -5,9 +5,9 @@ internal const val UNIT = "Unit"
 internal const val STRING = "String"
 
 private val STANDARD_TYPE_MAP = mapOf(
-    "any" to "Any",
+    "any" to "Any?",
     "object" to "Any",
-    "unknown" to "Any",
+    "unknown" to "Any?",
 
     "boolean" to "Boolean",
     "string" to STRING,
