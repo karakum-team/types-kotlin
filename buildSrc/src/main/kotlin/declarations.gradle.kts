9bcb83921a3fe8a1f4e8fcad0312314fa5d53e20
Victor Turansky <victor.turansky@gmail.com>
2025-03-31 00:13:47 +0300

    Build. Update tasks list

--- buildSrc/src/main/kotlin/declarations.gradle.kts
+++ buildSrc/src/main/kotlin/declarations.gradle.kts
@@ -7,7 +7,7 @@ val generateDeclarations by tasks.registering {
 }
 
 val compileTasks = listOfNotNull(
-    tasks.findByPath("compileCommonMainKotlinMetadata"),
+    tasks.findByPath("compileKotlinMetadata"),
     tasks.getByPath("compileKotlinJs"),
     tasks.findByPath("compileKotlinWasmJs"),
 )
