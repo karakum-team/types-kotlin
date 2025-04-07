e0c36217a88a022875f0e476a0d789ea05dc183c
Victor Turansky <victor.turansky@gmail.com>
2025-04-08 00:14:03 +0300

    Actions. Fix sync

--- actions-kotlin/build.gradle.kts
+++ actions-kotlin/build.gradle.kts
@@ -19,7 +19,77 @@ dependencies {
     jsMainImplementation(libs.coroutines.core)
 }
 
+val syncActionsArtifact by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/artifact/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-artifact"))
+}
+
+val syncActionsCache by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/cache/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-cache"))
+}
+
+val syncActionsCore by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/core/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-core"))
+}
+
+val syncActionsExec by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/exec/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-exec"))
+}
+
+val syncActionsGlob by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/glob/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-glob"))
+}
+
+val syncActionsGitHub by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/github/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-github"))
+}
+
+val syncActionsHttpClient by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/http/client/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-http-client"))
+}
+
+val syncActionsIO by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/io/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-io"))
+}
+
+val syncActionsToolCache by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("actions/tool/cache/")
+    }
+    into(kotlinWrappersCommonDir("kotlin-actions-tool-cache"))
+}
+
 val syncWithWrappers by tasks.registering(SyncWrappers::class) {
-    from(jsGeneratedDir)
-    into(kotlinWrappersDir("kotlin-actions-toolkit"))
+    dependsOn(syncActionsArtifact)
+    dependsOn(syncActionsCache)
+    dependsOn(syncActionsCore)
+    dependsOn(syncActionsExec)
+    dependsOn(syncActionsGlob)
+    dependsOn(syncActionsGitHub)
+    dependsOn(syncActionsHttpClient)
+    dependsOn(syncActionsIO)
+    dependsOn(syncActionsToolCache)
 }
